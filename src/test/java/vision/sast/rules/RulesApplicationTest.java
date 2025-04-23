0cff3bf590b68a7f1fcccc01bfa738afb666dcd9
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-23 23:53:52 +0800

    react

--- src/test/java/vision/sast/rules/RulesApplicationTest.java
+++ src/test/java/vision/sast/rules/RulesApplicationTest.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class RulesApplicationTest {
     public static void main(String[] args) {
-        String[] args1 = {"src/test/resources/issue.json"};
+        String[] args1 = {"D:/development/github/engine/vision/target/workspace1/projectName/systemConstraint.json"};
         System.out.println(new File(args1[0]).getAbsolutePath());
         RulesApplication.main(args1);
     }
