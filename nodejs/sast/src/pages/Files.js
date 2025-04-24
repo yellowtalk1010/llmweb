4cb4dca9864859d8d9a3643518cc3904fc6373bd
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-24 14:16:47 +0800

    react开发

---
+++ nodejs/sast/src/pages/Files.js
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+
+function Files() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>文件集</h1>
+       
+    </div>
+  );
+}
+
+export default Files;
---
+++ nodejs/sast/src/pages/Rules.js
@@ -0,0 +1,12 @@
+import { useNavigate } from 'react-router-dom';
+
+function Rules() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>规则集</h1>
+    </div>
+  );
+}
+
+export default Rules;
