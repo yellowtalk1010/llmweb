a6c7a9b8df6748bc0285f5159ec761af0e959b5b
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-24 16:47:34 +0800

    react开发

--- nodejs/sast/src/pages/SourceCode.js
+++ nodejs/sast/src/pages/SourceCode.js
@@ -2,6 +2,14 @@ import { useNavigate } from 'react-router-dom';
 import {Fragment, useState } from "react"
 import { useParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+
+import '../cpp.css'
+import '../marked.min.js'
+// import './resources/float_window.js'
+// import '../../public/resources/cpp.css'
+// import './resources/marked.min.js'
 
 function SourceCode() {
 
@@ -20,7 +28,7 @@ function SourceCode() {
     status: 0
   })
   if(ruleData.status==0){
-    fetch('/rule_vtid?vtid='+vtid, {
+    fetch('/sourceCode_list?vtid='+vtid + '&file='+file, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
@@ -42,11 +50,17 @@ function SourceCode() {
   }
 
   return (
-    <div id="SourceCode">
-      
-    
+    <>
+        <div id="SourceCode">
+        
+            hello world.
+            <br></br>
+            {vtid}
+            <br></br>
+            {file}
 
-    </div>
+        </div>
+    </>
   );
 }
 
