1cc13397d9fde7ddb5c87ce4e1c2ec64b864b18b
huangliao1010-admin <huangliao1010@gmail.com>
2025-04-27 22:56:55 +0800

    文件上传

--- src/main/java/vision/sast/rules/controller/IndexController.java
+++ src/main/java/vision/sast/rules/controller/IndexController.java
@@ -1,30 +1,11 @@
 package vision.sast.rules.controller;
 
-import org.commonmark.node.Node;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import vision.sast.rules.RulesApplication;
 
-@RestController
+@Controller
 public class IndexController {
-
-    public static String baseInfo(){
-        return RulesApplication.ISSUE_FILEPATH + "<br>"
-                + "issue 总数：" + RulesApplication.ISSUE_RESULT.getResult().size() + "<br>"
-                + "<a href='files'>文件集</a>"  + "<br>"
-                + "<a href='rules'>规则集</a>"  + "<br>"
-                ;
-    }
-
-
-    @GetMapping("llm")
-    public  String index1(){
-        return baseInfo();
-    }
-
 
     @GetMapping("/")
     public String index(Model model) {
@@ -32,5 +13,4 @@ public class IndexController {
         System.out.println("Hello Spring User");
         return "index"; // 这将返回templates目录下的index.html文件
     }
-
 }
---
+++ src/main/java/vision/sast/rules/controller/LLMController.java
@@ -0,0 +1,24 @@
+package vision.sast.rules.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import vision.sast.rules.RulesApplication;
+
+@RestController
+public class LLMController {
+
+    public static String baseInfo(){
+        return RulesApplication.ISSUE_FILEPATH + "<br>"
+                + "issue 总数：" + RulesApplication.ISSUE_RESULT.getResult().size() + "<br>"
+                + "<a href='files'>文件集</a>"  + "<br>"
+                + "<a href='rules'>规则集</a>"  + "<br>"
+                ;
+    }
+
+
+    @GetMapping("llm")
+    public  String llm(){
+        return baseInfo();
+    }
+
+}
