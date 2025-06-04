b5e255527f586df8ab2f63246bc9f29a1fb449e9
huangliao1010-admin <huangliao1010@gmail.com>
2025-06-04 20:43:52 +0800

    尝试GBK/UTF-8两种方式

--- src/main/java/vision/sast/rules/controller/FileController.java
+++ src/main/java/vision/sast/rules/controller/FileController.java
@@ -12,42 +12,8 @@ import java.util.stream.Collectors;
 @RestController
 public class FileController {
 
-    @GetMapping("file")
-    public synchronized String file(String f) {
-        List<IssueDto> ls = Database.fileIssuesMap.get(f);
 
-        //数量
-        Map<String, List<IssueDto>> vtidGroupMap = ls.stream().collect(Collectors.groupingBy(dto->dto.getVtId()));
 
-        Map<String, String> vtidMap = new HashMap<>();
-        ls.stream().forEach(dto->{
-            int size = 0;
-            if(vtidGroupMap.get(dto.getVtId())!=null){
-                size = vtidGroupMap.get(dto.getVtId()).size();
-            }
-            String url = "<a href='sourceCode?vtid=" + dto.getVtId() + "&file=" + f + "'>"+ dto.getVtId() + "</a>&nbsp;&nbsp;&nbsp;" + size
-                    + "<br>" + dto.getRule()
-                    + "<br>" + dto.getDefectLevel()
-                    + "<br>" + dto.getRuleDesc()
-                    + "<br>" + "-------------------------------------------------------"
-                    + "<br>";
-            vtidMap.put(dto.getVtId(), url);
-        });
 
-        StringBuilder stringBuilder = new StringBuilder();
-        vtidMap.values().forEach(vtid->stringBuilder.append(vtid));
-
-        return f + ", " + ls.size() + "<br>" + stringBuilder.toString();
-    }
-
-    @GetMapping("/llm/files")
-    public String files(){
-        StringBuilder stringBuilder = new StringBuilder();
-        Database.fileList.stream().map(file->{
-            String str = "<a href='file?f="+file+"'>"+file+"</a>&nbsp;&nbsp;&nbsp;" + Database.fileIssuesMap.get(file).size();
-            return str + "<br>";
-        }).forEach(stringBuilder::append);
-        return stringBuilder.toString();
-    }
 
 }
--- src/main/java/vision/sast/rules/controller/LLMController.java
+++ src/main/java/vision/sast/rules/controller/LLMController.java
@@ -4,23 +4,63 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vision.sast.rules.Database;
 import vision.sast.rules.RulesApplication;
+import vision.sast.rules.dto.IssueDto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class LLMController {
 
-    public static String baseInfo(){
+
+    @GetMapping("llm")
+    public  String llm(){
         return Database.ISSUE_FILEPATH + "<br>"
                 + "issue 总数：" + Database.ISSUE_RESULT.getResult().size() + "<br>"
-                + "<a href='llm/files'>文件集</a>"  + "<br>"
+                + "<a href='llm_files'>文件集</a>"  + "<br>"
                 + "<a href='llm_rules'>规则集</a>"  + "<br>"
                 + "<a>http://localhost:8080/pages/Rules</a>" + "<br>"
                 ;
     }
 
+    @GetMapping("llm_files")
+    public String llm_files(){
+        StringBuilder stringBuilder = new StringBuilder();
+        Database.fileList.stream().map(file->{
+            String str = "<a href='llm_file?f="+file+"'>"+file+"</a>&nbsp;&nbsp;&nbsp;" + Database.fileIssuesMap.get(file).size();
+            return str + "<br>";
+        }).forEach(stringBuilder::append);
+        return stringBuilder.toString();
+    }
 
-    @GetMapping("llm")
-    public  String llm(){
-        return baseInfo();
+    @GetMapping("llm_file")
+    public synchronized String llm_file(String f) {
+        List<IssueDto> ls = Database.fileIssuesMap.get(f);
+
+        //数量
+        Map<String, List<IssueDto>> vtidGroupMap = ls.stream().collect(Collectors.groupingBy(dto->dto.getVtId()));
+
+        Map<String, String> vtidMap = new HashMap<>();
+        ls.stream().forEach(dto->{
+            int size = 0;
+            if(vtidGroupMap.get(dto.getVtId())!=null){
+                size = vtidGroupMap.get(dto.getVtId()).size();
+            }
+            String url = "<a href='llm_sourcecode?vtid=" + dto.getVtId() + "&file=" + f + "'>"+ dto.getVtId() + "</a>&nbsp;&nbsp;&nbsp;" + size
+                    + "<br>" + dto.getRule()
+                    + "<br>" + dto.getDefectLevel()
+                    + "<br>" + dto.getRuleDesc()
+                    + "<br>" + "-------------------------------------------------------"
+                    + "<br>";
+            vtidMap.put(dto.getVtId(), url);
+        });
+
+        StringBuilder stringBuilder = new StringBuilder();
+        vtidMap.values().forEach(vtid->stringBuilder.append(vtid));
+
+        return f + ", " + ls.size() + "<br>" + stringBuilder.toString();
     }
 
 }
