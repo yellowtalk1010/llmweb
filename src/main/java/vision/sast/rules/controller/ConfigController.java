bb9472a0d1b449666a34152141b8abd19b42cad3
zuk <huangliao1010@gmail.com>
2025-06-29 22:58:23 +0800

    add

--- src/main/java/vision/sast/rules/controller/ConfigController.java
+++ src/main/java/vision/sast/rules/controller/ConfigController.java
@@ -1,14 +1,62 @@
 package vision.sast.rules.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import vision.sast.rules.Database;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @RestController
 public class ConfigController {
 
-    @GetMapping("upload_config")
-    public String upload_config() {
-        return "";
+    @PostMapping("upload_config")
+    public String upload_config(@RequestParam("file") MultipartFile file) {
+
+        if (file.isEmpty()) {
+            return "文件为空！";
+        }
+
+        try {
+            // 直接读取文件内容
+            String content = new BufferedReader(
+                    new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8)
+            ).lines().collect(Collectors.joining("\n"));
+
+            // 可以在这里处理 content，比如解析、打印、返回等
+//            System.out.println("收到文件内容：");
+//            System.out.println(content);
+
+//            Database.buildIssue(null, content);
+
+            StringBuilder stringBuilder = new StringBuilder();
+//            stringBuilder.append("<!DOCTYPE html>\n" +
+//                    "                        <html lang=\"zh-CN\">\n" +
+//                    "                        <head>\n" +
+//                    "                          <meta charset=\"UTF-8\">\n" +
+//                    "                          <title>配置文件</title>\n" +
+//                    "                        </head>\n" +
+//                    "                        <body>");
+            Arrays.stream(content.split("\n")).forEach(line->{
+                line = "<div>" + line + "</div>";
+                stringBuilder.append(line);
+            });
+
+//            stringBuilder.append("\n" +
+//                    "                        </body>\n" +
+//                    "                        </html>");
+            return content;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
     }
 
     @GetMapping("config")
