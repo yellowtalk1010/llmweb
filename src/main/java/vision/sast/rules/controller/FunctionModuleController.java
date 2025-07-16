106dbfe2ff1b800fa89aff8896236661702ebc38
zuk <huangliao1010@gmail.com>
2025-07-16 23:44:17 +0800

    5.0.0版本开发

--- src/main/java/vision/sast/rules/controller/FunctionModuleController.java
+++ src/main/java/vision/sast/rules/controller/FunctionModuleController.java
@@ -6,31 +6,71 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RestController("")
 public class FunctionModuleController {
 
+    private static List<Map<String, String>> MAP = new ArrayList<>();
+
+    private static String createKey(Map<String, String> map) {
+        String filePath =  map.get("filePath");
+        String line = String.valueOf(map.get("line"));
+        return filePath + ":" + line;
+    }
+
     @GetMapping("func_module_path")
     public String func_module_path(String path){
+        MAP.clear();
         File file = new File(path);
         if(!file.exists()){
             return "函数模型路径不存在:" + path;
         }
         else {
             try {
-                StringBuilder stringBuilder = new StringBuilder();
                 List<String> list = FileUtils.readLines(new File(path), "UTF-8");
-                list.forEach(line->{
-                    Map<String, String> map = JSON.parseObject(line,Map.class);
-                    stringBuilder.append(JSON.toJSONString(map) + "<br>");
+                Set<String> paths = new HashSet<>();
+                list.forEach(jsonline->{
+                    Map<String, String> map = JSON.parseObject(jsonline, Map.class);
+                    MAP.add(map);
+
+                    String filePath =  map.get("filePath");
+//                    String line = String.valueOf(map.get("line"));
+//                    String key = createKey(map);
+                    paths.add(filePath);
                 });
-                return stringBuilder.toString();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                paths.stream().forEach(e->{
+                    stringBuilder.append("<li>" + e + "</li>");
+                });
+
+                String html = """
+                    <!DOCTYPE html>
+                    <html lang="zh-CN">
+                    <head>
+                      <meta charset="UTF-8">
+                      <title>函数模型</title>
+                    </head>
+                    <body>
+                        <ul>
+                            {{{stringBuilder}}}
+                        </ul>
+                    </body>
+                    </html>
+                        """;
+
+                html = html.replace("{{{stringBuilder}}}", stringBuilder.toString());
+
+                return html;
             }catch (Exception e){
                 return e.getMessage();
             }
         }
 
     }
+
+
+
 }
