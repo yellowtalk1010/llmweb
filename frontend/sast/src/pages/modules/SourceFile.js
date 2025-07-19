36db112bf70bd4fd41d2ac224e63cfbf4397365d
zuk <huangliao1010@gmail.com>
2025-07-19 15:50:19 +0800

    5.0.0版本开发

--- frontend/sast/src/pages/modules/SourceFile.js
+++ frontend/sast/src/pages/modules/SourceFile.js
@@ -1,11 +1,59 @@
 import {Fragment, useState, useEffect } from "react"
 
 function SourceFile({node}) {
+
+    const file = node.path
+    
+    const [sourceCodeData, setSourceCodeData] = useState([])
+
+    useEffect(() => {
+        fetch('/otherSourceCode_list?file='+file, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(res =>{
+          const json = res.json();
+          // console.info(json)
+          return json
+        }).then(data =>{
+          // console.log("rule_vtid 的数据")
+          console.log(data)
+          setSourceCodeData(data)
+          // console.info("渲染完成后执行")
+        }).catch(e =>{
+          console.log(e)
+        })
+    }, []);
+
+  
+
     return (
         <>
-        <span>{node.path}</span>
-        
-        <div>展示源代码</div>
+            <div>
+                <span>{file}</span>
+            </div>
+
+            <div id="SourceCode">
+                <div id="sourcecode_file">
+                    <ol>
+                    {
+                        sourceCodeData.map((lineHtml, index) => {
+                            const newLiElement = (
+                            <span
+                            key={index}
+                            dangerouslySetInnerHTML={{ __html: lineHtml }} //将字符串转成 react元素
+                            />
+                            );
+                            return newLiElement
+ 
+                        })
+                    
+                    }
+                    </ol>
+                </div>
+            </div>
+            
         </>
     );
 }
