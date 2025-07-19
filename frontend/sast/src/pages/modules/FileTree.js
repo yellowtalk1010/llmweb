e9571f40887654c81992d945348bcfc6f63d07b4
zuk <huangliao1010@gmail.com>
2025-07-19 12:31:07 +0800

    5.0.0版本开发

--- frontend/sast/src/pages/modules/FileTree.js
+++ frontend/sast/src/pages/modules/FileTree.js
@@ -4,19 +4,40 @@ function FileTree({ nodes, onSelectFile }) {
   return (
     <ul style={{ listStyleType: 'none', paddingLeft: '1rem' }}>
       {nodes.map((node, index) => (
-        <li key={index}>
-          <span
-            style={{ cursor: node.children ? 'default' : 'pointer', fontWeight: node.children ? 'bold' : 'normal' }}
-            onClick={() => !node.children && onSelectFile(node)}
-          >
-            {node.name}
-          </span>
-          {node.children && (
-            <FileTree nodes={node.children} onSelectFile={onSelectFile} />
-          )}
-        </li>
+        <TreeNode key={index} node={node} onSelectFile={onSelectFile} />
       ))}
     </ul>
+  );
+}
+
+function TreeNode({ node, onSelectFile }) {
+  const [expanded, setExpanded] = useState(false);
+  const isFolder = Array.isArray(node.children) && Array.from(node.children).length > 0;  //如果子孩子数量大于1就是文件夹
+
+  const handleClick = () => {
+    if (isFolder) {
+      setExpanded(!expanded);
+    } else {
+      onSelectFile(node);
+    }
+  };
+
+  return (
+    <li>
+      <div
+        onClick={handleClick}
+        style={{
+          cursor: 'pointer',
+          fontWeight: isFolder ? 'bold' : 'normal',
+          userSelect: 'none',
+        }}
+      >
+        {isFolder ? (expanded ? '📂' : '📁') : '📄'} {node.name}
+      </div>
+      {isFolder && expanded && (
+        <FileTree nodes={node.children} onSelectFile={onSelectFile} />
+      )}
+    </li>
   );
 }
 
