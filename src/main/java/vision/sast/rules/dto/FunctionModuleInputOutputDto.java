003fa73d1a58cbd360cc150660f62cba63d42aab
zuk <huangliao1010@gmail.com>
2025-07-21 23:49:13 +0800

    6.0.0 自留版本

--- src/main/java/vision/sast/rules/dto/FunctionModuleInputOutputDto.java
+++ src/main/java/vision/sast/rules/dto/FunctionModuleInputOutputDto.java
@@ -1,7 +1,28 @@
 package vision.sast.rules.dto;
 
+import lombok.Data;
+
+import java.util.Map;
+
 /***
  * 函数建模
  */
+@Data
 public class FunctionModuleInputOutputDto {
+//    private String compiler; //编译器
+//    private String version; //编译器版本
+    private String fileName; //文件名称
+    private String fileMD5;  //文件MD5
+    private String functionName; //函数名称
+    private String functionRawSignature; //函数内容
+    private java.util.List<FunctionParamDto> params; //函数参数列表
+    private Map<String, String> returnValueMap; //返回值
+
+    @Data
+    public static class FunctionParamDto {
+        private String paramName; //参数名称
+        private boolean input;
+        private boolean output;
+    }
+
 }
