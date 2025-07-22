1e181d52c079a0fb9dc2a5552511c3ac5ccf3502
qq5132834 <513283439@qq.com>
2025-07-22 10:47:16 +0800

    窗口

--- src/main/java/vision/sast/rules/dto/FunctionModuleInputOutputDto.java
+++ src/main/java/vision/sast/rules/dto/FunctionModuleInputOutputDto.java
@@ -9,14 +9,14 @@ import java.util.Map;
  */
 @Data
 public class FunctionModuleInputOutputDto {
-//    private String compiler; //编译器
-//    private String version; //编译器版本
+    private String compiler; //编译器
+    private String version; //编译器版本
     private String fileName; //文件名称
     private String fileMD5;  //文件MD5
     private String functionName; //函数名称
     private String functionRawSignature; //函数内容
     private java.util.List<FunctionParamDto> params; //函数参数列表
-    private Map<String, String> returnValueMap; //返回值
+    private FunctionReturnValueDto returnValueDto; //返回值
 
     @Data
     public static class FunctionParamDto {
@@ -24,6 +24,13 @@ public class FunctionModuleInputOutputDto {
         private String paramRawSignature; //参数内容
         private boolean input;
         private boolean output;
+    }
+
+    @Data
+    public static class FunctionReturnValueDto {
+        private Boolean isLiteral;
+        private String literalValue;
+        private Map<String, String> returnValueMap;
     }
 
 }
