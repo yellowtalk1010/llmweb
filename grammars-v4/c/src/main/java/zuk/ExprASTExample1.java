7976ecdf33463d7db8ee0b640b27395d6e6ec452
zuk <huangliao1010@gmail.com>
2025-07-25 00:12:33 +0800

    commit

--- grammars-v4/c/src/main/java/zuk/ExprASTExample1.java
+++ grammars-v4/c/src/main/java/zuk/ExprASTExample1.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class ExprASTExample1 {
 
     public static void main(String[] args) throws IOException {
-        String source = Files.readString(Paths.get("grammars-v4/c/examples/zuk_keil51.c"));
+        String source = Files.readString(Paths.get("grammars-v4/c/examples/keil51_demo1.c"));
 
         System.out.println("=== 原始源码 ===");
         System.out.println(source);
@@ -50,22 +50,26 @@ public class ExprASTExample1 {
 
         @Override
         public void enterFunctionDefinitonSuffix(CParser.FunctionDefinitonSuffixContext ctx) {
-            Token start = ctx.getStart();
-            Token stop = ctx.getStop();
+            List<ParseTree> children = ctx.children;
+            if(children!=null && children.size()>0){
+                Token start = ctx.getStart();
+                Token stop = ctx.getStop();
 
-            // 计算总长度（含换行、空格）
-            int startIdx = start.getStartIndex();
-            int stopIdx = stop.getStopIndex();
-            int length = stopIdx - startIdx + 1;
+                // 计算总长度（含换行、空格）
+                int startIdx = start.getStartIndex();
+                int stopIdx = stop.getStopIndex();
+                int length = stopIdx - startIdx + 1;
 
-            // 生成等长空格字符串
-            StringBuilder blank = new StringBuilder();
-            for (int i = 0; i < length; i++) blank.append(' ');
+                // 生成等长空格字符串
+                StringBuilder blank = new StringBuilder();
+                for (int i = 0; i < length; i++) blank.append(' ');
 
-            // 替换原始区域为空格
-            rewriter.replace(start, stop, blank.toString());
+                // 替换原始区域为空格
+                rewriter.replace(start, stop, blank.toString());
 
-            System.out.println("清空 FunctionDefinitonSuffix 区域：" + startIdx + " ~ " + stopIdx);
+                System.out.println("清空 FunctionDefinitonSuffix 区域：" + startIdx + " ~ " + stopIdx);
+            }
+
         }
     }
 
