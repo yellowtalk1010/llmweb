9d638a857b46ee8e64ffeb7529d4de9167a91cd1
qq5132834 <513283439@qq.com>
2025-07-24 18:34:48 +0800

    窗口

--- src/main/java/vision/sast/rules/DatabaseFunctionModule.java
+++ src/main/java/vision/sast/rules/DatabaseFunctionModule.java
@@ -29,13 +29,22 @@ public class DatabaseFunctionModule {
     }
 
     public static IssueDto queryIssueDtoById(String id) {
-        List<IssueDto> issueDtos = functionModuleIssues.stream().filter(issueDto -> issueDto.getId().equals(id)).toList();
+//        List<IssueDto> issueDtos = functionModuleIssues.stream().filter(issueDto -> issueDto.getId().equals(id)).toList();
+//        if(issueDtos!=null && issueDtos.size()>0){
+//            return issueDtos.get(0);
+//        }
+//        else {
+//            return null;
+//        }
+
+        List<IssueDto> issueDtos = DatabaseIssue.getAllIssue().stream().filter(issueDto -> issueDto.getId().equals(id)).toList();
         if(issueDtos!=null && issueDtos.size()>0){
             return issueDtos.get(0);
         }
         else {
             return null;
         }
+
     }
 
     /***
