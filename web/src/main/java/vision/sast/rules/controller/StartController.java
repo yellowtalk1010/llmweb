b39b2acddf3eeb647465439441263b09a119e04e
qq5132834 <513283439@qq.com>
2025-07-25 13:37:36 +0800

    commit

--- web/src/main/java/vision/sast/rules/controller/StartController.java
+++ web/src/main/java/vision/sast/rules/controller/StartController.java
@@ -11,25 +11,9 @@ public class StartController {
         if(token==null || token.isEmpty() || !token.equals(MarioController.RUN_TOKEN)){
             return "启动启动启动，马力欧.奥德赛";
         }
-        MarioController.RUN_TOKEN = "";
-
-        return """
-                    <!DOCTYPE html>
-                    <html lang="zh-CN">
-                    <head>
-                      <meta charset="UTF-8">
-                      <title>问题结果</title>
-                    </head>
-                    <body>
-                       <form action="/start_form">
-                       <input type="text" name="projectName" value="" />  <br>
-                       
-                       <input type="text" name="password" value="" />  <br>
-                       <button type="submit" value="启动" />
-                       </form>
-                    </body>
-                    </html>
-               """;
+        else {
+            return "o_o";
+        }
     }
 
 }
