b3510272f90a2356608bebefbf045721f13c45e2
qq5132834 <513283439@qq.com>
2025-08-07 14:26:48 +0800

    run

--- web/src/main/java/vision/sast/rules/controller/StartController.java
+++ web/src/main/java/vision/sast/rules/controller/StartController.java
@@ -3,16 +3,18 @@ package vision.sast.rules.controller;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONWriter;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class StartController {
+
+    private static final List<String> COMMAND_LIST = new ArrayList<>(); //记录运行成功的命令
 
     @GetMapping("start")
     public String start(String token) {
@@ -31,8 +33,14 @@ public class StartController {
         private String fileContent;
     }
 
+
+
     @PostMapping("run_command")
     public Map<String, String> runCommand(@RequestBody RunCommandDto runCommandDto) {
+//        if(StringUtils.isNotBlank(runCommandDto.getCommand())){
+//            StringBuilder stringBuilder = new StringBuilder();
+//            Arrays.stream(runCommandDto.getCommand().split(" ")).forEach(s->stringBuilder.append(s + " "));
+//        }
         System.out.println(JSON.toJSONString(runCommandDto, JSONWriter.Feature.PrettyFormat));
         Map<String, String> map = new HashMap<>();
         return map;
