8009852282de7b9c759bf5bc93c3c3d61fe1758a
zuk <huangliao1010@gmail.com>
2025-08-08 23:18:56 +0800

    添加许可

--- web/src/main/java/vision/sast/rules/interceptor/LocalhostInterceptor.java
+++ web/src/main/java/vision/sast/rules/interceptor/LocalhostInterceptor.java
@@ -10,11 +10,15 @@ import vision.sast.rules.DatabaseIssue;
  * 限制只能localhost、127.0.0.1访问
  */
 public class LocalhostInterceptor implements HandlerInterceptor {
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        DatabaseIssue.checkLicense();
+
+        DatabaseIssue.checkLicense(); //许可是否过期
+
         String ip = request.getRemoteAddr();
         if (!"127.0.0.1".equals(ip) && !"0:0:0:0:0:0:0:1".equals(ip)) {
+            // 限制只能localhost、127.0.0.1访问
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             response.getWriter().write("403 - Only localhost allowed");
             return false;
@@ -31,4 +35,5 @@ public class LocalhostInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         //获取一个返回结果，请求结束了
     }
+
 }
