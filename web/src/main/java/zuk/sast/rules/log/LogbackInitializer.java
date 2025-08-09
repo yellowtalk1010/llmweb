5bef7b36a78a91fa41bd4cad928c554d95877d46
zuk <huangliao1010@gmail.com>
2025-08-09 16:36:36 +0800

    添加日志

--- web/src/main/java/zuk/sast/rules/log/LogbackInitializer.java
+++ web/src/main/java/zuk/sast/rules/log/LogbackInitializer.java
@@ -9,14 +9,42 @@ import ch.qos.logback.core.FileAppender;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.Level;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /***
  * 在文件中初始化日志信息
  */
 @Configuration
 public class LogbackInitializer {
 
+    private static final String LOG_DIR = "web_workspace/logs/";
+    private static final SimpleDateFormat sdf1 = new SimpleDateFormat("yyyyMMdd");
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
+    private void deleteLogs(){
+        File dir = new File(LOG_DIR);
+        if(dir.exists()){
+            //移除非当天日志
+            Arrays.stream(dir.listFiles()).filter(file -> {
+                String dateStr = sdf1.format(new Date());
+                return !file.getName().startsWith(dateStr);
+            }).forEach(File::delete);
+        }
+    }
+
+    private String createLogFile(){
+        String date = sdf.format(new Date());
+        return LOG_DIR + date + ".log";
+    }
+
     @PostConstruct
     public void initLogger() {
+        this.deleteLogs();
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
 
         // 创建 encoder
@@ -29,7 +57,7 @@ public class LogbackInitializer {
         FileAppender fileAppender = new FileAppender();
         fileAppender.setContext(context);
         fileAppender.setName("GLOBAL_FILE_APPENDER");
-        fileAppender.setFile("web_workspace/logs/global-log.log");
+        fileAppender.setFile(this.createLogFile());
         fileAppender.setEncoder(encoder);
         fileAppender.setAppend(true);
         fileAppender.setImmediateFlush(true);
