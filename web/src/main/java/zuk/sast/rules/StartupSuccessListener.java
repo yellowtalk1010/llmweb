3991eff1f158d8007f75b1952dd5e691bc1f51e6
qq5132834 <513283439@qq.com>
2025-08-21 16:12:51 +0800

    添加h2数据库

--- web/src/main/java/zuk/sast/rules/StartupSuccessListener.java
+++ web/src/main/java/zuk/sast/rules/StartupSuccessListener.java
@@ -5,9 +5,16 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Slf4j
 @Component
 public class StartupSuccessListener {
+
+    private static final ExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
 
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationReady() {
@@ -16,5 +23,16 @@ public class StartupSuccessListener {
                 1. 在浏览器器中输入：http://localhost:8080/config
                 2. 将在工具的/workspaces/项目名称/parseConfig.json上传
                 """);
+        EXECUTOR_SERVICE.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String args[] = new String[0];
+                    org.h2.tools.Console.main(args); //启动h2数据库
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
