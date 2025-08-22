f1938a4c5649824a3f74f15e75650a09b41b0a7b
qq5132834 <513283439@qq.com>
2025-08-22 15:43:27 +0800

    初始化数据库schema数据

--- web/src/main/java/zuk/sast/rules/database/DatabaseInitializer.java
+++ web/src/main/java/zuk/sast/rules/database/DatabaseInitializer.java
@@ -19,6 +19,7 @@ public class DatabaseInitializer {
     @PostConstruct
     public void init() {
         createProjectTable();
+        createIssueTable();
     }
 
     private void createProjectTable() {
@@ -34,6 +35,18 @@ public class DatabaseInitializer {
         jdbcTemplate.execute(sql);
     }
 
+
+    private void createIssueTable(){
+        //创建issue表
+        String sql = """
+            CREATE TABLE IF NOT EXISTS issue (
+                id VARCHAR(100) PRIMARY KEY,
+                num BIGINT NOT NULL,
+                content BLOB NOT NULL
+            )
+            """;
+        jdbcTemplate.execute(sql);
+    }
 
 
 }
