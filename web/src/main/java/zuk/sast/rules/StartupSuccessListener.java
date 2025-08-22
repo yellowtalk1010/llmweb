2906fee639b8a7768c6bcf341e60fe7e4174fddf
qq5132834 <513283439@qq.com>
2025-08-22 11:01:57 +0800

    添加h2数据库

--- web/src/main/java/zuk/sast/rules/StartupSuccessListener.java
+++ web/src/main/java/zuk/sast/rules/StartupSuccessListener.java
@@ -1,10 +1,13 @@
 package zuk.sast.rules;
 
 import lombok.extern.slf4j.Slf4j;
+import org.h2.Zuk;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -19,10 +22,11 @@ public class StartupSuccessListener {
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationReady() {
         log.info("StartupSuccessListener onApplicationReady");
-        System.out.println("""
+        System.out.print("""
                 1. 在浏览器器中输入：http://localhost:8080/config
                 2. 将在工具的/workspaces/项目名称/parseConfig.json上传
                 """);
+        System.out.println("3. DB地址：" + Zuk.URL);
 
     }
 }
