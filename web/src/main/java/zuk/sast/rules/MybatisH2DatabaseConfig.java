0bce83a537910c4ae338ac3829ec94353e214a65
qq5132834 <513283439@qq.com>
2025-08-22 10:07:42 +0800

    添加h2数据库

--- web/src/main/java/zuk/sast/rules/MybatisH2DatabaseConfig.java
+++ web/src/main/java/zuk/sast/rules/MybatisH2DatabaseConfig.java
@@ -7,6 +7,8 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -79,9 +81,9 @@ public class MybatisH2DatabaseConfig {
         return sessionFactory.getObject();
     }
 
-//    @Bean
-//    public PlatformTransactionManager transactionManager(DataSource dataSource) {
-//        return new DataSourceTransactionManager(dataSource);
-//    }
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 
 }
