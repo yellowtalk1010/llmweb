fafe12fd204b5458a8e432dde7241e0961686c6e
qq5132834 <513283439@qq.com>
2025-08-25 13:57:09 +0800

    函数建模

--- web/src/main/java/zuk/sast/rules/DatabaseFunctionModule.java
+++ web/src/main/java/zuk/sast/rules/DatabaseFunctionModule.java
@@ -33,14 +33,6 @@ public class DatabaseFunctionModule {
     }
 
     public static IssueDto queryIssueDtoById(String id) {
-//        List<IssueDto> issueDtos = functionModuleIssues.stream().filter(issueDto -> issueDto.getId().equals(id)).toList();
-//        if(issueDtos!=null && issueDtos.size()>0){
-//            return issueDtos.get(0);
-//        }
-//        else {
-//            return null;
-//        }
-
         List<IssueDto> issueDtos = DatabaseIssue.getAllIssue().stream().filter(issueDto -> issueDto.getId().equals(id)).toList();
         if(issueDtos!=null && issueDtos.size()>0){
             return issueDtos.get(0);
@@ -48,50 +40,49 @@ public class DatabaseFunctionModule {
         else {
             return null;
         }
-
     }
 
-    /***
-     * 查询当前文件中需要建模的issue数据
-     * @param file
-     * @return
-     */
-    public static List<IssueDto> queryIssuesByFile(String file) {
-        return functionModuleIssues.stream().filter(issueDto -> issueDto.getFilePath()!=null && issueDto.getFilePath().equals(file)).toList();
-    }
+//    /***
+//     * 查询当前文件中需要建模的issue数据
+//     * @param file
+//     * @return
+//     */
+//    public static List<IssueDto> queryIssuesByFile(String file) {
+//        return functionModuleIssues.stream().filter(issueDto -> issueDto.getFilePath()!=null && issueDto.getFilePath().equals(file)).toList();
+//    }
 
-    /***
-     * 根据路径，初始化函数建模数据
-     * @param funcitonModulePath
-     */
-    private static List<IssueDto> initFunctionModuleDatabase(String funcitonModulePath) {
-        try {
-            MAP.clear();
-            functionModuleIssues.clear();
-
-            List<String> list = FileUtils.readLines(new File(funcitonModulePath), "UTF-8");
-            Set<String> paths = new HashSet<>();
-            list.forEach(jsonline->{
-                Map<String, String> map = JSON.parseObject(jsonline, Map.class);
-
-                IssueDto issueDto = JSONObject.parseObject(JSONObject.toJSONString(map), IssueDto.class);
-                issueDto.setLine(Integer.valueOf(String.valueOf(map.get("line"))));
-                FunctionModuleInputOutputDto functionModuleInputOutputDto = JSONObject.parseObject(JSONObject.toJSONString(map.get("functionModuleInputOutputDto")), FunctionModuleInputOutputDto.class);
-                issueDto.setData(functionModuleInputOutputDto);
-
-                functionModuleIssues.add(issueDto);
-
-                paths.add(issueDto.getFilePath());
-            });
-
-            files.addAll(paths);
-
-            return functionModuleIssues;
-        }catch (Exception e){
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
-
-    }
+//    /***
+//     * 根据路径，初始化函数建模数据
+//     * @param funcitonModulePath
+//     */
+//    private static List<IssueDto> initFunctionModuleDatabase(String funcitonModulePath) {
+//        try {
+//            MAP.clear();
+//            functionModuleIssues.clear();
+//
+//            List<String> list = FileUtils.readLines(new File(funcitonModulePath), "UTF-8");
+//            Set<String> paths = new HashSet<>();
+//            list.forEach(jsonline->{
+//                Map<String, String> map = JSON.parseObject(jsonline, Map.class);
+//
+//                IssueDto issueDto = JSONObject.parseObject(JSONObject.toJSONString(map), IssueDto.class);
+//                issueDto.setLine(Integer.valueOf(String.valueOf(map.get("line"))));
+//                FunctionModuleInputOutputDto functionModuleInputOutputDto = JSONObject.parseObject(JSONObject.toJSONString(map.get("functionModuleInputOutputDto")), FunctionModuleInputOutputDto.class);
+//                issueDto.setData(functionModuleInputOutputDto);
+//
+//                functionModuleIssues.add(issueDto);
+//
+//                paths.add(issueDto.getFilePath());
+//            });
+//
+//            files.addAll(paths);
+//
+//            return functionModuleIssues;
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            return new ArrayList<>();
+//        }
+//
+//    }
 
 }
