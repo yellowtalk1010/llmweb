4cb9ca855ce7f27e1e666e9dd2309b7c0cfc847c
qq5132834 <513283439@qq.com>
2025-08-26 13:53:47 +0800

    ai checker

--- web/frontend/sast/src/pages/modules/AiCheckTemplate.js
+++ web/frontend/sast/src/pages/modules/AiCheckTemplate.js
@@ -1,13 +1,26 @@
 import {Fragment, useState, useEffect } from "react"
 
 function AiCheckTemplate({ issue }) {
-    console.info("ai checker: " + issue)
+    console.info("ai checker: ")
+    console.info(issue)
 
     return (
         <>
             <div>
-                <a class="ai_check" id={"ai_check_" + issue.id}>AI审计</a>
+                <a class="ai_check">AI审计</a>
+                <div id={"ai_check_" + issue.id}>
+                    <textarea placeholder="请输入..." rows="4" class="textarea"></textarea>
+                </div>
             </div>
+
+            <style>
+                {`
+                textarea {
+                    width: 900px;
+                    height: 100px;
+                }
+                `}
+            </style>
         </>
     );
 }
