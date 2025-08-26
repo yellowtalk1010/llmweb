2dca2e67a0cde955a832c765a78fcfad1204a990
qq5132834 <513283439@qq.com>
2025-08-26 14:06:46 +0800

    ai checker

--- web/frontend/sast/src/pages/modules/AiCheckTemplate.js
+++ web/frontend/sast/src/pages/modules/AiCheckTemplate.js
@@ -8,9 +8,16 @@ function AiCheckTemplate({ issue }) {
         <>
             <div>
                 <a class="ai_check">AI审计</a>
-                <div id={"ai_check_" + issue.id}>
+                <div class="ai_float">
                     <textarea placeholder="请输入..." rows="4" class="textarea"></textarea>
+                    <br></br>
+                    <button class="check-btn">审计</button> 
+                    <button class="close-btn">关闭</button>
+                    <br></br>
+                    <textarea class="textarea_hidden" id="textarea_hidden-${issueId}" hidden="hidden"></textarea>
+                    <div class="result" id={"ai_check_" + issue.id}></div>
                 </div>
+
             </div>
 
             <style>
@@ -18,6 +25,26 @@ function AiCheckTemplate({ issue }) {
                 textarea {
                     width: 900px;
                     height: 100px;
+                }
+                    
+                .close-btn {
+                    margin-top: 10px;
+                    padding: 5px 10px;
+                    background-color: #f44336;
+                    color: #fff;
+                    border: none;
+                    border-radius: 4px;
+                    cursor: pointer;
+                }
+
+                .check-btn {
+                    margin-top: 10px;
+                    padding: 5px 10px;
+                    background-color: green;
+                    color: #fff;
+                    border: none;
+                    border-radius: 4px;
+                    cursor: pointer;
                 }
                 `}
             </style>
