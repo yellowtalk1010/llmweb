68161229aefd2707fa1d08818de4e0e1427d9c12
zuk <huangliao1010@gmail.com>
2025-08-31 02:19:33 +0800

    stock

--- web/src/main/java/zuk/sast/rules/controller/stock/AllStockController.java
+++ web/src/main/java/zuk/sast/rules/controller/stock/AllStockController.java
@@ -14,6 +14,7 @@ import zuk.sast.rules.utils.ResourceFileUtils;
 
 import java.io.File;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /***
  *
@@ -51,30 +52,18 @@ public class AllStockController {
     @GetMapping("delete")
     public synchronized Map<String, Object> delete(String api_code) {
         log.info("delete:" + api_code);
-        File resultFile = new File("data/mystock");
-        log.info("resultFile:" + resultFile.getAbsolutePath() + ", exists:" + resultFile.exists());
+
         Map<String, Object> result = new HashMap<>();
         try {
-            List<String> list = FileUtils.readLines(resultFile, "UTF-8");
-            Map<String, JSONObject> mymap = new HashMap<>();
-            list.stream().forEach(l->{
-                JSONObject jsonObject = JSONObject.parseObject(l);
-                mymap.put(jsonObject.getString("api_code"), jsonObject);
-            });
-            log.info("总数：" + mymap.size());
+
+            Map<String, JSONObject> mymap = readAttention();
 
             mymap.remove(api_code);
 
-            //重新转成行数据
-            List<String> newLines = mymap.entrySet().stream().map(entry->{
-                return JSONObject.toJSONString(entry.getValue(), JSONWriter.Feature.LargeObject);
-            }).toList();
-            //重新写入文件中
-            FileUtils.writeLines(resultFile,"UTF-8", newLines);
+            writeAttention(mymap);
+
             result.put("status", "ok");
             return result;
-
-
 
         }catch (Exception e) {
             //e.printStackTrace();
@@ -85,20 +74,35 @@ public class AllStockController {
         return result;
     }
 
+    private static final File resultFile = new File("data/mystock");
+    public static Map<String, JSONObject> readAttention() throws Exception {
+        log.info("resultFile:" + resultFile.getAbsolutePath() + ", exists:" + resultFile.exists());
+        List<String> list = FileUtils.readLines(resultFile, "UTF-8");
+        Map<String, JSONObject> mymap = new HashMap<>();
+        list.stream().forEach(l->{
+            JSONObject jsonObject = JSONObject.parseObject(l);
+            mymap.put(jsonObject.getString("api_code"), jsonObject);
+        });
+        log.info("总数：" + mymap.size());
+        return mymap;
+    }
+
+    public static boolean writeAttention(Map<String, JSONObject> mymap) throws Exception {
+        //重新转成行数据
+        List<String> newLines = mymap.entrySet().stream().map(entry->{
+            return JSONObject.toJSONString(entry.getValue(), JSONWriter.Feature.LargeObject);
+        }).toList();
+        //重新写入文件中
+        FileUtils.writeLines(resultFile,"UTF-8", newLines);
+        return true;
+    }
+
     @GetMapping("add")
     public synchronized Map<String, Object> add(String api_code) {
         log.info("add:" + api_code);
-        File resultFile = new File("data/mystock");
-        log.info("resultFile:" + resultFile.getAbsolutePath() + ", exists:" + resultFile.exists());
         Map<String, Object> result = new HashMap<>();
         try {
-            List<String> list = FileUtils.readLines(resultFile, "UTF-8");
-            Map<String, JSONObject> mymap = new HashMap<>();
-            list.stream().forEach(l->{
-                JSONObject jsonObject = JSONObject.parseObject(l);
-                mymap.put(jsonObject.getString("api_code"), jsonObject);
-            });
-            log.info("总数：" + mymap.size());
+            Map<String, JSONObject> mymap = readAttention();
             if(mymap.get(api_code)!=null){
                 //已经存在
                 result.put("status", "已存在");
@@ -112,12 +116,8 @@ public class AllStockController {
                     jsonObject.put("name", socket.get("name"));
                     mymap.put(api_code, jsonObject);
                 });
-                //重新转成行数据
-                List<String> newLines = mymap.entrySet().stream().map(entry->{
-                    return JSONObject.toJSONString(entry.getValue(), JSONWriter.Feature.LargeObject);
-                }).toList();
-                //重新写入文件中
-                FileUtils.writeLines(resultFile,"UTF-8", newLines);
+
+                writeAttention(mymap);
 
                 result.put("status", "ok");
                 return result;
@@ -131,6 +131,25 @@ public class AllStockController {
         }
 
         return result;
+    }
+
+    @GetMapping("attention")
+    public Map<String, Object> attention (){
+        try {
+            Map<String, Object> map = new HashMap<>();
+            Set<String> sets = readAttention().entrySet().stream().map(e->e.getKey()).collect(Collectors.toSet());
+            List<Map<String, String>> ls = STOCKS.stream().filter(stock->{
+                return sets.contains(stock.get("api_code"));
+            }).toList();
+
+            map.put("stocks", ls);
+            return map;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage());
+        }
+        return new HashMap<>();
     }
 
     @GetMapping("all")
