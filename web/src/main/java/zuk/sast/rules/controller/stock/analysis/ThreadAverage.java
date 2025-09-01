6ae8d7ea0d8afed73150f210f333785a1f407cde
qq5132834 <513283439@qq.com>
2025-09-01 17:32:20 +0800

    STOCK

--- web/src/main/java/zuk/sast/rules/controller/stock/analysis/ThreadAverage.java
+++ web/src/main/java/zuk/sast/rules/controller/stock/analysis/ThreadAverage.java
@@ -1,5 +1,6 @@
 package zuk.sast.rules.controller.stock.analysis;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -18,7 +19,38 @@ public class ThreadAverage implements Runnable{
 
     @Override
     public void run() {
-        
+
+    }
+
+
+    /***
+     * {
+     * "code":"000001.SZ",
+     * "time":"2025-08-01",
+     * "open":"12.24",
+     * "turnoverRatio":"0.5215960590205436",
+     * "amount":"1240239179",
+     * "high":"12.33",
+     * "low":"12.15",
+     * "changeRatio":"0.40883074407194553", //换手率
+     * "close":"12.28",                     //收盘价
+     * "volume":"101218698"
+     * }
+     */
+
+    @Data
+    public static class StockDayVo {
+        private String code;
+        private String time;
+        private String open;
+        private String turnoverRatio;
+        private String amount;
+        private String high;
+        private String low;
+        private String changeRatio;
+        private String close;
+        private String volume;
+
     }
 
 }
