6c1a8542afc81fb0ce23d4cb1816e0eb8dcb6478
zuk <huangliao1010@gmail.com>
2025-09-01 22:07:06 +0800

    stock

--- web/src/test/java/zuk/stock/StockTest.java
+++ web/src/test/java/zuk/stock/StockTest.java
@@ -1,5 +1,6 @@
 package zuk.stock;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import zuk.sast.rules.controller.stock.analysis.LoaderStockData;
 import zuk.sast.rules.controller.stock.analysis.ThreadAverage;
@@ -14,7 +15,8 @@ public class StockTest {
     @Test
     public void test1() throws Exception {
         loadStockData();
-        ThreadAverage threadAverage = new ThreadAverage(LoaderStockData.STOCKS.stream().map(e->e.getApi_code()).toList());
+//        ThreadAverage threadAverage = new ThreadAverage(LoaderStockData.STOCKS.stream().map(e->e.getApi_code()).toList());
+        ThreadAverage threadAverage = new ThreadAverage(Lists.list("300287"));
         threadAverage.run();
     }
 
