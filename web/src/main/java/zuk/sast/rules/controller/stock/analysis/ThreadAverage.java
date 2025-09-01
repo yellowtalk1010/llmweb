acc126104405b38c4c03475cdbe6f04adc8f06a4
qq5132834 <513283439@qq.com>
2025-09-01 17:26:11 +0800

    STOCK

--- web/src/main/java/zuk/sast/rules/controller/stock/analysis/ThreadAverage.java
+++ web/src/main/java/zuk/sast/rules/controller/stock/analysis/ThreadAverage.java
@@ -2,11 +2,19 @@ package zuk.sast.rules.controller.stock.analysis;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 /***
  * 均线计算
  */
 @Slf4j
 public class ThreadAverage implements Runnable{
+
+    private List<String> codes;
+
+    public ThreadAverage(List<String> codes) {
+        this.codes = codes;
+    }
 
     @Override
     public void run() {
