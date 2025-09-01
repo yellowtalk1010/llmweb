33f2cbd04cd301d89a10f5f17791641e103f62e4
qq5132834 <513283439@qq.com>
2025-09-01 17:17:05 +0800

    STOCK

---
+++ web/src/main/java/zuk/sast/rules/controller/stock/analysis/DownloadStockDayThread.java
@@ -0,0 +1,61 @@
+package zuk.sast.rules.controller.stock.analysis;
+
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import zuk.sast.rules.utils.HttpClientUtil;
+
+import java.io.File;
+import java.util.List;
+
+@Slf4j
+public class DownloadStockDayThread implements Runnable{
+
+    @Override
+    public void run() {
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+        //String ym = sdf.format(new Date());
+        final String ym = "202508";
+        final int total = 21;
+        String startTime = "2025-08-01";
+        String endTime = "2025-08-31";
+        LoaderStockData.STOCKS.stream().forEach(stockApiVO -> {
+            String path = LoaderStockData.STOCK_DAY + File.separator + stockApiVO.getApi_code() + File.separator + ym + ".jsonl";
+            try {
+                if(new File(path).exists()){
+                    List<String> lines = FileUtils.readLines(new File(path), "UTF-8");
+                    if(lines.size()!=total){
+                        FileUtils.delete(new File(path));
+                        log.info(path + "存在，但是数据总行数不对");
+                    }
+                    else {
+//                            log.info(path + "相同");
+                    }
+                }
+                else {
+                    //不存在
+                    String url = "https://stockapi.com.cn/v1/base/day?token=" + LoaderStockData.TOKEN + "&code="+stockApiVO.getApi_code()+"&startDate="+startTime+"&endDate="+endTime+"&calculationCycle=100";
+                    String response = HttpClientUtil.sendGetRequest(url);
+                    JSONArray jsonArray = (JSONArray) JSONObject.parseObject(response).get("data");
+                    List<String> lines = jsonArray.stream().map(e->{
+                        String line = JSONObject.toJSONString(e, JSONWriter.Feature.LargeObject);
+                        return line;
+                    }).toList();
+                    if(lines.size()==total){
+                        FileUtils.writeLines(new File(path), lines);
+                        log.info(path + "， 下载成功");
+                    }
+                    else {
+                        log.info(path + "， 下载失败");
+                    }
+                }
+            }catch (Exception e) {
+                e.printStackTrace();
+                log.error(e.getMessage());
+                log.info(path + "， 失败");
+            }
+        });
+    }
+}
--- web/src/main/java/zuk/sast/rules/controller/stock/analysis/LoaderStockData.java
+++ web/src/main/java/zuk/sast/rules/controller/stock/analysis/LoaderStockData.java
@@ -21,12 +21,12 @@ import java.util.concurrent.Executors;
 public class LoaderStockData implements InitializingBean {
 
     public static final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
-    private static final String TOKEN = "36c92182f783f08005017f78e7a264608a82952f8b91de2a";
+    public static final String TOKEN = "36c92182f783f08005017f78e7a264608a82952f8b91de2a";
 
-    private static final String STOCK_DATA_DIR_PATH = "D:\\AAAAAAAAAAAAAAAAAAAA\\github\\llmweb1\\web\\stocks\\";
-    private static final String STOCK_TOKEN = STOCK_DATA_DIR_PATH + File.separator + "token";
-    private static final String STOCK_ALL = STOCK_DATA_DIR_PATH + File.separator + "all_stock.json";
-    private static final String STOCK_DAY = STOCK_DATA_DIR_PATH + File.separator + "days";
+    public static final String STOCK_DATA_DIR_PATH = "D:\\AAAAAAAAAAAAAAAAAAAA\\github\\llmweb1\\web\\stocks\\";
+    public static final String STOCK_TOKEN = STOCK_DATA_DIR_PATH + File.separator + "token";
+    public static final String STOCK_ALL = STOCK_DATA_DIR_PATH + File.separator + "all_stock.json";
+    public static final String STOCK_DAY = STOCK_DATA_DIR_PATH + File.separator + "days";
 
 
 
@@ -63,59 +63,6 @@ public class LoaderStockData implements InitializingBean {
             System.exit(0);
         }
     }
-
-    /***
-     * 获取历史日线数据，按月统计
-     */
-    public static class DownloadStockDayThread implements Runnable {
-
-        @Override
-        public void run() {
-            //SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
-            //String ym = sdf.format(new Date());
-            final String ym = "202508";
-            final int total = 21;
-            String startTime = "2025-08-01";
-            String endTime = "2025-08-31";
-            STOCKS.stream().forEach(stockApiVO -> {
-                String path = STOCK_DAY + File.separator + stockApiVO.getApi_code() + File.separator + ym + ".jsonl";
-                try {
-                    if(new File(path).exists()){
-                        List<String> lines = FileUtils.readLines(new File(path), "UTF-8");
-                        if(lines.size()!=total){
-                            FileUtils.delete(new File(path));
-                            log.info(path + "存在，但是数据总行数不对");
-                        }
-                        else {
-//                            log.info(path + "相同");
-                        }
-                    }
-                    else {
-                        //不存在
-                        String url = "https://stockapi.com.cn/v1/base/day?token=" + TOKEN + "&code="+stockApiVO.getApi_code()+"&startDate="+startTime+"&endDate="+endTime+"&calculationCycle=100";
-                        String response = HttpClientUtil.sendGetRequest(url);
-                        JSONArray jsonArray = (JSONArray) JSONObject.parseObject(response).get("data");
-                        List<String> lines = jsonArray.stream().map(e->{
-                            String line = JSONObject.toJSONString(e, JSONWriter.Feature.LargeObject);
-                            return line;
-                        }).toList();
-                        if(lines.size()==total){
-                            FileUtils.writeLines(new File(path), lines);
-                            log.info(path + "， 下载成功");
-                        }
-                        else {
-                            log.info(path + "， 下载失败");
-                        }
-                    }
-                }catch (Exception e) {
-                    e.printStackTrace();
-                    log.error(e.getMessage());
-                    log.info(path + "， 失败");
-                }
-            });
-        }
-    }
-
 
 
     @Data
