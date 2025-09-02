4afc406e9f45b3cfefeee5e895a95dbab2aab60b
zuk <huangliao1010@gmail.com>
2025-09-02 22:56:44 +0800

    stock

--- web/frontend/sast/src/pages/stock/BiddingStock.js
+++ web/frontend/sast/src/pages/stock/BiddingStock.js
@@ -2,13 +2,51 @@ import { useState, useEffect } from "react";
 import "./StockTable.css"
 
 function BiddingStock() {
+
+    const [period, setPeriod] = useState("1");
+    const [type, setType] = useState("1");
+
+    const [stockDatas, setStockDatas] = useState({
+        code: "",
+        msg: "",
+        data: []
+    });
+
+    const handleSubmit = () => {
+        const period = document.getElementById("period").value
+        const type = document.getElementById("type").value
+        console.info(period + ", " + type)
+        fetch("/stock/bidding?period="+period+"&type="+type, {
+                method: "GET",
+                headers: { "Content-Type": "application/json" }
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log("后端返回:", data);
+                setStockDatas(data)
+            })
+            .catch(err => console.error("请求失败:", err));
+    };
+
     
     return (
-        <>
-            <div>
-                竞价
+        <div className="container">
+            {/* 查询表单 */}
+            <div className="form">
+                <select value={period} id="period" >
+                    <option value={0}>竞价抢筹</option>
+                    <option value={1}>尾盘抢筹</option>
+                </select>
+                <select value={type} id="type">
+                    <option value={1}>委托金额排序</option>
+                    <option value={2}>成交金额排序</option>
+                    <option value={3}>开盘金额顺序</option>
+                    <option value={4}>抢筹涨幅排序</option>
+                </select>
+                <button onClick={handleSubmit}>查询</button>
             </div>
-        </>
+ 
+        </div>
     );
 }
 
--- web/src/main/java/zuk/sast/rules/controller/stock/AllStockController.java
+++ web/src/main/java/zuk/sast/rules/controller/stock/AllStockController.java
@@ -1,5 +1,6 @@
 package zuk.sast.rules.controller.stock;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.beust.jcommander.internal.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import zuk.sast.rules.controller.mapper.StockMapper;
 import zuk.sast.rules.controller.mapper.entity.StockEntity;
 
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
 import zuk.sast.rules.controller.stock.analysis.LoaderStockData;
+import zuk.sast.rules.utils.HttpClientUtil;
 
 /***
  *
@@ -159,6 +162,33 @@ public class AllStockController {
         map.put("stocks", list);
         map.put("blocks",tags);
         return map;
+    }
+
+
+    /***
+     * 竞价强筹
+     * @param period 时期，抢筹类型，0-竞价抢筹，1-尾盘抢筹
+     * @param type 1委托金额排序  2成交金额排序  3开盘金额排序 4涨幅排序
+     * @return
+     *
+     */
+    @GetMapping("bidding")
+    public synchronized JSONObject bidding(String period, String type) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String tradeDate = sdf.format(new Date());
+        String url = "https://stockapi.com.cn/v1/base/jjqc?period=" + period + "&type=" + type + "&tradeDate=" + tradeDate;
+        try {
+            String string = HttpClientUtil.sendGetRequest(url);
+            JSONObject jsonObject = JSONObject.parseObject(string);
+            return jsonObject;
+        }catch (Exception e){
+            e.printStackTrace();
+            log.error(e.getMessage());
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("code", 500);
+            jsonObject.put("msg", e.getMessage());
+            return jsonObject;
+        }
     }
 
 }
