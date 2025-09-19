29c6149816d8d9244312ffc4bfe1bafdbab5a977
qq5132834 <513283439@qq.com>
2025-09-19 14:53:05 +0800

    STOCK

--- web/stockapi/src/main/scala/zuk/stockapi/model/MA1_Model.scala
+++ web/stockapi/src/main/scala/zuk/stockapi/model/MA1_Model.scala
@@ -14,7 +14,7 @@ class MA1_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(st
   override def isHit(): Boolean = this.isOK
 
   override def run(): Unit = {
-    val list = maList.slice(0,3)
+    val list = maList.slice(0,5)
     if(comp(list(0))){
       val filterList = list.filter(comp(_))
       isOK = list.size>filterList.size
