4089811895e13b7cc14de459323eb3dc7bcfb336
zuk <huangliao1010@gmail.com>
2025-09-19 21:42:39 +0800

    commit

--- web/stockapi/src/main/scala/zuk/stockapi/DownloadDayStock.scala
+++ web/stockapi/src/main/scala/zuk/stockapi/DownloadDayStock.scala
@@ -35,51 +35,59 @@ object DownloadDayStock {
         val url = "https://stockapi.com.cn/v1/base/day?token=" + LoaderLocalStockData.TOKEN + "&code=" + stockApiVO.getApi_code + "&startDate=" + startTime + "&endDate=" + endTime + "&calculationCycle=100"
         try {
           //不存在
-          val response = HttpClientUtil.sendGetRequest(url)
-          val jsonObject = JSONObject.parseObject(response)
-          val resCode = jsonObject.get("code").asInstanceOf[Integer]
-          val resMsg = jsonObject.get("msg").asInstanceOf[String]
-          if (resCode.toString.equals("20000") && resMsg == "success") {
-            val jsonArray = JSONObject.parseObject(response).get("data").asInstanceOf[JSONArray]
-            val lines = jsonArray.asScala.map((e: AnyRef) => {
-              val line = JSONObject.toJSONString(e, JSONWriter.Feature.LargeObject)
-              val stockDayVo = JSONObject.parseObject(line, classOf[StockDayVo])
-              val newLine = if (StringUtils.isNotEmpty(stockDayVo.getOpen) //开盘价
-                && StringUtils.isNotEmpty(stockDayVo.getTime)//交易时间
-                && StringUtils.isNotEmpty(stockDayVo.getCode)//代码
-                && StringUtils.isNotEmpty(stockDayVo.getAmount)//交易总金额
-                && StringUtils.isNotEmpty(stockDayVo.getChangeRatio)//相比上次收盘价的涨跌比率
-                && StringUtils.isNotEmpty(stockDayVo.getHigh)//最高价
-                && StringUtils.isNotEmpty(stockDayVo.getLow)//最低价
-                && StringUtils.isNotEmpty(stockDayVo.getTurnoverRatio)//换手率
-                && StringUtils.isNotEmpty(stockDayVo.getVolume)//交易量
-                && StringUtils.isNotEmpty(stockDayVo.getClose) //收盘
-              ) {
-                JSONObject.toJSONString(stockDayVo, JSONWriter.Feature.LargeObject)
-              }
-              else {
-                println(line + "， 数据为空")
-                ""
-              }
-              newLine
-            }).toList
-            FileUtils.writeLines(new File(path), lines.filter(l=>StringUtils.isNotEmpty(l)).asJava)
-            println(stockApiVO.getApi_code + "，行数：" + lines.size + "，" + "成功，" + num.get + "/" + LoaderLocalStockData.STOCKS.size + "， " + startTime + "至" + endTime)
-            if (lines.size == 0) {
-              println(stockApiVO.getApi_code + "， 下载数据为空。（可能停牌）")
+          var response:String = null
+          var isBreak = false
+          val looptime = new AtomicInteger(0) //循环次数
+          while (!isBreak){
+            response = HttpClientUtil.sendGetRequest(url)
+            val jsonObject = JSONObject.parseObject(response)
+            val resCode = jsonObject.get("code").asInstanceOf[Integer]
+            val resMsg = jsonObject.get("msg").asInstanceOf[String]
+            if (resCode.toString.equals("20000") && resMsg == "success") {
+              isBreak = true
             }
-        }
-        else
-        {
-          //failStocks.add(stockApiVO.getApi_code)
-          println(stockApiVO.getApi_code + "，返回数据异常" + response)
-        }
-      } catch {
-        case e: Exception =>
-          //failStocks.add(stockApiVO.getApi_code)
-          e.printStackTrace()
-          println(url + "\n" + path + "\n失败")
-      } finally {
+            else {
+              println(s"数据采集异常（${looptime.incrementAndGet()}次）：${url}")
+            }
+          }
+
+          val jsonArray = JSONObject.parseObject(response).get("data").asInstanceOf[JSONArray]
+          val lines = jsonArray.asScala.map(e => {
+            val line = JSONObject.toJSONString(e, JSONWriter.Feature.LargeObject)
+            val stockDayVo = JSONObject.parseObject(line, classOf[StockDayVo])
+            val newLine = if (StringUtils.isNotEmpty(stockDayVo.getOpen) //开盘价
+              && StringUtils.isNotEmpty(stockDayVo.getTime)//交易时间
+              && StringUtils.isNotEmpty(stockDayVo.getCode)//代码
+              && StringUtils.isNotEmpty(stockDayVo.getAmount)//交易总金额
+              && StringUtils.isNotEmpty(stockDayVo.getChangeRatio)//相比上次收盘价的涨跌比率
+              && StringUtils.isNotEmpty(stockDayVo.getHigh)//最高价
+              && StringUtils.isNotEmpty(stockDayVo.getLow)//最低价
+              && StringUtils.isNotEmpty(stockDayVo.getTurnoverRatio)//换手率
+              && StringUtils.isNotEmpty(stockDayVo.getVolume)//交易量
+              && StringUtils.isNotEmpty(stockDayVo.getClose) //收盘
+            ) {
+              JSONObject.toJSONString(stockDayVo, JSONWriter.Feature.LargeObject)
+            }
+            else {
+              println(s"${stockApiVO.getApi_code}，${stockApiVO.getName}，数据为空，可能停牌，${line}")
+              ""
+            }
+            newLine
+          }).filter(l=>StringUtils.isNotEmpty(l)).toList
+
+          FileUtils.writeLines(new File(path), lines.asJava)
+
+          println(stockApiVO.getApi_code + "，行数：" + lines.size + "，" + "成功，" + num.get + "/" + LoaderLocalStockData.STOCKS.size + "， " + startTime + "至" + endTime)
+          if (lines.size == 0) {
+            println(stockApiVO.getApi_code + "， 下载数据为空。（可能停牌很久）")
+          }
+
+        } catch {
+          case e: Exception =>
+            //failStocks.add(stockApiVO.getApi_code)
+            e.printStackTrace()
+            println(url + "\n" + path + "\n失败")
+        } finally {
           num.incrementAndGet
         }
       })
