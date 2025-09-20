3429f650e5489efef35f11d65d21d9e3d0a7f9af
zuk <huangliao1010@gmail.com>
2025-09-20 12:27:48 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/stockapi/utils/CloseIncreaseUtil.scala
@@ -0,0 +1,24 @@
+package zuk.stockapi.utils
+
+import zuk.stockapi.StockMaVo
+import java.math.BigDecimal
+
+object CloseIncreaseUtil {
+
+  /**
+   * 收盘价是否递增
+   */
+  def comIncrea(list: List[StockMaVo]): Boolean = {
+    var st = true
+    for (i <- 0 until list.size if st) {
+      val e0 = list(i)
+      val ls = list.splitAt(i)._2
+      val filter = ls.filter(e => {
+        new BigDecimal(e0.getStockDayVo.getClose).compareTo(new BigDecimal(e.getStockDayVo.getClose)) >= 0
+      })
+      st = ls.size == filter.size
+    }
+    st
+  }
+
+}
