089419f5ae2d707b622c3169c90916369b3dd657
zuk <huangliao1010@gmail.com>
2025-09-20 11:58:59 +0800

    commit

--- web/stockapi/src/main/scala/zuk/stockapi/model/MA1_Model.scala
+++ web/stockapi/src/main/scala/zuk/stockapi/model/MA1_Model.scala
@@ -14,14 +14,13 @@ class MA1_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(st
   override def isHit(): Boolean = this.isOK
 
   override def run(): Unit = {
-    val len = 10
+    val len = 3
     if(maList.size > len){
       val list = maList.slice(0, len)
       if (comp(list(0))) {
         val filterList = list.filter(comp(_))
         isOK = list.size != filterList.size
       }
-      isOK = false
     }
   }
 
