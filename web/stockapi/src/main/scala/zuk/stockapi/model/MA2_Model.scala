e894d49b1ed298abaa625531852c5251f561fc06
zuk <huangliao1010@gmail.com>
2025-09-20 12:02:19 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/stockapi/model/MA2_Model.scala
@@ -0,0 +1,36 @@
+package zuk.stockapi.model
+
+import zuk.stockapi.{StockApiVo, StockMaVo}
+
+import java.math.BigDecimal
+
+/***
+ * 刚刚M5>M20>M30 金叉
+ */
+class MA2_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(stockMaVo) {
+
+  var isOK: Boolean = false
+
+  override def isHit(): Boolean = this.isOK
+
+  override def run(): Unit = {
+    val len = 3
+    if(maList.size > len){
+      val list = maList.slice(0, len)
+      if (comp(list(0))) {
+        val filterList = list.filter(comp(_))
+        isOK = list.size != filterList.size
+      }
+    }
+  }
+
+  private def comp(v0: StockMaVo): Boolean = {
+    val ma5 = new BigDecimal(v0.getMa5)
+    val ma10 = new BigDecimal(v0.getMa10)
+    val ma20 = new BigDecimal(v0.getMa20)
+    val ma30 = new BigDecimal(v0.getMa30)
+    ma5.compareTo(ma20) > 0
+      && ma20.compareTo(ma30) > 0
+  }
+
+}
