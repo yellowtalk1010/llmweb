06377538d5205f6189f3e2f2f0f1a7c2b184e193
zuk <huangliao1010@gmail.com>
2025-09-22 18:41:57 +0800

    commit

--- web/stockapi/src/main/java/zuk/stockapi/utils/HttpClientUtil.java
+++ web/stockapi/src/main/java/zuk/stockapi/utils/HttpClientUtil.java
@@ -26,7 +26,9 @@ public class HttpClientUtil {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
                 .header("Content-Type", "application/json")
-                .header("User-Agent", "Java HttpClient")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36")
+                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+//                .header("host", "www.stockapi.com.cn")
                 .GET()
                 .build();
 
