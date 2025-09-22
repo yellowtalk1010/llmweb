b6337e2a2f5a94d974bffc06c29eb76284d7c2da
zuk <huangliao1010@gmail.com>
2025-09-22 21:38:44 +0800

    commit

--- web/stockapi/src/main/scala/zuk/stockapi/DownloadMinuteStock.scala
+++ web/stockapi/src/main/scala/zuk/stockapi/DownloadMinuteStock.scala
@@ -1,12 +1,17 @@
 package zuk.stockapi
 
+import java.util.concurrent.atomic.AtomicInteger
+
 /***
  * 获取交易日分钟信息，在2点45分时执行
  */
 object DownloadMinuteStock {
 
-  def run() = {
+  def run(stockList: List[StockApiVo]) = {
+    val num = new AtomicInteger(0)
+    while (stockList.size != num.get()) {
 
+    }
   }
 
 }
