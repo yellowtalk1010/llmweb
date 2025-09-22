2d02b83333b94f3d863b2d6aa08e3378d07cf0e6
zuk <huangliao1010@gmail.com>
2025-09-22 20:44:22 +0800

    commit

--- web/stockapi/src/main/scala/zuk/stockapi/model/MA2_Model.scala
+++ web/stockapi/src/main/scala/zuk/stockapi/model/MA2_Model.scala
@@ -4,7 +4,6 @@ import zuk.stockapi.utils.CloseIncreaseUtil
 import zuk.stockapi.{StockApiVo, StockMaVo}
 
 import java.math.BigDecimal
-import scala.collection.mutable.ListBuffer
 
 /***
  * 刚刚M5>M20>M30 金叉
@@ -19,8 +18,13 @@ class MA2_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(st
     val len = 3
     if(maList.size > len){
       val list = maList.slice(0, len)
-      val isIncre = CloseIncreaseUtil.comIncrea(list)
-      if (comp(list(0)) && isIncre) {
+      val today = list.head
+      val isIncre = CloseIncreaseUtil.comIncrea(list) //3个交易日递增
+      if (comp(today) //今日MA5>MA20>MA30
+        && isIncre //最近3天收盘价递增
+        && new BigDecimal(today.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(4.5/100)) > 0 //换手率要大于4.5%
+        && new BigDecimal(today.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(2/100)) > 0 //涨幅大于3%
+      ) {
         val filterList = list.filter(comp(_))
         isOK = list.size != filterList.size
       }
