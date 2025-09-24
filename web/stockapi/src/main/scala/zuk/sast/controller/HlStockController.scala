81f73f3279a3d2474bad31a34a19d7a2997307a0
zuk <huangliao1010@gmail.com>
2025-09-24 16:52:01 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/sast/controller/HlStockController.scala
@@ -0,0 +1,24 @@
+package zuk.sast.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import java.util
+
+/***
+ * 使用scala开发controller接口
+ */
+@RestController
+@RequestMapping(value=Array("hlStock"))
+class HlStockController {
+
+  @GetMapping(value=Array("list"))
+  def all(search: String): util.Map[String, String] = {
+    val map = new util.HashMap[String, String]()
+    map.put("code", "success")
+    map.put("time", s"${System.currentTimeMillis()}")
+    map
+  }
+
+}
