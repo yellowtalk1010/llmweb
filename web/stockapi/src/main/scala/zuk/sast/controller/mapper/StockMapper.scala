693addac27bc20e3698a793e21931b4fc5770603
zuk <huangliao1010@gmail.com>
2025-09-24 19:01:24 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/sast/controller/mapper/StockMapper.scala
@@ -0,0 +1,22 @@
+package zuk.sast.controller.mapper
+
+import org.apache.ibatis.annotations._
+import zuk.sast.controller.mapper.entity.{StockEntity}
+import java.util
+
+@Mapper
+trait StockMapper {
+
+  @Select(Array("SELECT * FROM stock"))
+  def selectAll(): util.List[StockEntity]
+
+  @Insert(Array("INSERT INTO stock(id, code, jys, name) VALUES (#{id}, #{code}, #{jys}, #{name})"))
+  def insert(stock: StockEntity): Int
+
+  @Delete(Array("DELETE FROM stock WHERE code = #{code}"))
+  def deleteByCode(@Param("code") code: String): Int
+
+  @Select(Array("SELECT * FROM stock WHERE code = #{code}"))
+  def selectByCode(@Param("code") code: String): util.List[String]
+
+}
---
+++ web/stockapi/src/main/scala/zuk/sast/controller/mapper/entity/StockEntity.scala
@@ -0,0 +1,12 @@
+package zuk.sast.controller.mapper.entity
+
+import scala.beans.BeanProperty
+
+class StockEntity {
+
+  @BeanProperty var id: String = null
+  @BeanProperty var code: String = null
+  @BeanProperty var jys: String = null
+  @BeanProperty var name: String = null
+
+}
