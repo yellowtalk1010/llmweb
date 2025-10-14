f46676e97295b88e0adc7b4232ce4ee562d2855b
zuk <huangliao1010@gmail.com>
2025-10-14 18:26:12 +0800

    commit

--- web/stockapi/src/main/scala/zuk/stockapi/model/MA55_Model.scala
+++ web/stockapi/src/main/scala/zuk/stockapi/model/MA55_Model.scala
@@ -8,7 +8,6 @@ import java.math.BigDecimal
 /***
  * 预测
  */
-//class MA5_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(stockMaVo) {
 class MA55_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(stockMaVo) {
 
   private var isOK: Boolean = false
@@ -16,12 +15,12 @@ class MA55_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(s
   override def isHit(): Boolean = isOK
 
   override def run(): Unit = {
-    if(maList.size>=3){
+    if (maList.size >= 3) {
       val list = maList.take(3)
       val day0 = list(0)
       val day1 = list(1)
       val day2 = list(2)
-      if(new BigDecimal(day0.getMa5).compareTo(new BigDecimal(day1.getMa5)) > 0
+      if (new BigDecimal(day0.getMa5).compareTo(new BigDecimal(day1.getMa5)) > 0
         && new BigDecimal(day1.getMa5).compareTo(new BigDecimal(day2.getMa5)) > 0 //递增
 
         && new BigDecimal(day0.getMa10).compareTo(new BigDecimal(day1.getMa10)) < 0
@@ -29,22 +28,24 @@ class MA55_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(s
 
         && new BigDecimal(day0.getMa10).compareTo(new BigDecimal(day0.getMa5)) > 0
         && new BigDecimal(day1.getMa10).compareTo(new BigDecimal(day1.getMa5)) > 0
-        && new BigDecimal(day2.getMa10).compareTo(new BigDecimal(day2.getMa5)) > 0 //
+        //        && new BigDecimal(day2.getMa10).compareTo(new BigDecimal(day2.getMa5)) > 0 //
 
-        && new BigDecimal(day0.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(4)) >=0
-        && new BigDecimal(day1.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(4)) >=0 //huan shou
+        && new BigDecimal(day0.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(4)) >= 0
+        && new BigDecimal(day1.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(4)) >= 0 //huan shou
+        //
+        && new BigDecimal(day0.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(1)) >= 0
+        && new BigDecimal(day1.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(1)) >= 0 //zhang fu
+      ) {
 
-        && new BigDecimal(day0.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(3)) >=0
-        && new BigDecimal(day1.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(3)) >=0   //zhang fu
+        if (yuce(maList)) {
+          this.isOK = true
+        }
 
-        && yuce(maList)
-      ){
-        isOK = true
       }
     }
 
-
   }
+
 
 
   /***
@@ -55,7 +56,7 @@ class MA55_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(s
   private def yuce(maList: List[StockMaVo]): Boolean = {
     val lastDay = maList.head
     val close = lastDay.getStockDayVo.getClose
-    val ycj = new BigDecimal(close).multiply(new BigDecimal("1.3"))
+    val ycj = (new BigDecimal(close).multiply(new BigDecimal("0.03"))).add(new BigDecimal(close))
 
     val m5list = List(ycj) ++ maList.take(4).map(e=>{
       new BigDecimal(e.getStockDayVo.getClose)
@@ -70,7 +71,7 @@ class MA55_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(s
 
     val new10 = m10list.reduceOption[BigDecimal]((a,b)=>{
       a.add(b)
-    }).get.divide(new BigDecimal(5), 5, math.BigDecimal.ROUND_HALF_DOWN)
+    }).get.divide(new BigDecimal(10), 5, math.BigDecimal.ROUND_HALF_DOWN)
 
     val st = new5.compareTo(new10) > 0
 
