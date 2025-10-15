050705357c9f6dffe3625af86c830ca00c7664f1
zuk <huangliao1010@gmail.com>
2025-10-15 18:56:50 +0800

    commit

--- web/stockapi/src/main/scala/zuk/stockapi/model/MA55_Model.scala
+++ web/stockapi/src/main/scala/zuk/stockapi/model/MA55_Model.scala
@@ -56,7 +56,7 @@ class MA55_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(s
   private def yuce(maList: List[StockMaVo]): Boolean = {
     val lastDay = maList.head
     val close = lastDay.getStockDayVo.getClose
-    val ycj = (new BigDecimal(close).multiply(new BigDecimal("0.02"))).add(new BigDecimal(close))
+    val ycj = (new BigDecimal(close).multiply(new BigDecimal("0.05"))).add(new BigDecimal(close))
 
     val m5list = List(ycj) ++ maList.take(4).map(e=>{
       new BigDecimal(e.getStockDayVo.getClose)
