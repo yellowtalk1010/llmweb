37f7efa57ad2e9c21b408a38fd8583a142c1247a
qq5132834 <513283439@qq.com>
2025-10-23 10:54:44 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/jiucai/Window.scala
@@ -0,0 +1,9 @@
+package zuk.jiucai
+
+import scala.beans.BeanProperty
+
+
+class Window() {
+  @BeanProperty val x = 337
+  @BeanProperty val y = 973
+}
--- web/stockapi/src/test/scala/zuk/jiucai/test/JiucaiTest.scala
+++ web/stockapi/src/test/scala/zuk/jiucai/test/JiucaiTest.scala
@@ -5,7 +5,8 @@ import org.scalatest.funsuite.AnyFunSuite
 class JiucaiTest extends AnyFunSuite {
 
   test("config"){
-    
+    val list = List()
+    val c1 = zuk.jiucai.test.Config.createConfigJsonFile(list)
   }
 
 }
