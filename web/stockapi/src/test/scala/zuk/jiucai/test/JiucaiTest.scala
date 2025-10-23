b6cc58c703d5970759ed902ed76d5e2959dc08c0
zuk <huangliao1010@gmail.com>
2025-10-23 20:23:00 +0800

    cbapi

--- web/stockapi/src/test/scala/zuk/jiucai/test/JiucaiTest.scala
+++ web/stockapi/src/test/scala/zuk/jiucai/test/JiucaiTest.scala
@@ -1,12 +1,38 @@
 package zuk.jiucai.test
 
+import com.alibaba.fastjson2.JSONObject
+import com.alibaba.fastjson2.JSONWriter.Feature
+import org.apache.commons.lang3.StringUtils
 import org.scalatest.funsuite.AnyFunSuite
+import zuk.stockapi.LoaderLocalStockData
+
+import scala.collection.mutable.ListBuffer
+import scala.jdk.CollectionConverters.*
 
 class JiucaiTest extends AnyFunSuite {
 
+  val codes = List("920300")
+
   test("config"){
-    val list = List()
-    val c1 = zuk.jiucai.test.Config.createConfigJsonFile(list)
+    LoaderLocalStockData.loadToken()
+    val stocks = LoaderLocalStockData.STOCKS
+    stocks.asScala.map(_.getJys).toSet.foreach(println)
+    val list = stocks.asScala.filter(e=>{
+      codes.contains(e.getApi_code)
+    }).map(e=>{
+      var code = ""
+      if(e.getJys.equals("SZ")){
+        code = s"0.${e.getApi_code}"
+      }
+      else if(e.getJys.equals("SH")) {
+        code = s"1.${e.getApi_code}"
+      }
+      (code, e.getName)
+    }).filter((c,n)=>StringUtils.isNotBlank(c))
+
+    val c1 = zuk.jiucai.test.Config.createConfigJsonFile(list.toList)
+    val json = JSONObject.toJSONString(c1, Feature.PrettyFormat)
+    println(json)
   }
 
 }
