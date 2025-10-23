13b237e66e34606b98b3d2a1b2ad8d32aa21e568
qq5132834 <513283439@qq.com>
2025-10-23 10:45:53 +0800

    commit

---
+++ web/stockapi/src/test/scala/zuk/jiucai/test/Config.scala
@@ -0,0 +1,48 @@
+package zuk.jiucai.test
+
+import scala.beans.BeanProperty
+import scala.jdk.CollectionConverters.*
+
+object Config {
+
+  def createConfigJsonFile(code_names: List[(String, String)]): Config = {
+    val config = new Config
+    code_names.map(e=>{
+      val stock = new Stock
+      stock.stock_code = e._1
+      stock.stock_name = e._2
+      stock
+    }).foreach(config.stocks.add)
+    config
+  }
+
+}
+
+class Config {
+  @BeanProperty val log_level = "INFO"
+  @BeanProperty val window = new Window
+  @BeanProperty val text_opacity = 168
+  @BeanProperty val rest_reminder = new RestReminder
+  @BeanProperty val profit_reminder_enabled = false
+  @BeanProperty val show_profit_info = false
+  @BeanProperty val show_stock_code = true
+  @BeanProperty val stocks = List[Stock]().asJava
+}
+
+class Stock {
+  @BeanProperty var stock_code = ""
+  @BeanProperty val buy_count = 0
+  @BeanProperty val buy_price = 0.0
+  @BeanProperty var stock_name = ""
+  @BeanProperty val stock_name_alias = ""
+}
+
+class Window {
+  @BeanProperty val x = 337
+  @BeanProperty val y = 973
+}
+
+class RestReminder {
+  @BeanProperty val enabled = false
+  @BeanProperty val hours = List(10, 11, 14, 16).asJava.toArray
+}
---
+++ web/stockapi/src/test/scala/zuk/jiucai/test/JiucaiTest.scala
@@ -0,0 +1,11 @@
+package zuk.jiucai.test
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class JiucaiTest extends AnyFunSuite {
+
+  test("config"){
+    
+  }
+
+}
