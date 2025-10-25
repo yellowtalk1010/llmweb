9d419663aff2d1297f7c771ae497950575d18167
zuk <huangliao1010@gmail.com>
2025-10-25 19:11:49 +0800

    cbapi

--- web/stockapi/src/test/scala/zuk/stock/test/StockModelTest.scala
+++ web/stockapi/src/test/scala/zuk/stock/test/StockModelTest.scala
@@ -1,116 +1,116 @@
-package zuk.stock.test
-
-import org.apache.commons.io.FileUtils
-import org.scalatest.funsuite.AnyFunSuite
-import zuk.stockapi.model.{AVG_Model, MA1_Model, MA2_Model, MA_Model}
-import zuk.stockapi.{CalculateMAForDay, LoaderLocalStockData, StockApiVo}
-
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.Date
-import scala.collection.mutable.ListBuffer
-import scala.jdk.CollectionConverters.*
-
-class StockModelTest extends AnyFunSuite {
-
-  test("模型策略") {
-    LoaderLocalStockData.loadToken()
-    val codes = LoaderLocalStockData.STOCKS.asScala.filter(e=>{
-      e.getApi_code.equals("600641")
-        || true
-    }).toList
-
-    val tpList = CalculateMAForDay.run(codes)
-
-    val maModelList = ListBuffer[StockApiVo]()
-    val ma1ModelList = ListBuffer[StockApiVo]()
-    val ma2ModelList = ListBuffer[StockApiVo]()
-    val avgModelList = ListBuffer[StockApiVo]()
-
-    tpList.filter(_._2.size>0).foreach(tp=>{
-      val stock = tp._1
-      val malist = tp._2
-
-      //转m5>m20
-      val ma2Model = new MA2_Model(stock, malist)
-      ma2Model.run()
-      if (ma2Model.isHit()) {
-        ma2ModelList += stock
-      }
-
-      //转m5>m10>m20>m30
-      val ma1Model = new MA1_Model(stock, malist)
-      ma1Model.run()
-      if (ma1Model.isHit()) {
-        ma1ModelList += stock
-      }
-
-      //m5>m10>m20>m30
-      val maModel = new MA_Model(stock, malist)
-      maModel.run()
-      if (maModel.isHit()) {
-        maModelList += stock
-      }
-
-      //AVG递增策略
-      val avgModel = new AVG_Model(stock, malist)
-      avgModel.run()
-      if (avgModel.isHit()) {
-        avgModelList += stock
-      }
-
-    })
-
-    val lines = ListBuffer[String]()
-
-    //
-    println("ma2模型策略")
-    lines += "ma2模型策略"
-    ma2ModelList.foreach(e => {
-      lines += e.getApi_code
-      println(s"${e.getApi_code}")
-    })
-
-    //
-    println("ma1模型策略")
-    lines += "ma1模型策略"
-    ma1ModelList.foreach(e => {
-      lines += e.getApi_code
-      println(s"${e.getApi_code}")
-    })
-
-    //
-    println(s"ma模型策略: ${maModelList.size}")
-    lines += s"ma模型策略: ${maModelList.size}"
-    maModelList.foreach(e=>{
-      lines += e.getApi_code
-      println(s"${e.getApi_code}")
-    })
-
-    //
-    println("avg模型策略")
-    lines += "avg模型策略"
-    avgModelList.foreach(e=>{
-      lines += e.getApi_code
-      println(s"${e.getApi_code}")
-    })
-
-    //
-    println("模型策略交集")
-    lines += "模型策略交集"
-    (maModelList ++ ma1ModelList ++ avgModelList)
-      .groupBy(_.getApi_code)
-      .filter(_._2.size>1)
-      .map(_._1)
-      .foreach(code=>{
-        lines += code
-        println(code)
-      })
-
-
-    FileUtils.writeLines(new File(s"stockapi/model_result/策略结果-${new SimpleDateFormat("yyyyMMdd").format(new Date())}.txt"), "UTF-8", lines.asJava)
-
-
-  }
-
-}
+//package zuk.stock.test
+//
+//import org.apache.commons.io.FileUtils
+//import org.scalatest.funsuite.AnyFunSuite
+//import zuk.stockapi.model.{AVG_Model, MA1_Model, MA2_Model, MA_Model}
+//import zuk.stockapi.{CalculateMAForDay, LoaderLocalStockData, StockApiVo}
+//
+//import java.io.File
+//import java.text.SimpleDateFormat
+//import java.util.Date
+//import scala.collection.mutable.ListBuffer
+//import scala.jdk.CollectionConverters.*
+//
+//class StockModelTest extends AnyFunSuite {
+//
+//  test("模型策略") {
+//    LoaderLocalStockData.loadToken()
+//    val codes = LoaderLocalStockData.STOCKS.asScala.filter(e=>{
+//      e.getApi_code.equals("600641")
+//        || true
+//    }).toList
+//
+//    val tpList = CalculateMAForDay.run(codes)
+//
+//    val maModelList = ListBuffer[StockApiVo]()
+//    val ma1ModelList = ListBuffer[StockApiVo]()
+//    val ma2ModelList = ListBuffer[StockApiVo]()
+//    val avgModelList = ListBuffer[StockApiVo]()
+//
+//    tpList.filter(_._2.size>0).foreach(tp=>{
+//      val stock = tp._1
+//      val malist = tp._2
+//
+//      //转m5>m20
+//      val ma2Model = new MA2_Model(stock, malist)
+//      ma2Model.run()
+//      if (ma2Model.isHit()) {
+//        ma2ModelList += stock
+//      }
+//
+//      //转m5>m10>m20>m30
+//      val ma1Model = new MA1_Model(stock, malist)
+//      ma1Model.run()
+//      if (ma1Model.isHit()) {
+//        ma1ModelList += stock
+//      }
+//
+//      //m5>m10>m20>m30
+//      val maModel = new MA_Model(stock, malist)
+//      maModel.run()
+//      if (maModel.isHit()) {
+//        maModelList += stock
+//      }
+//
+//      //AVG递增策略
+//      val avgModel = new AVG_Model(stock, malist)
+//      avgModel.run()
+//      if (avgModel.isHit()) {
+//        avgModelList += stock
+//      }
+//
+//    })
+//
+//    val lines = ListBuffer[String]()
+//
+//    //
+//    println("ma2模型策略")
+//    lines += "ma2模型策略"
+//    ma2ModelList.foreach(e => {
+//      lines += e.getApi_code
+//      println(s"${e.getApi_code}")
+//    })
+//
+//    //
+//    println("ma1模型策略")
+//    lines += "ma1模型策略"
+//    ma1ModelList.foreach(e => {
+//      lines += e.getApi_code
+//      println(s"${e.getApi_code}")
+//    })
+//
+//    //
+//    println(s"ma模型策略: ${maModelList.size}")
+//    lines += s"ma模型策略: ${maModelList.size}"
+//    maModelList.foreach(e=>{
+//      lines += e.getApi_code
+//      println(s"${e.getApi_code}")
+//    })
+//
+//    //
+//    println("avg模型策略")
+//    lines += "avg模型策略"
+//    avgModelList.foreach(e=>{
+//      lines += e.getApi_code
+//      println(s"${e.getApi_code}")
+//    })
+//
+//    //
+//    println("模型策略交集")
+//    lines += "模型策略交集"
+//    (maModelList ++ ma1ModelList ++ avgModelList)
+//      .groupBy(_.getApi_code)
+//      .filter(_._2.size>1)
+//      .map(_._1)
+//      .foreach(code=>{
+//        lines += code
+//        println(code)
+//      })
+//
+//
+//    FileUtils.writeLines(new File(s"stockapi/model_result/策略结果-${new SimpleDateFormat("yyyyMMdd").format(new Date())}.txt"), "UTF-8", lines.asJava)
+//
+//
+//  }
+//
+//}
