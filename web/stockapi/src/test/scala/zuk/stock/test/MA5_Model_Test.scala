e88dd62a5814791e4a22fae5a2e114932f52e880
zuk <huangliao1010@gmail.com>
2025-11-05 17:59:48 +0800

    cbapi

--- web/stockapi/src/test/scala/zuk/stock/test/MA5_Model_Test.scala
+++ web/stockapi/src/test/scala/zuk/stock/test/MA5_Model_Test.scala
@@ -44,68 +44,27 @@ class MA5_Model_Test extends AnyFunSuite {
 
 
 
-//  def run(stockMaVo: StockApiVo, maList: List[StockMaVo]): Boolean = {
-//    if(maList.size>=3){
-//      val list = maList.take(3)
-//      val day0 = list(0)
-//      val day1 = list(1)
-//      val day2 = list(2)
-//      if(new BigDecimal(day0.getMa5).compareTo(new BigDecimal(day1.getMa5)) > 0
-//        && new BigDecimal(day1.getMa5).compareTo(new BigDecimal(day2.getMa5)) > 0 //递增
-//
-//        && new BigDecimal(day0.getMa10).compareTo(new BigDecimal(day1.getMa10)) < 0
-//        && new BigDecimal(day1.getMa10).compareTo(new BigDecimal(day2.getMa10)) < 0 //递减
-//
-//        && new BigDecimal(day0.getMa10).compareTo(new BigDecimal(day0.getMa5)) > 0
-//        && new BigDecimal(day1.getMa10).compareTo(new BigDecimal(day1.getMa5)) > 0
-////        && new BigDecimal(day2.getMa10).compareTo(new BigDecimal(day2.getMa5)) > 0 //
-//
-//        && new BigDecimal(day0.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(4)) >=0
-//        && new BigDecimal(day1.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(4)) >=0 //huan shou
-////
-//        && new BigDecimal(day0.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(1)) >=0
-//        && new BigDecimal(day1.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(1)) >=0   //zhang fu
-//      ){
-//
-//        if(yuce(maList)){
-//          return true
-//        }
-//
-//      }
-//    }
-//
-//    false
-//  }
-//
-//
-//  /***
-//   * shang zhang 3 上升
-//   * @param maList
-//   * @return
-//   */
-//  private def yuce(maList: List[StockMaVo]): Boolean = {
-//    val lastDay = maList.head
-//    val close = lastDay.getStockDayVo.getClose
-//    val ycj = (new BigDecimal(close).multiply(new BigDecimal("0.03"))).add(new BigDecimal(close))
-//
-//    val m5list = List(ycj) ++ maList.take(4).map(e=>{
-//      new BigDecimal(e.getStockDayVo.getClose)
-//    })
-//    val new5 = m5list.reduceOption[BigDecimal]((a,b)=>{
-//      a.add(b)
-//    }).get.divide(new BigDecimal(5), 5, math.BigDecimal.ROUND_HALF_DOWN)
-//
-//    val m10list = List(ycj) ++ maList.take(9).map(e => {
-//      new BigDecimal(e.getStockDayVo.getClose)
-//    })
-//
-//    val new10 = m10list.reduceOption[BigDecimal]((a,b)=>{
-//      a.add(b)
-//    }).get.divide(new BigDecimal(10), 5, math.BigDecimal.ROUND_HALF_DOWN)
-//
-//    val st = new5.compareTo(new10) > 0
-//
-//    st
-//  }
+  test("主题"){
+    LoaderLocalStockData.loadToken()
+    val sdm = new SimpleDateFormat("yyyyMMdd")
+    val files = List(s"stockapi/model_result/MA3-${sdm.format(new Date)}.txt",
+      s"stockapi/model_result/MA4-${sdm.format(new Date)}.txt",
+      s"stockapi/model_result/MA5-${sdm.format(new Date)}.txt")
+    files.map(f=>new File(f)).filter(f=>f.exists()).flatMap(file=>{
+      val lines = FileUtils.readLines(file, "UTF-8")
+      lines.asScala.map(l=>{
+        l.split("，")(0)
+      })
+    }).map(code=>LoaderLocalStockData.STOCKS.asScala.filter(_.getApi_code.equals(code)).head)
+      .flatMap(stock=>stock.getGl.split(","))
+      .groupBy(e=>e)
+      .toList
+      .sortBy(_._2.size)
+      .reverse
+      .foreach(tp=>{
+        println(s"${tp._1}，${tp._2.size}")
+      })
+
+  }
 
 }
