b20c591c4fffb2db1a85986856508494c86e1657
qq5132834 <513283439@qq.com>
2025-11-17 14:57:29 +0800

    commit

--- web/continue/src/main/scala/zuk/continue/BinServer.scala
+++ web/continue/src/main/scala/zuk/continue/BinServer.scala
@@ -1,12 +1,20 @@
 package zuk.continue
 
-import java.io.{BufferedReader, File, InputStreamReader}
+import com.alibaba.fastjson2.{JSON, JSONObject}
+
+import java.io.{BufferedReader, BufferedWriter, File, InputStreamReader, OutputStreamWriter}
 import java.util.concurrent.{ExecutorService, Executors, TimeUnit}
+import scala.collection.mutable.ListBuffer
 import scala.jdk.CollectionConverters.*
 
 object BinServer {
 
   val path = "C:\\Users\\visio\\Downloads\\continue-intellij-extension-1.0.52\\continue-intellij-extension\\core\\win32-x64\\continue-binary.exe"
+
+  val getIdeSettings = """{"messageId":"XXX-XXX-XXX-XXX","messageType":"getIdeSettings","data":{"remoteConfigSyncPeriod":60,"userToken":"","pauseCodebaseIndexOnStart":false,"continueTestEnvironment":"production"}}"""
+  val getControlPlaneSessionInfo = """{"messageId":"XXX-XXX-XXX-XXX","messageType":"getControlPlaneSessionInfo"}"""
+  val getIdeInfo = """{"messageId":"XXX-XXX-XXX-XXX","messageType":"getIdeInfo","data":{"ideType":"jetbrains","name":"IntelliJ IDEA 2024.1","version":"2024.1","remoteName":"local","extensionVersion":"1.0.52","isPrerelease":false}}"""
+  val getWorkspaceDirs = """{"messageId":"XXX-XXX-XXX-XXX","messageType":"getWorkspaceDirs","data":["file:///D:/development/github/continue-1.5.11/continue-1.5.11/manual-testing-sandbox"]}"""
 
   val executorService: ExecutorService = Executors.newCachedThreadPool()
 
@@ -19,63 +27,60 @@ object BinServer {
   }
 
   def main(args: Array[String]): Unit = {
+
+    val continueNeedTypes = List("getIdeSettings", "getControlPlaneSessionInfo", "getIdeInfo", "getWorkspaceDirs")
+    val continueNeedTypeMap = scala.collection.mutable.HashMap[String, String]()
+
     val process = runProcess()
 
-    val  stdoutReader: BufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))
-    val  stderrReader: BufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))
+    val stdoutReader: BufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))
+    executorService.execute(()=>{
+      //val stdout = new java.lang.StringBuilder
+      var line: String = stdoutReader.readLine()
+      while (line != null) {
+        //stdout.append(line).append("\n")
+        line = stdoutReader.readLine()
+        try{
+          val obj = JSONObject.parse(line)
+          if(obj!=null){
+            val messageType = obj.get("messageType").asInstanceOf[String]
+            val messageId = obj.get("messageId").asInstanceOf[String]
+            if(continueNeedTypes.filter(e=>e.equals(messageType)).size>0){
+              continueNeedTypeMap.put(messageType, messageId)
+            }
+          }
+        }
+        catch
+          case exception: Exception => exception.printStackTrace()
 
-    val stdout = new java.lang.StringBuilder
-    val stderr = new java.lang.StringBuilder
-    var line: String = null
+        println(s"stdout:${line}")
+      }
+    })
 
-    line = stdoutReader.readLine()
-    while (line != null) {
-      stdout.append(line).append("\n")
-      line = stdoutReader.readLine()
-      println(s"stdout:${line}")
-    }
+    val stderrReader: BufferedReader = new BufferedReader(new InputStreamReader(process.getErrorStream()))
+    executorService.execute(()=>{
+//      val stderr = new java.lang.StringBuilder
+      var line: String = stderrReader.readLine()
+      while (line != null) {
+        //stdout.append(line).append("\n")
+        line = stdoutReader.readLine()
+        println(s"stderr:${line}")
+      }
+    })
 
-    line = stderrReader.readLine()
-    while (line!=null) {
-      stderr.append(line).append("\n")
-      line = stderrReader.readLine()
-      println(s"stderr:${line}")
-    }
+    val bufferedWriter: BufferedWriter = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()))
+    executorService.execute(()=>{
+      Log.parse()
+      val reqLines = Log.requestLines
+      reqLines.foreach(req=>{
+        Thread.sleep(2000)
+        println()
+        bufferedWriter.write(req.toCharArray)
+        println()
+      })
+    })
 
     val exitCode = process.waitFor
-
-
-//    process.errorReader()
-//    process.inputReader()
-
-
-//    executorService.execute(new Runnable(){
-//      override def run(): Unit = {
-//        while (true){
-//          print(".")
-//          val bufReader = process.errorReader()
-//          if(bufReader.lines().toList.size()>0){
-//            val line = bufReader.lines().toList.asScala.mkString("\n")
-//            println("error:" + line)
-//          }
-//          Thread.sleep(200)
-//        }
-//      }
-//    })
-
-//    executorService.execute(new Runnable() {
-//      override def run(): Unit = {
-//        while (true) {
-//          print("*")
-//          val bufReader = process.inputReader()
-//          if (bufReader.lines().toList.size() > 0) {
-//            val line = bufReader.lines().toList.asScala.mkString("\n")
-//            println("input:" + line)
-//          }
-//          Thread.sleep(200)
-//        }
-//      }
-//    })
 
     println("等待")
     Thread.sleep(9999999)
