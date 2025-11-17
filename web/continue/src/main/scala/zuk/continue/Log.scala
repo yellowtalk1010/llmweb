cf7da8f282365fff0ed9feda84ba8e9ba7959e44
qq5132834 <513283439@qq.com>
2025-11-17 14:07:50 +0800

    commit

--- web/continue/src/main/scala/zuk/continue/Log.scala
+++ web/continue/src/main/scala/zuk/continue/Log.scala
@@ -1,10 +1,16 @@
 package zuk.continue
 
+import com.alibaba.fastjson2.JSONObject
 import org.apache.commons.io.FileUtils
+
 import scala.jdk.CollectionConverters.*
 import java.io.File
+import scala.collection.mutable.ListBuffer
 
 object Log {
+
+  val REQUEST = "request"
+  val REPONSE = "reponse"
 
   /**
    * 分析continue.log日志
@@ -16,14 +22,74 @@ object Log {
     val lines = FileUtils.readLines(logFile, "utf-8").asScala.map(l=>{
       val pres = List("handleMessage:", "request:").filter(e=>l.startsWith(e))
       if(pres.size>0){
-        l.substring(pres.head.length)
+        (REQUEST, l.substring(pres.head.length))
       }
       else {
-        l
+        (REPONSE, l)
       }
     })
     println(lines.size)
-    lines.foreach(println)
+    val requestLines = lines.filter(_._1.equals(REQUEST)).map(_._2)
+    val responseLines = lines.filter(_._1.equals(REPONSE)).map(_._2)
+
+    val requestObjs = requestLines.map(e=>{
+      try{
+        JSONObject.parseObject(e)
+      }
+      catch
+        case exception: Exception =>
+          exception.printStackTrace()
+          null
+    }).filter(_!=null)
+    val responseObjs = responseLines.map(e=>{
+      try{
+        JSONObject.parseObject(e)
+      }
+      catch
+        case exception: Exception =>
+          println(e)
+          exception.printStackTrace()
+          null
+    }).filter(_!=null)
+
+
+    println(s"完成： ${requestObjs.size}, ${responseObjs.size}")
+
+    val messageTypes = (requestObjs ++ responseObjs).map(_.get("messageType")).toSet
+    println("消息类型总数：" + messageTypes.size)
+    messageTypes.foreach(println)
+
+    println("开始配对")
+    val resIds = ListBuffer[String]()
+    requestObjs.map(req=>{
+      val reqId = req.get("messageId").asInstanceOf[String]
+      val resObjs = responseObjs.filter(res=>{
+        val resId = res.get("messageId").asInstanceOf[String]
+        resIds += resId
+        resId!=null && resId.equals(reqId)
+      })
+      (req, resObjs)
+    }).foreach(tp2=>{
+      println("\n\n")
+      val reqStr = JSONObject.toJSONString(tp2._1)
+      println(reqStr)
+      tp2._2.foreach(res=>{
+        val resStr = JSONObject.toJSONString(res)
+        println(resStr)
+      })
+    })
+
+    println(resIds.toSet.size)
+    println("未配对的回复")
+    responseObjs.filter(res=>{
+      val resId = res.get("messageId").asInstanceOf[String]
+      !resIds.contains(resId)
+    }).foreach(res=>{
+      val resStr = JSONObject.toJSONString(res)
+      println(resStr)
+    })
+
+    println("over.")
   }
 
 }
