b4736408462cb2c3aa05060611665a89ed97d449
zuk <huangliao1010@gmail.com>
2025-11-23 09:15:26 +0800

    stockapi

--- web/boot/src/main/java/zuk/sast/rules/webSocket/AISocketHandler.java
+++ web/boot/src/main/java/zuk/sast/rules/webSocket/AISocketHandler.java
@@ -1,7 +1,5 @@
 package zuk.sast.rules.webSocket;
 
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -10,7 +8,6 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import zuk.Client;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -25,15 +22,45 @@ public class AISocketHandler extends TextWebSocketHandler {
 
     static {
         zuk.Client.init();
+
+        EXECUTOR_SERVICE.execute(()=>{
+            while (true) {
+                try {
+                    //清理断开的websocket连接
+                    sessionMap.entrySet().stream().filter(entry->!entry.getValue().isOpen()).forEach(entry->{
+                        sessionMap.remove(entry.getKey());
+                    });
+                    Thread.sleep(500);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        EXECUTOR_SERVICE.execute(()=>{
+            while (true) {
+                try {
+                    Thread.sleep(10);
+                    if(!Client.queue().isEmpty()){
+                        Client.LlmStreamChat llmStreamChat = Client.queue().poll();
+                        WebSocketSession session = sessionMap.get(llmStreamChat.getMessageId());
+                        if(session!=null && session.isOpen()){
+                            session.sendMessage(new TextMessage(llmStreamChat.getData().getContent().getContent()));
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         sessionMap.put(session.getId(), session);
-        sessionMap.entrySet().stream().filter(entry->!entry.getValue().isOpen()).forEach(entry->{
-            sessionMap.remove(entry.getKey());
-        });
-        log.info("连接总数:" + sessionMap.size());
+        //log.info("连接总数:" + sessionMap.size());
     }
 
     @Override
@@ -42,10 +69,10 @@ public class AISocketHandler extends TextWebSocketHandler {
         try {
             String messageStr = message.getPayload();
             log.info("接受到数据:" + messageStr);
-            //EXECUTOR_SERVICE.execute(new TestThread(messageStr, session));
+            //EXECUTOR_SERVICE.execute(new MockDataThread(messageStr, session));  //mock假数据调试
             EXECUTOR_SERVICE.execute(()->{
                 try {
-                    Client.testWrite();
+                    Client.testWrite(session.getId());
                     while (true) {
                         Thread.sleep(50);
                         if(!Client.queue().isEmpty()){
@@ -79,89 +106,5 @@ public class AISocketHandler extends TextWebSocketHandler {
         }
     }
 
-
-    /***
-     * 测试 markdown
-     */
-    private class TestThread implements Runnable {
-
-        private String messageStr;
-        private WebSocketSession session;
-
-        public TestThread(String messageStr, WebSocketSession session) {
-            this.messageStr = messageStr;
-            this.session = session;
-        }
-
-        @Override
-        public void run() {
-            String md = getMD();
-            md = this.messageStr + "\n" + md;
-            Arrays.stream(md.split("\n")).forEach(line->{
-                try{
-                    char arr[] = line.toCharArray();
-                    for (int i =0; i< arr.length; i++) {
-                        this.session.sendMessage(new TextMessage(arr[i]+"")); //一个字符一个字符的输出
-                        Thread.sleep(50);
-                    }
-                    this.session.sendMessage(new TextMessage("\n"));
-
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-
-
-        private static final String getMD() {
-            return """
-                # 一级标题
-                ## 二级标题
-                ### 三级标题
-                                
-                这是一个 **加粗** 的文字，这是一个 *斜体*，这是一个 ***加粗+斜体***。 \s
-                这是一个 ~~删除线~~。
-                                
-                ---
-                                
-                ## 列表示例
-                                
-                ### 无序列表
-                - 苹果
-                - 香蕉
-                    - 小香蕉
-                    - 大香蕉
-                - 橘子
-                                
-                ### 有序列表
-                1. 第一项
-                2. 第二项
-                    1. 子项 A
-                    2. 子项 B
-                3. 第三项
-                                
-                ---
-                                
-                ## 引用
-                > 这是一个引用。
-                >> 这是嵌套引用。
-                                
-                ---
-                                
-                ## 代码示例
-                                
-                ### 行内代码
-                这是 `print("Hello, World!")` 的例子。
-                                
-                ### 多行代码（Python）
-                ```python
-                def greet(name):
-                    print(f"Hello, {name}!")
-                                
-                greet("张三")
-                                
-                """;
-        }
-    }
 
 }
---
+++ web/boot/src/main/java/zuk/sast/rules/webSocket/MockDataThread.java
@@ -0,0 +1,91 @@
+package zuk.sast.rules.webSocket;
+
+import org.springframework.web.socket.WebSocketSession;
+import zuk.Client;
+
+import java.util.Arrays;
+
+/***
+ * MOCK 大模型的 数据
+ */
+public class MockDataThread implements Runnable {
+
+    private String messageStr;
+    private WebSocketSession session;
+
+    public MockDataThread(String messageStr, WebSocketSession session) {
+        this.messageStr = messageStr;
+        this.session = session;
+    }
+
+    @Override
+    public void run() {
+        String md = getMD();
+        md = this.messageStr + "\n" + md;
+        Arrays.stream(md.split("\n")).forEach(line->{
+            try{
+                char arr[] = line.toCharArray();
+                for (int i =0; i< arr.length; i++) {
+                    Client.ContentData contentData = new Client.ContentData("assistant", arr[i]+"")
+                    Client.Data data = new Client.Data(false, contentData, "success")
+                    Client.LlmStreamChat llmStreamChat = new Client.LlmStreamChat("llm/streamChat", data, session.getId());
+                    Client.queue().put(llmStreamChat);
+                }
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+
+    private static final String getMD() {
+        return """
+                # 一级标题
+                ## 二级标题
+                ### 三级标题
+                                
+                这是一个 **加粗** 的文字，这是一个 *斜体*，这是一个 ***加粗+斜体***。 \s
+                这是一个 ~~删除线~~。
+                                
+                ---
+                                
+                ## 列表示例
+                                
+                ### 无序列表
+                - 苹果
+                - 香蕉
+                    - 小香蕉
+                    - 大香蕉
+                - 橘子
+                                
+                ### 有序列表
+                1. 第一项
+                2. 第二项
+                    1. 子项 A
+                    2. 子项 B
+                3. 第三项
+                                
+                ---
+                                
+                ## 引用
+                > 这是一个引用。
+                >> 这是嵌套引用。
+                                
+                ---
+                                
+                ## 代码示例
+                                
+                ### 行内代码
+                这是 `print("Hello, World!")` 的例子。
+                                
+                ### 多行代码（Python）
+                ```python
+                def greet(name):
+                    print(f"Hello, {name}!")
+                                
+                greet("张三")
+                                
+                """;
+    }
+}
