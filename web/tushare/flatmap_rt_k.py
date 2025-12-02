223e43c421e9fb717388073acaf759eb2ee69a07
qq5132834 <513283439@qq.com>
2025-12-02 15:38:13 +0800

    commit

--- web/tushare/flatmap_rt_k.py
+++ web/tushare/flatmap_rt_k.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     for index, row in df.iterrows():
         ts_code = row['ts_code']
         # ts_code	name	pre_close	high	open	low	close	vol	amount	num
-        print(ts_code)
+        print(ts_code, row['name'])
         ts_code_path = ts_code.replace(".", "_")
         module_path = f"module/{ts_code_path}.csv"
         if not os.path.exists(module_path):
@@ -21,35 +21,34 @@ if __name__ == '__main__':
         if module_df.empty or len(module_df) == 0:
             continue
 
-        module_head_row = module_df.iloc[0]
-
-        module_head_row_trade_date = module_head_row["trade_date"]
-
-        if(module_head_row_trade_date == trade_date or str(module_head_row_trade_date) == trade_date):
+        # 取第一条记录
+        module_first_row = module_df.iloc[0]
+        if(module_first_row["trade_date"] == trade_date or str(module_first_row["trade_date"]) == trade_date):
             # 记录已存在
             continue
 
-        turnover_rate = row['vol'] / module_head_row["float_share"]
-        change = (row['close'] - module_head_row["close"]) / module_head_row["close"]
-
-        print(module_head_row["ts_code"], module_head_row["name"])
+        # 计算换手率
+        turnover_rate = row['vol'] / module_first_row["float_share"]
+        # 计算涨跌
+        change = (row['close'] - module_first_row["close"]) / module_first_row["close"]
+        print(module_first_row["ts_code"], module_first_row["name"])
         # ts_code	name	trade_date	open	high	low	close	pre_close	change	vol	amount	turnover_rate	float_share	area	industry	market
-        new_record = {'ts_code': module_head_row["ts_code"],
-                      'name': module_head_row["name"],
+        new_record = {'ts_code': module_first_row["ts_code"],
+                      'name': module_first_row["name"],
                       'trade_date': trade_date,
                       'open': row["open"],
                       'high': row["high"],
                       'low': row["low"],
                       'close': row["close"],
-                      'pre_close': module_head_row["close"],
+                      'pre_close': module_first_row["close"],
                       'change': round(change, 4),
                       'vol': row["vol"],
                       'amount': row["amount"],
                       'turnover_rate': round(turnover_rate, 4),
-                      'float_share': module_head_row["float_share"],
-                      'area': module_head_row["area"],
-                      'industry': module_head_row["industry"],
-                      'market': module_head_row["market"]
+                      'float_share': module_first_row["float_share"],
+                      'area': module_first_row["area"],
+                      'industry': module_first_row["industry"],
+                      'market': module_first_row["market"]
                       }
 
         new_df = pd.DataFrame([new_record])
