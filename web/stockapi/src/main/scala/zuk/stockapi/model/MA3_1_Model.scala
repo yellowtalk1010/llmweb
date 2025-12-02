cd7518c260ce00614d265ef9ca9fb30c8d08035e
zuk <huangliao1010@gmail.com>
2025-12-02 21:21:51 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/stockapi/model/MA3_1_Model.scala
@@ -0,0 +1,35 @@
+package zuk.stockapi.model
+
+import zuk.stockapi.{StockApiVo, StockMaVo}
+
+import java.math
+import java.math.BigDecimal
+
+/***
+ * 价高量小
+ */
+class MA3_1_Model(stockMaVo: StockApiVo, maList: List[StockMaVo]) extends Model(stockMaVo) {
+
+  private var isOK: Boolean = false
+
+  override def isHit(): Boolean = isOK
+
+  override def run(): Unit = {
+    if(maList.size>=3){
+      val list = maList.take(3)
+      val head = list.head
+      if (
+        new BigDecimal(list(0).getMa5).compareTo(new BigDecimal(list(0).getMa10)) >= 0
+          && new BigDecimal(list(1).getMa5).compareTo(new BigDecimal(list(1).getMa10)) <= 0
+          && new BigDecimal(list(2).getMa5).compareTo(new BigDecimal(list(2).getMa10)) <= 0
+          && new BigDecimal(head.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(0)) >= 0 //换手率
+          && new BigDecimal(head.getStockDayVo.getTurnoverRatio).compareTo(new BigDecimal(5)) <= 0 //换手率
+          && new BigDecimal(head.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(4)) >= 0 //涨幅度
+          && new BigDecimal(head.getStockDayVo.getChangeRatio).compareTo(new BigDecimal(15)) <= 0 //涨幅度
+      ) {
+        isOK = true
+      }
+    }
+  }
+
+}
--- web/stockapi/src/test/scala/zuk/stock/test/Tushare_MA3_Model_Test.scala
+++ web/stockapi/src/test/scala/zuk/stock/test/Tushare_MA3_Model_Test.scala
@@ -2,7 +2,7 @@ package zuk.stock.test
 
 import org.apache.commons.io.FileUtils
 import org.scalatest.funsuite.AnyFunSuite
-import zuk.stockapi.model.MA3_Model
+import zuk.stockapi.model.{MA3_1_Model, MA3_Model}
 import zuk.stockapi.{CalculateMAForDay, CalculateMAForDay_Tushare, LoaderLocalStockData, StockApiVo}
 
 import java.io.File
@@ -21,7 +21,7 @@ class Tushare_MA3_Model_Test extends AnyFunSuite {
   val path = "tushare"
   CalculateMAForDay_Tushare.CSV_PATH = path
 
-  test("tushare"){
+  test("tushare-3"){
     //将tushare的csv数据转成对象
     val in = new FileReader(s"${path}\\all_stocks.csv")
     val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in)
@@ -61,8 +61,55 @@ class Tushare_MA3_Model_Test extends AnyFunSuite {
     val sdm = new SimpleDateFormat("yyyyMMdd")
 
     val mailAddress = "513283439@qq.com"
-    SendMail.sendSimpleEmail(mailAddress, mailAddress, "推荐", socketList.map(e=>s"${e._1.getApi_code},${e._1.getName}").mkString("\n"))
+    SendMail.sendSimpleEmail(mailAddress, mailAddress, "推荐 M3", socketList.map(e=>s"${e._1.getApi_code},${e._1.getName}").mkString("\n"))
 //    FileUtils.writeLines(new File(s"stockapi/model_result/${sdm.format(new Date)}-MA3.txt"), socketList.map(e => s"${e._1.getApi_code}，${e._1.getName}").asJava)
+
+  }
+
+
+
+  test("tushare-3.1"){
+    //将tushare的csv数据转成对象
+    val in = new FileReader(s"${path}\\all_stocks.csv")
+    val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in)
+
+    val codes = records.asScala.map(record=>{
+        val ts_code = record.get("ts_code")
+        val name = record.get("name")
+        val splits = ts_code.split("\\.")
+        val code = splits(0)
+        val jys = splits(1)
+        val gl = record.get("industry")
+        val stockApiVo = new StockApiVo()
+        stockApiVo.setApi_code(code)
+        stockApiVo.setName(name)
+        stockApiVo.setJys(jys)
+        stockApiVo.setGl(gl)
+        stockApiVo
+      })
+      //      .filter(e=>e.getApi_code.contains("000753"))
+      .toList
+    in.close()
+    println(s"${codes.size}")
+
+    val tpList = CalculateMAForDay_Tushare.run(codes)
+
+    val socketList = tpList.filter(_._2.size > 0).filter(tp => {
+        val stock = tp._1
+        val malist = tp._2
+        val model = new MA3_1_Model(stock, malist)
+        model.run()
+        model.isHit()
+      })
+      .filter(e=> !e._1.getName.contains("ST")) //排除ST股票
+
+    println(socketList.size)
+    socketList.map(e => e._1.getApi_code + "，" + e._1.getName).foreach(println)
+    val sdm = new SimpleDateFormat("yyyyMMdd")
+
+    val mailAddress = "513283439@qq.com"
+    SendMail.sendSimpleEmail(mailAddress, mailAddress, "推荐 M3.1", socketList.map(e=>s"${e._1.getApi_code},${e._1.getName}").mkString("\n"))
+    //    FileUtils.writeLines(new File(s"stockapi/model_result/${sdm.format(new Date)}-MA3.txt"), socketList.map(e => s"${e._1.getApi_code}，${e._1.getName}").asJava)
 
   }
 
