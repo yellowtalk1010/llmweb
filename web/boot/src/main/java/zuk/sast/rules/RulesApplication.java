408ac7c2f9a2724d71c1cdd2a8180fc92cc8fc4e
zuk <huangliao1010@gmail.com>
2025-12-02 22:16:51 +0800

    commit

--- web/boot/src/main/java/zuk/sast/rules/RulesApplication.java
+++ web/boot/src/main/java/zuk/sast/rules/RulesApplication.java
@@ -14,33 +14,9 @@ public class RulesApplication {
         System.getProperties().put("spring.servlet.multipart.max-file-size", 1024*10 + "MB"); //上传文件最大10G
         System.getProperties().put("spring.servlet.multipart.max-request-size", 1024*10 + "MB"); //上传文件最大10G
 
-        //loadProperties();
         SpringApplication.run(RulesApplication.class, args);
 
     }
 
-    /***
-     * 加载config.properties文件
-     */
-//    public static void loadProperties() {
-//        try {
-//            Database.PROPERTIES.clear();
-//            String configFileName = "config.properties";
-//            File propertiesFile = new File(configFileName);
-//            if(!propertiesFile.exists()){
-//                try (FileOutputStream out = new FileOutputStream(propertiesFile)) {
-//                    PropertiesKey.properties.store(out, "配置文件 - 用户信息");
-//                    System.out.println("写入完成！");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            InputStream input = new FileInputStream(propertiesFile);
-//            Database.PROPERTIES.load(input);
-//            System.out.println(Database.PROPERTIES);
-//        }catch (Exception exception) {
-//            exception.printStackTrace();
-//        }
-//    }
 
 }
