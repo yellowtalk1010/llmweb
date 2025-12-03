beffe40509792f81230aaaf58ab8641ebb0965f3
zuk <huangliao1010@gmail.com>
2025-12-03 21:22:17 +0800

    2025-12-02

--- web/tushare/ZukTuShare.py
+++ web/tushare/ZukTuShare.py
@@ -1,2 +1,16 @@
+import tushare as ts
 
 token = "4dfe55ae66614ca943e09a6d82339eb65b77dcaf327841ba3d5c1574"
+
+
+def getPro_5000():
+    token_5000 = "4dfe55ae66614ca943e09a6d82339eb65b77dcaf327841ba3d5c1574"
+    pro = ts.pro_api(token_5000)
+    return pro
+
+
+def getPro_10000():
+    pro = ts.pro_api("c1r591l1j03n22x258")
+    pro._DataApi__token = 'c1r591l1j03n22x258'
+    pro._DataApi__http_url = 'http://proplus.tushare.nlink.vip'
+    return pro
