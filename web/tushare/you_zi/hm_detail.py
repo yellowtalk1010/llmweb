a3d7a20a778877f0ea2d9f86b7aced95ea8e798c
zuk <huangliao1010@gmail.com>
2025-12-03 21:41:55 +0800

    2025-12-02

---
+++ web/tushare/you_zi/hm_detail.py
@@ -0,0 +1,39 @@
+
+# 导入tushare
+import tushare as ts
+import ZukTuShare
+import pandas as pd
+
+# 初始化pro接口
+pro = ZukTuShare.getPro()
+
+# 游资交易每日明细
+def download_hm_detail(trade_date):
+    # 拉取数据
+    df = pro.hm_detail(**{
+        "trade_date": trade_date,
+        "ts_code": "",
+        "hm_name": "",
+        "start_date": "",
+        "end_date": "",
+        "limit": "",
+        "offset": ""
+    }, fields=[
+        "trade_date",
+        "ts_code",
+        "ts_name",
+        "buy_amount",
+        "sell_amount",
+        "net_amount",
+        "hm_name",
+        "hm_orgs",
+        "tag"
+    ])
+    # print(df)
+    return df
+
+if __name__ == '__main__':
+    print("游资交易每日明细")
+    df = download_hm_detail("20251203")
+    df.to_csv("hm_detail.csv", encoding="utf-8", index=False)
+    print("完成")
