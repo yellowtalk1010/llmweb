13a059deb055862b95e415feaedb7f3831861f14
zuk <huangliao1010@gmail.com>
2025-12-06 19:29:58 +0800

    commit

---
+++ web/stockapi/src/main/java/zuk/tu_share/dto/ModuleDay.scala
@@ -0,0 +1,21 @@
+package zuk.tu_share.dto
+
+import scala.beans.BeanProperty
+
+class ModuleDay {
+
+  @BeanProperty var ts_code: String = _
+  @BeanProperty var name: String = _
+  @BeanProperty var trade_date: String = _
+  @BeanProperty var open: String = _
+  @BeanProperty var high: String = _
+  @BeanProperty var low: String = _
+  @BeanProperty var close: String = _
+  @BeanProperty var pre_close: String = _
+  @BeanProperty var change: String = _
+  @BeanProperty var vol: String = _
+  @BeanProperty var amount: String = _
+  @BeanProperty var turnover_rate: String = _
+  @BeanProperty var float_share: String = _
+
+}
