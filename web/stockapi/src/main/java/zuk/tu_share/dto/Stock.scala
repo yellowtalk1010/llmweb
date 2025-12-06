e26667b9387e6c452ce82b889e79c2cb591b38fe
zuk <huangliao1010@gmail.com>
2025-12-06 19:25:41 +0800

    commit

---
+++ web/stockapi/src/main/java/zuk/tu_share/dto/Stock.scala
@@ -0,0 +1,15 @@
+package zuk.tu_share.dto
+
+import scala.beans.BeanProperty
+
+class Stock {
+
+  @BeanProperty var ts_code: String = _     //ts代码
+  @BeanProperty var symbol: String = _      //股票代码
+  @BeanProperty var name: String = _        //名称
+  @BeanProperty var area: String = _        //地域
+  @BeanProperty var industry: String = _    //所属行业
+  @BeanProperty var market: String = _      //市场类型（主板/创业板/科创板/CDR）
+
+}
+
