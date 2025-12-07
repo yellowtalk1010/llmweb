afdea5d7e006c64e569405be3baf7cb327104e06
zuk <huangliao1010@gmail.com>
2025-12-07 17:23:48 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA3_2_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA3_2_Model.scala
@@ -10,21 +10,24 @@ class MA3_2_Model extends IModel {
   val stocks = ListBuffer[String]()
 
   override def run(days: List[ModuleDay]): Unit = {
-    if(days.size>=3){
-      val list = days.take(3)
+    if(days.size>=5){
+      val list = days.take(5)
       val head = list.head
       if (
-        new BigDecimal(list(2).change).compareTo(new BigDecimal(0)) < 0
-        && new BigDecimal(list(1).change).compareTo(new BigDecimal(0)) < 0  //连续两天下跌
-        && new BigDecimal(list(0).change).compareTo(new BigDecimal(0)) > 0
+        new BigDecimal(list(3).change).compareTo(new BigDecimal(0)) < 0       //下跌
+          && new BigDecimal(list(2).change).compareTo(new BigDecimal(0)) < 0  //下跌
+          && new BigDecimal(list(1).change).compareTo(new BigDecimal(0)) > 0  //上涨
+          && new BigDecimal(list(0).change).compareTo(new BigDecimal(0)) < 0  //下跌
 
-        && new BigDecimal(list(1).close).compareTo(new BigDecimal(list(2).close)) < 0 //连续2天下载
-        && new BigDecimal(list(0).low).compareTo(new BigDecimal(list(1).close)) < 0
+        && new BigDecimal(list(2).close).compareTo(new BigDecimal(list(3).close)) < 0 //连续2天下跌是递减趋势
+        && new BigDecimal(list(1).low).compareTo(new BigDecimal(list(2).close)) < 0   //
 
-        && new BigDecimal(list(0).vol).compareTo(new BigDecimal(list(1).vol).add(new BigDecimal(list(2).vol))) > 0   //上涨并且，反包两日下跌交易量
-        && new BigDecimal(list(0).change).compareTo(new BigDecimal(4)) > 0
-        && new BigDecimal(list(0).change).compareTo(new BigDecimal(9)) < 0
-        && new BigDecimal(list(0).turnover_rate).compareTo(new BigDecimal(5)) > 0
+        && new BigDecimal(list(1).vol).compareTo(new BigDecimal(list(2).vol).add(new BigDecimal(list(3).vol))) > 0   //上涨并且，反包两日下跌交易量
+        && new BigDecimal(list(1).change).compareTo(new BigDecimal(4)) > 0
+        && new BigDecimal(list(1).change).compareTo(new BigDecimal(9)) < 0
+        && new BigDecimal(list(1).turnover_rate).compareTo(new BigDecimal(5)) > 0
+
+        && new BigDecimal(list(1).high).compareTo(new BigDecimal(list(0).high)) > 0 //  最新一日的最高价，不超过上一个最高价，防止抛售（如果是均价是最好的）
 
       ) {
         //缩量上涨
