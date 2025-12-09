5f1e90ee945d85fe83b546b707a73d031f70e3d3
zuk <huangliao1010@gmail.com>
2025-12-09 19:24:14 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
@@ -2,7 +2,7 @@ package zuk.tu_share.pass
 
 import zuk.tu_share.DataFrame
 import zuk.tu_share.dto.{ModuleDay, TsStock}
-import zuk.tu_share.module.{IModel, MA3_1_Model, MA3_2_Model, MA3_Model}
+import zuk.tu_share.module.{IModel, MA3_1_Model, MA3_2_Model, MA3_3_Model, MA3_Model}
 import zuk.utils.SendMail
 
 import java.math.{BigDecimal, RoundingMode}
@@ -92,9 +92,11 @@ object PassFactory {
   }
 
   private def moduleList(): List[IModel] = {
-    List(new MA3_Model,
+    List(
+      new MA3_Model,
       new MA3_1_Model,
-      new MA3_2_Model
+      new MA3_2_Model,
+//      new MA3_3_Model
     )
   }
 
