cd1b43944d62cbc3b28c325c4e3899724cc609b0
zuk <huangliao1010@gmail.com>
2025-12-10 19:05:34 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/backtest/BackTest.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/backtest/BackTest.scala
@@ -9,34 +9,36 @@ object BackTest {
 
   val backTestList = ListBuffer[IModel]()
 
-  def analysis(clsName: String): Unit = {
+  def analysis(): Unit = {
 
-    val filterList = backTestList.filter(_.getClass.getSimpleName.equals(clsName)).filter(e=>e.getTsStocks()!=null && e.getTsStocks().size>0)
-    if(filterList.size==0){
-      println("无数据")
-      return
-    }
+    backTestList.filter(e=>e.getTsStocks()!=null && e.getTsStocks().size>0)
+      .groupBy(_.getClass.getSimpleName)
+      .filter(_._2.size>0)
+      .foreach(e=>{
+        val clsName = e._1
+        val ls = e._2
+        val victoryList = ls.filter(mod => {
 
-    val victoryList = filterList.filter(mod=>{
+          val preClose = mod.sells.head.pre_close
+          val highStr = mod.sells.map(e => {
+            val change = ((new BigDecimal(e.high).subtract(new BigDecimal(preClose))).multiply(new BigDecimal(100))).divide(new BigDecimal(preClose), 4, RoundingMode.UP)
+            s"${e.trade_date}【${change}】【卖出】"
+          }).mkString(", ")
 
-      val preClose = mod.sells.head.pre_close
-      val highStr = mod.sells.map(e=>{
-        val change = ((new BigDecimal(e.high).subtract(new BigDecimal(preClose))).multiply(new BigDecimal(100))).divide(new BigDecimal(preClose), 4, RoundingMode.UP)
-        s"${e.trade_date}【${change}】【卖出】"
-      }).mkString(", ")
+          val st = mod.sells.filter(e => {
+            e.high.toFloat > mod.buy.close.toFloat
+          }).size > 0
 
-      val st = mod.sells.filter(e => {
-        e.high.toFloat > mod.buy.close.toFloat
-      }).size > 0
+          val ok = if (st) "" else "X"
+          println(s"${mod.buy.ts_code}, ${mod.buy.name}, ${mod.buy.trade_date}【买入】, ${highStr}, ${ok}")
 
-      val ok = if (st) "" else "X"
-      println(s"${mod.buy.ts_code}, ${mod.buy.name}, ${mod.buy.trade_date}【买入】, ${highStr}, ${ok}")
+          st
 
-      st
+        })
 
+        println(s"${clsName}胜率：${new BigDecimal(victoryList.size).divide(new BigDecimal(ls.size), 4, RoundingMode.UP)}")
     })
 
-    println(s"${clsName}胜率：${new BigDecimal(victoryList.size).divide(new BigDecimal(filterList.size), 4, RoundingMode.UP)}")
 
   }
 
--- web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
@@ -92,8 +92,8 @@ object PassFactory {
 
   private def moduleList(): List[IModel] = {
     List(
-      //      new MA3_Model,
-      //      new MA3_1_Model,
+//            new MA3_Model,
+//            new MA3_1_Model,
       new MA3_2_Model,
       new MA3_3_Model
     )
