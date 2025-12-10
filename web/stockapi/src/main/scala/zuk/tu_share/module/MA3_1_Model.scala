7b6481ef8a64d0511e79a3bd82bc0777b6792f6b
zuk <huangliao1010@gmail.com>
2025-12-10 21:32:49 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA3_1_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA3_1_Model.scala
@@ -10,8 +10,8 @@ class MA3_1_Model extends IModel {
   val stocks = ListBuffer[String]()
 
   override def run(days: List[ModuleDay]): Unit = {
-    if(days.size>=3){
-      val list = days.take(3)
+    if(days.size>=4){
+      val list = days.take(4)
       val head = list.head
       if (
         list(0).ma.ma5.compareTo(list(0).ma.ma10) >= 0
@@ -19,9 +19,12 @@ class MA3_1_Model extends IModel {
           && list(2).ma.ma5.compareTo(list(2).ma.ma10) <= 0
           && new BigDecimal(head.turnover_rate).compareTo(BigDecimal(0)) >= 0 //换手率
           && new BigDecimal(head.turnover_rate).compareTo(BigDecimal(5)) <= 0 //换手率
+          && (new BigDecimal(list(1).turnover_rate).compareTo(BigDecimal(4)) > 0
+            || new BigDecimal(list(2).turnover_rate).compareTo(BigDecimal(4)) > 0) //换手率
           && new BigDecimal(head.change).compareTo(BigDecimal(4)) >= 0 //涨幅度
           && new BigDecimal(head.change).compareTo(BigDecimal(9)) <= 0 //涨幅度
           && new BigDecimal(head.vol).compareTo(new BigDecimal(list(1).vol).add(new BigDecimal(list(2).vol))) < 0 //
+          && new BigDecimal(head.high).compareTo(new BigDecimal(list(1).high)) > 0
       ) {
         //缩量上涨
         if(!head.name.contains("ST")){
