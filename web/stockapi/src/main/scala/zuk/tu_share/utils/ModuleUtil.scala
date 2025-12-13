8a16869125353045f8a1b42e94e3f7fae7525825
zuk <huangliao1010@gmail.com>
2025-12-14 02:15:10 +0800

    20251211

--- web/stockapi/src/main/scala/zuk/tu_share/utils/ModuleUtil.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/utils/ModuleUtil.scala
@@ -10,17 +10,17 @@ object ModuleUtil {
 
   def handule(): Unit = {
     val codeSet = HmDetailUtil.loadData().flatMap(_._2.map(e=>s"${e.ts_code}")).toSet
-    val stocks = AllStockUtil.loadData().filter(e=>codeSet.contains(e.ts_code))
-    println("总票数:" + stocks.size)
-    stocks.map(toStr).foreach(println)
+    val hm_stocks = AllStockUtil.loadData().filter(e=>codeSet.contains(e.ts_code))
+    println("总票数:" + hm_stocks.size)
+    hm_stocks.map(toStr).foreach(println)
     println(s"\n${LINE}行业排序${LINE}")
-    val industrySortStocks = stocks.groupBy(_.industry).toList.sortBy(_._2.size).reverse
+    val industrySortStocks = hm_stocks.groupBy(_.industry).toList.sortBy(_._2.size).reverse
     industrySortStocks.map(tp=>s"${tp._1},${tp._2.size}").foreach(println)
     println(s"\n${LINE}区域排序${LINE}")
-    val areaSortStocks = stocks.groupBy(_.area).toList.sortBy(_._2.size).reverse
+    val areaSortStocks = hm_stocks.groupBy(_.area).toList.sortBy(_._2.size).reverse
     areaSortStocks.map(tp=>s"${tp._1},${tp._2.size}").foreach(println)
     println(s"\n${LINE}符合行业、区域排名前10交集${LINE}")
-    stocks.filter(s=>{
+    hm_stocks.filter(s=>{
       industrySortStocks.take(10).map(_._1).contains(s.industry)   //行业排名取前5名，前几名，差距不大。
       && areaSortStocks.take(10).map(_._1).contains(s.area)        //地区排名取第1名，因为第一名的权重很大，所以，只取第一名。
     }).map(toStr).foreach(println)
@@ -30,6 +30,15 @@ object ModuleUtil {
       val stock = AllStockUtil.loadData().filter(s=>s.ts_code.equals(tp._1)).head
       println(s"${toStr(stock)}, ${hmNames.size}个, ${hmNames.mkString("; ")}")
       (stock, hmNames)
+    })
+    println(s"\n${LINE}过去30个交易日净买卖排序${LINE}")
+    val netAmounts = HmDetailUtil.loadData().flatMap(_._2).groupBy(_.ts_code).map(tp=>{
+      val stock = AllStockUtil.loadData().filter(_.ts_code.equals(tp._1)).head
+      val sum = tp._2.map(_.net_amount.toDouble).sum
+      (stock, sum)
+    }).toList.sortBy(_._2).reverse
+    netAmounts.foreach(tp=>{
+      println(s"${toStr(tp._1)}, ${tp._2}")
     })
 
 
