f836de64921610965c6d84c66c8809e13b96e6ec
zuk <huangliao1010@gmail.com>
2025-12-18 18:25:31 +0800

    2025-12-18

--- web/tushare/hm/top_inst.py
+++ web/tushare/hm/top_inst.py
@@ -30,7 +30,7 @@ def top_inst(trade_date):
 
 
 if __name__ == '__main__':
-    trade_date = "20251217"
+    trade_date = "20251218"
     df = top_inst(trade_date)
     df.to_csv(f"top_inst/{trade_date}_top_inst.csv", encoding="utf-8", index=False)
     print("完成")
