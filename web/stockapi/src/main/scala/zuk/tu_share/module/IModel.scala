89faa2e67069c77195c6d390817a1c7771f85f89
zuk <huangliao1010@gmail.com>
2025-12-26 11:50:38 +0800

    20251225

--- web/stockapi/src/main/scala/zuk/tu_share/module/IModel.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/IModel.scala
@@ -37,19 +37,20 @@ trait IModel {
 
   def changeUpRate(days: List[ModuleDay]): Float = {
     try{
+      //换手率超过5%的比例
       if(days.size==0){
         return 0.0
       }
       val max = 30
       var size = 0
         if (days.size > max) {
-          size = days.take(max).filter(_.turnover_rate.toFloat >= 3.0).size
+          size = days.take(max).filter(_.turnover_rate.toFloat >= 5.0).size
+          new BigDecimal(size).divide(new BigDecimal(max), 5, RoundingMode.UP).floatValue()
       }
       else {
-        size = days.filter(_.change.toFloat >= 3.0).size
+        size = days.filter(_.change.toFloat >= 5.0).size
+        new BigDecimal(size).divide(new BigDecimal(days.size), 5, RoundingMode.UP).floatValue()
       }
-//      val size = days.filter(_.turnover_rate.toFloat >= 3.0).size
-      new BigDecimal(size).divide(new BigDecimal(days.size), 5, RoundingMode.UP).floatValue()
     }
     catch
       case exception: Exception => 0.0
