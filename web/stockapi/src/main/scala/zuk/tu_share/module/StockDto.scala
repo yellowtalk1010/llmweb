a4e11b7ce8dfb62f4424593214c38011a3e8bf52
zuk <huangliao1010@gmail.com>
2025-12-26 21:21:27 +0800

    20251226

--- web/stockapi/src/main/scala/zuk/tu_share/module/StockDto.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/StockDto.scala
@@ -6,7 +6,7 @@ import scala.beans.BeanProperty
 
 class StockDto {
 
-  @BeanProperty var limitUp: Boolean = false
+  @BeanProperty var limitUp: Boolean = false  //过去是否存在涨停情况
   @BeanProperty var tsStock: TsStock = _
-  @BeanProperty var turnoverRate: Float = 0.0
+  @BeanProperty var turnoverRate: Float = 0.0  //过去30个交易日中，换手率大于3.5的比例
 }
