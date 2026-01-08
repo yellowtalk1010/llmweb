0ecb41c4d5a8bba689b5233f353835f596d3940f
zuk <huangliao1010@gmail.com>
2026-01-08 18:46:25 +0800

    20260108

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA3_3_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA3_3_Model.scala
@@ -66,7 +66,7 @@ class MA3_3_Model extends IModel {
 
   override def desc(): String = "反包两日阴线后继续上升（下引线最好涵盖在上一个交易日的柱子里面）。赶紧入手。"
 
-  override def winRate: Float = 0.8937
+  override def winRate: Float = 0.8914
 
   override def reference: Float = 0.00
 
