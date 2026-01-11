fc92e543256efa3afd91bdf11dbf4f8fdbe341d9
zuk <huangliao1010@gmail.com>
2026-01-11 16:22:51 +0800

    20260111

--- web/stockapi/src/main/scala/zuk/tu_share/utils/License.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/utils/License.scala
@@ -24,7 +24,7 @@ object License {
   }
 
   def check(pwd: String): Boolean = {
-    pwd.toLowerCase.equals("ilovehuangliao".toLowerCase) && check()
+    pwd.toLowerCase.equals("huangliaofather".toLowerCase) && check()
   }
 
 }
