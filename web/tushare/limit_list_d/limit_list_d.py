741a9c3cae0ffce8c2610ce638a283439c6f8bc0
zuk <huangliao1010@gmail.com>
2026-01-18 02:21:46 +0800

    add

---
+++ web/tushare/limit_list_d/limit_list_d.py
@@ -0,0 +1,50 @@
+import os
+
+import tushare as ts
+import pandas as pd
+import time
+
+import ZukTuShare
+
+# 初始化pro接口
+pro = ZukTuShare.getPro_10000()
+
+def download_limit_list_d(trade_date):
+    # 拉取数据
+    df = pro.limit_list_d(**{
+        "trade_date": trade_date,
+        "ts_code": "",
+        "limit_type": "",
+        "exchange": "",
+        "start_date": "",
+        "end_date": "",
+        "limit": "",
+        "offset": ""
+    }, fields=[
+        "trade_date",
+        "ts_code",
+        "industry",
+        "name",
+        "close",
+        "pct_chg",
+        "amount",
+        "limit_amount",
+        "float_mv",
+        "total_mv",
+        "turnover_ratio",
+        "fd_amount",
+        "first_time",
+        "last_time",
+        "open_times",
+        "up_stat",
+        "limit_times",
+        "limit"
+    ])
+    print(df)
+    return df
+
+# 获取炸板数据
+if __name__ == '__main__':
+    trade_date = "20260116"
+    df = download_limit_list_d(trade_date)
+    df.to_csv(f"data/{trade_date}_limit_list_d.csv")
