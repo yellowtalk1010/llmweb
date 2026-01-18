d3a3ccdf8ff5ed720dba2355940839ff454f9221
zuk <huangliao1010@gmail.com>
2026-01-18 17:24:27 +0800

    20260117

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA1_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA1_Model.scala
@@ -1,5 +1,7 @@
 package zuk.tu_share.module
+import zuk.tu_share.DataFrame
 import zuk.tu_share.dto.ModuleDay
+
 import java.math.BigDecimal
 /***
  * 中长持有
@@ -17,13 +19,25 @@ class MA1_Model extends IModel {
   var stockDto: StockDto = _
 
   override def run(days: List[ModuleDay]): Unit = {
-    if(days.size>max && days.take(max).filter(_.limit.equals("Z")).size > 0){
-      val maxDays = days.take(max)
-      val index = maxDays.indexWhere(_.limit.equals("Z"))
-      if(index >= 2){
-
+    try {
+      if (days.size > max && days.take(max).filter(_.limit.equals("Z")).size > 0) {
+        val maxDays = days.take(max)
+        val index = maxDays.indexWhere(_.limit.equals("Z"))
+        if (index >= 2) {
+          val maxDay = maxDays(index)
+          val maxDay_1 = maxDays(index - 1)
+          if (maxDay_1.high.toFloat > maxDay.high.toFloat //价涨
+            && maxDay_1.vol.toFloat < maxDay.vol.toFloat  //量缩
+          ) {
+            val tsStock = DataFrame.STOCKS_MAP.get(days.head.ts_code).getOrElse(null)
+            stockDto = new StockDto(tsStock, super.limitUp(days), super.limitDown(days), super.changeUpRate(days))
+          }
+        }
       }
     }
+    catch
+      case exception: Exception =>
+
   }
 
 
