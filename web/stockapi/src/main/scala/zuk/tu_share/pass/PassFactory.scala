1fef1090678fb70e3ca36640ca86d8defe372b48
zuk <huangliao1010@gmail.com>
2026-01-25 16:26:08 +0800

    20260123

--- web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
@@ -78,7 +78,13 @@ object PassFactory {
     println("完成模型分析")
     val filterModules = finishModules.filter(e=>e.getStockDto()!=null && e.getStockDto().tsStock!=null)
 
-    val emailContent = filterModules.groupBy(_.getClass.getSimpleName).filter(_._2.size>0).toList.sortBy(_._2.head.winRate).reverse.map(tp2=>{
+    //todo 多模型推荐
+    val moreModuleStr = filterModules.map(_.getStockDto().tsStock).groupBy(_.ts_code).filter(_._2.size>1).map(_._2.head).map(e=>{
+      e.ts_code + ", " + e.name
+    }).mkString("<br><br>\n\n")
+
+    //todo 各模型推荐
+    val singleModuleStr = filterModules.groupBy(_.getClass.getSimpleName).filter(_._2.size>0).toList.sortBy(_._2.head.winRate).reverse.map(tp2=>{
       val moduleName = tp2._1
       val moduleList = tp2._2
 
@@ -105,7 +111,7 @@ object PassFactory {
     }).mkString("<br><br>\n\n")
 
     if(backtestLenght==0 && LicenseUtil.check()){
-      sendMail(emailContent)
+      sendMail(moreModuleStr + singleModuleStr)
     }
 
   }
