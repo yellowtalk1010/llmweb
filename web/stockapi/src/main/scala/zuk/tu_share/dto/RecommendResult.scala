9d53082d1ce98d397c9e29fe475bc6aa47d93469
zuk <huangliao1010@gmail.com>
2026-01-27 22:34:03 +0800

    20260127

--- web/stockapi/src/main/scala/zuk/tu_share/dto/RecommendResult.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/dto/RecommendResult.scala
@@ -1,10 +1,14 @@
 package zuk.tu_share.dto
 
+import java.util
 import scala.beans.BeanProperty
 
 /***
  * 推荐结果
  */
+object RecommendResult{
+  @BeanProperty val result: java.util.List[RecommendResult] = new util.ArrayList[RecommendResult]()
+}
 class RecommendResult extends TsStock {
 
   @BeanProperty var modDesc: String = ""      //模型描述
