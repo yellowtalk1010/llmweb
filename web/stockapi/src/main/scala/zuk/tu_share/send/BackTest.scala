7e7b7d7df6c60e7c437aef357317ab0acf6e62f2
zuk <huangliao1010@gmail.com>
2026-01-28 21:14:45 +0800

    20260128

---
+++ web/stockapi/src/main/scala/zuk/tu_share/send/BackTest.scala
@@ -0,0 +1,23 @@
+package zuk.tu_share.send
+
+import zuk.tu_share.backtest.BackTest
+import zuk.tu_share.module.IModel
+
+/***
+ * 收集回测数据
+ */
+class BackTest extends ISend {
+
+  override def doSend(list: List[IModel]): Unit = {
+
+    list.groupBy(_.getClass.getSimpleName).filter(_._2.size > 0).toList.sortBy(_._2.head.winRate).reverse.foreach(tp2 => {
+      val moduleName = tp2._1
+      val moduleList = tp2._2
+
+      BackTest.backTestList ++= moduleList  //收集回测数据
+
+    })
+
+  }
+
+}
