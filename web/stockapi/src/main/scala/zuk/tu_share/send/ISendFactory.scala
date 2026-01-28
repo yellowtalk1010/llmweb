804ca7a7c9184fefd77cd75701a0838efd75d96d
zuk <huangliao1010@gmail.com>
2026-01-28 21:22:22 +0800

    20260128

--- web/stockapi/src/main/scala/zuk/tu_share/send/ISendFactory.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/send/ISendFactory.scala
@@ -1,8 +1,18 @@
 package zuk.tu_share.send
 
+import zuk.tu_share.module.IModel
+
 object ISendFactory {
 
-  def list(): List[ISend] = {
+  def doSend(list: List[IModel]): Unit = {
+    try {
+      sendList.foreach(_.doSend(list))
+    }
+    catch
+      case exception: Exception =>
+  }
+
+  private def sendList: List[ISend] = {
     List(
       new Console,
       new Email,
