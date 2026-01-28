1c1d8236feb3ed3041a51c7302ba2080165dbcfd
zuk <huangliao1010@gmail.com>
2026-01-28 21:55:16 +0800

    20260128

--- web/stockapi/src/main/scala/zuk/tu_share/send/JsonFile.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/send/JsonFile.scala
@@ -1,6 +1,15 @@
 package zuk.tu_share.send
 
+import com.alibaba.fastjson2.JSONObject
+import com.alibaba.fastjson2.JSONWriter.Feature
+import org.apache.commons.io.FileUtils
 import zuk.tu_share.module.IModel
+
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util
+import java.util.Date
+import scala.jdk.CollectionConverters.*
 
 /***
  * 保存json文件
@@ -8,7 +17,49 @@ import zuk.tu_share.module.IModel
 class JsonFile extends ISend {
 
   override def doSend(list: List[IModel]): Unit = {
+    try {
+      val arr = list.map(m=>{
 
+        val map = new util.HashMap[String, String]()
+
+        val dto = m.getStockDto()
+
+        val limitUp = dto.limitUp
+        val limitDown = dto.limitDown
+        val turnoverRate = dto.turnoverRate //活跃
+
+        map.put("limitUp", limitUp)
+        map.put("limitDown", limitUp)
+        map.put("turnoverRate", s"${turnoverRate}活跃")
+
+        val ts_code = dto.tsStock.ts_code //代码
+        val name = dto.tsStock.name //名称
+        val area = dto.tsStock.area //区域
+        val industry = dto.tsStock.industry //行业
+
+        map.put("ts_code", ts_code)
+        map.put("name", name)
+        map.put("area", area)
+        map.put("industry", industry)
+
+        val modWinRate = list.head.winRate //模型胜率
+        val modDesc = list.head.desc()    //模型描述
+        val modClsName = list.head.getClass.getSimpleName.toUpperCase //模型代码
+
+        map.put("modWinRate", s"${modWinRate}")
+        map.put("modDesc", modDesc)
+        map.put("modClsName", modClsName)
+
+        map
+      }).asJava
+
+      val json = JSONObject.toJSONString(arr, Feature.PrettyFormat)
+      val sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
+      val filepath = s"json/${sdf.format(new Date)}"
+      FileUtils.write(new File(filepath), json, "UTF-8")
+    }
+    catch
+      case exception: Exception =>
   }
 
 }
