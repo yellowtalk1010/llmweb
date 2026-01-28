39cda8c0a91ac870d28284a832d014ff74c77d38
zuk <huangliao1010@gmail.com>
2026-01-28 22:01:14 +0800

    20260128

--- web/stockapi/src/main/scala/zuk/tu_share/send/JsonFile.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/send/JsonFile.scala
@@ -3,7 +3,9 @@ package zuk.tu_share.send
 import com.alibaba.fastjson2.JSONObject
 import com.alibaba.fastjson2.JSONWriter.Feature
 import org.apache.commons.io.FileUtils
+import zuk.tu_share.CammandParam
 import zuk.tu_share.module.IModel
+import zuk.tu_share.utils.LicenseUtil
 
 import java.io.File
 import java.text.SimpleDateFormat
@@ -18,45 +20,52 @@ class JsonFile extends ISend {
 
   override def doSend(list: List[IModel]): Unit = {
     try {
-      val arr = list.map(m=>{
 
-        val map = new util.HashMap[String, String]()
+      if(CammandParam.param.json && LicenseUtil.check()){
+        //校验命令、 许可校验
 
-        val dto = m.getStockDto()
+        val arr = list.map(m => {
 
-        val limitUp = dto.limitUp
-        val limitDown = dto.limitDown
-        val turnoverRate = dto.turnoverRate //活跃
+          val map = new util.HashMap[String, String]()
 
-        map.put("limitUp", limitUp)
-        map.put("limitDown", limitUp)
-        map.put("turnoverRate", s"${turnoverRate}活跃")
+          val dto = m.getStockDto()
 
-        val ts_code = dto.tsStock.ts_code //代码
-        val name = dto.tsStock.name //名称
-        val area = dto.tsStock.area //区域
-        val industry = dto.tsStock.industry //行业
+          val limitUp = dto.limitUp
+          val limitDown = dto.limitDown
+          val turnoverRate = dto.turnoverRate //活跃
 
-        map.put("ts_code", ts_code)
-        map.put("name", name)
-        map.put("area", area)
-        map.put("industry", industry)
+          map.put("limitUp", limitUp)
+          map.put("limitDown", limitUp)
+          map.put("turnoverRate", s"${turnoverRate}活跃")
 
-        val modWinRate = m.winRate //模型胜率
-        val modDesc = m.desc()    //模型描述
-        val modClsName = m.getClass.getSimpleName.toUpperCase //模型代码
+          val ts_code = dto.tsStock.ts_code //代码
+          val name = dto.tsStock.name //名称
+          val area = dto.tsStock.area //区域
+          val industry = dto.tsStock.industry //行业
 
-        map.put("modWinRate", s"${modWinRate}")
-        map.put("modDesc", modDesc)
-        map.put("modClsName", modClsName)
+          map.put("ts_code", ts_code)
+          map.put("name", name)
+          map.put("area", area)
+          map.put("industry", industry)
 
-        map
-      }).asJava
+          val modWinRate = m.winRate //模型胜率
+          val modDesc = m.desc() //模型描述
+          val modClsName = m.getClass.getSimpleName.toUpperCase //模型代码
 
-      val json = JSONObject.toJSONString(arr, Feature.PrettyFormat)
-      val sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
-      val filepath = s"json/${sdf.format(new Date)}.json"
-      FileUtils.write(new File(filepath), json, "UTF-8")
+          map.put("modWinRate", s"${modWinRate}")
+          map.put("modDesc", modDesc)
+          map.put("modClsName", modClsName)
+
+          map
+        }).asJava
+
+        val json = JSONObject.toJSONString(arr, Feature.PrettyFormat)
+        val sdf = new SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filepath = s"json/${sdf.format(new Date)}.json"
+        FileUtils.write(new File(filepath), json, "UTF-8")
+
+      }
+
     }
     catch
       case exception: Exception =>
