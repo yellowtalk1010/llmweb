f775fb430fcec5b13d6a5a52f75c836cb18e8a2d
zuk <huangliao1010@gmail.com>
2026-01-31 21:44:49 +0800

    0.9036

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA1_1_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA1_1_Model.scala
@@ -37,8 +37,8 @@ class MA1_1_Model extends IModel {
           && middleList.map(_.low.toFloat).min > zList.map(_.low.toFloat).min
           && middleList.map(_.vol.toFloat).max < zList.map(_.vol.toFloat).min
           && (list(1).limit.equals(ModuleDay.Z) || list(1).limit.equals(ModuleDay.U))//上一个交易日炸板
-//          && list(0).vol.toFloat < zList.map(_.vol.toFloat).min //缩量
-//          && list(0).high.toFloat < list(1).high.toFloat //缩量
+          && list(0).vol.toFloat < list(1).vol.toFloat
+          && list(0).change.toFloat > 0
         ) {
             reason = zList.reverse.map(_.trade_date).mkString("至")
             val tsStock = DataFrame.STOCKS_MAP.get(days.head.ts_code).getOrElse(null)
