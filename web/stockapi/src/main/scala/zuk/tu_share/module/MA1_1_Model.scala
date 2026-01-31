c3a5d1c41f072ae47c14addc6f32d61285700d39
zuk <huangliao1010@gmail.com>
2026-01-31 17:16:45 +0800

    0.8000

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA1_1_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA1_1_Model.scala
@@ -32,13 +32,16 @@ class MA1_1_Model extends IModel {
 
         val zList = zhaBanIndexList.map(_._1) //炸板列表
 
-        if ((ListOrderCheck.isIncreasing(zList.map(_.close.toFloat))
-          || ListOrderCheck.isIncreasing(zList.map(_.high.toFloat))
-//          || zList(0).high.toFloat > zList(1).close.toFloat
+        if ((
+//          ListOrderCheck.isIncreasing(zList.map(_.close.toFloat))
+//          || ListOrderCheck.isIncreasing(zList.map(_.high.toFloat))
+//          ||
+            zList(0).high.toFloat > zList(1).close.toFloat
+          && zList(0).close.toFloat > zList(1).open.toFloat
           ) //炸板收盘价递增
 //          && zList(0).vol.toFloat < zList(1).vol.toFloat
 //          && list(0).vol.toFloat < zList(0).vol.toFloat
-//          && list(1).limit.equals(ModuleDay.Z) //上一个交易日炸板
+          && list(1).limit.equals(ModuleDay.Z) //上一个交易日炸板
 //          && list.head.close.toFloat > list.head.pre_close.toFloat //收盘价大于昨收价格
         ) {
           reason = zList.reverse.map(_.trade_date).mkString("至")
