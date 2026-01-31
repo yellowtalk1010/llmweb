57148d5d58a8bdec95bcfbe70fcba809f73d7dd3
zuk <huangliao1010@gmail.com>
2026-01-31 15:58:05 +0800

    20260129

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA1_1_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA1_1_Model.scala
@@ -22,26 +22,37 @@ class MA1_1_Model extends IModel {
     if(days.size > max){
       val head = days.head
       val list = days.take(max)
-      val zList = list.filter(_.limit.equals(ModuleDay.Z)) //炸板次数
-      if(zList.size>=2 //多个次炸板
-        && (ListOrderCheck.isIncreasing(zList.map(_.close.toFloat))
-        || ListOrderCheck.isIncreasing(zList.map(_.high.toFloat))
-        || zList(0).high.toFloat > zList(1).close.toFloat) //炸板收盘价递增
-        && list(1).limit.equals(ModuleDay.Z) //上一个交易日炸板
-        && list.head.close.toFloat > list.head.pre_close.toFloat //收盘价大于昨收价格
+
+      val zhaBanIndexList = list.zipWithIndex.filter(_._1.limit.equals("Z"))
+
+      if(zhaBanIndexList.size>=2  //多个次炸板
+        && Math.abs(zhaBanIndexList(0)._2 - zhaBanIndexList(1)._2) > 1 //但不能是连续炸板
+        && list.slice(zhaBanIndexList(0)._2, zhaBanIndexList(1)._2).filter(e=>e.limit.equals(ModuleDay.U) || e.limit.equals(ModuleDay.D)).size==0 //中间不能存在涨停、跌停
       ){
-        reason = zList.map(_.trade_date).mkString("至")
-        val tsStock = DataFrame.STOCKS_MAP.get(days.head.ts_code).getOrElse(null)
-        stockDto = new StockDto(tsStock, super.limitUp(days), super.limitDown(days), super.changeUpRate(days))
-        if (StringUtils.isNotBlank(head.total_mv)) {
-          stockDto.totalMV = new BigDecimal(head.total_mv).divide(new BigDecimal(10000), 2, RoundingMode.UP).floatValue()
-          stockDto.preChangeRate = new BigDecimal(head.change).setScale(2, RoundingMode.HALF_UP).floatValue()
-        }
-        else {
-          stockDto.totalMV = new BigDecimal(days(1).total_mv).divide(new BigDecimal(10000), 2, RoundingMode.UP).floatValue()
-          stockDto.preChangeRate = new BigDecimal(days(1).change).setScale(2, RoundingMode.HALF_UP).floatValue()
+
+        val zList = zhaBanIndexList.map(_._1) //炸板列表
+
+        if ((ListOrderCheck.isIncreasing(zList.map(_.close.toFloat))
+          || ListOrderCheck.isIncreasing(zList.map(_.high.toFloat))
+          || zList(0).high.toFloat > zList(1).close.toFloat) //炸板收盘价递增
+          && list(1).limit.equals(ModuleDay.Z) //上一个交易日炸板
+          && list.head.close.toFloat > list.head.pre_close.toFloat //收盘价大于昨收价格
+        ) {
+          reason = zList.reverse.map(_.trade_date).mkString("至")
+          val tsStock = DataFrame.STOCKS_MAP.get(days.head.ts_code).getOrElse(null)
+          stockDto = new StockDto(tsStock, super.limitUp(days), super.limitDown(days), super.changeUpRate(days))
+          if (StringUtils.isNotBlank(head.total_mv)) {
+            stockDto.totalMV = new BigDecimal(head.total_mv).divide(new BigDecimal(10000), 2, RoundingMode.UP).floatValue()
+            stockDto.preChangeRate = new BigDecimal(head.change).setScale(2, RoundingMode.HALF_UP).floatValue()
+          }
+          else {
+            stockDto.totalMV = new BigDecimal(days(1).total_mv).divide(new BigDecimal(10000), 2, RoundingMode.UP).floatValue()
+            stockDto.preChangeRate = new BigDecimal(days(1).change).setScale(2, RoundingMode.HALF_UP).floatValue()
+          }
         }
       }
+
+
     }
   }
 
