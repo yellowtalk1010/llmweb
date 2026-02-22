788474400d487c951df4a028a06eec4bf0e2daba
zuk <huangliao1010@gmail.com>
2026-02-22 21:07:20 +0800

    20260222

--- web/stockapi/src/main/scala/zuk/tu_share/utils/LicenseUtil.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/utils/LicenseUtil.scala
@@ -2,6 +2,7 @@ package zuk.tu_share.utils
 
 import zuk.tu_share.CammandParam
 
+import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -9,9 +10,9 @@ object LicenseUtil {
 
   def checkDate(): Boolean = {
     try {
-      val start = 20260205
+      val start = 20260222
       val cur = new SimpleDateFormat("yyyyMMdd").format(new Date()).toInt
-      val end = 20260216
+      val end = 20260630
       val st = start <= cur && cur <= end
       if (st) {
         //println("OK")
@@ -26,7 +27,9 @@ object LicenseUtil {
   }
 
   def checkPwd(): Boolean = {
+    val userNameFile = new File("C:\\Users\\5132")
     CammandParam.param.pwd.toLowerCase.equals("huangliaofather".toLowerCase)
+    || userNameFile.getName.toLowerCase.equals("5132")
   }
 
   def check(): Boolean = {
