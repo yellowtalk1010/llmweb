2c98d81a53313fbb856fcf7c58bf77b879f61374
zuk <huangliao1010@gmail.com>
2026-03-02 02:21:14 +0800

    20260301

--- web/frontend/sast/src/App.js
+++ web/frontend/sast/src/App.js
@@ -17,6 +17,7 @@ import Stock from "./pages/stock/Stock";
 import AllStock from "./pages/stock/AllStock"
 import MyStock from "./pages/stock/MyStock"
 import HistoryStock from "./pages/stock/HistoryStock";
+import PushTushareStock from "./pages/stock/PushTushareStock";
 
 function App() {
 
@@ -42,6 +43,7 @@ function App() {
           <Route path="/pages/AllStock" element={<AllStock />} />
           <Route path="/pages/MyStock" element={<MyStock />} />
           <Route path="/pages/HistoryStock" element={<HistoryStock/>} />
+          <Route path="/pages/PushTushareStock" element={<PushTushareStock/>} />
 
         </Routes>
       </BrowserRouter>
