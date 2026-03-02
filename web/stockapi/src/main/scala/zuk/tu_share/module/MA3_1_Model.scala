0ccdbe350eb684abeb4bc1a1033f5ae39401c1c8
zuk <huangliao1010@gmail.com>
2026-03-02 11:02:32 +0800

    20260301

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA3_1_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA3_1_Model.scala
@@ -52,7 +52,7 @@ class MA3_1_Model extends IModel {
 
   override def getStockDto(): StockDto = stockDto
 
-  override def desc(): String = "缩量上涨"
+  override def desc(): String = "缩量上涨，建议下午买入"
 
   override def winRate: Float = {
     val v = DataFrame.properties.get(classOf[MA3_1_Model].getSimpleName.toUpperCase)
