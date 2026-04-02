abc8aea84381f50d24f13cf4c05924b785d88b44
zuk <huangliao1010@gmail.com>
2026-04-02 14:22:28 +0800

    20260401

---
+++ web/playwright/src/main/java/zuk/sast/PlaywrightTest.java
@@ -0,0 +1,32 @@
+package zuk.sast;
+
+import com.microsoft.playwright.*;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+public class PlaywrightTest {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+
+            try {
+                /***
+                 * 1. 启动系统已经安装的chrome
+                 * 2. 前提是系统的chrome启动时，启用了9222端口，
+                 *    命令执行： .\chrome.exe --remote-debugging-port=9222 --user-data-dir=D:\temp\chrome-debug
+                 *    检查端口： netstat -ano | findstr 9222
+                 */
+                Browser browser = playwright.chromium().connectOverCDP("http://127.0.0.1:9222");
+                BrowserContext context = browser.newContext();
+                Page page = context.newPage();
+                page.navigate("https://playwright.dev/");
+                page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("screenshot-" + playwright.chromium().name() + ".png")));
+            }
+            catch (Exception exception) {
+                exception.printStackTrace();
+            }
+
+        }
+    }
+}
