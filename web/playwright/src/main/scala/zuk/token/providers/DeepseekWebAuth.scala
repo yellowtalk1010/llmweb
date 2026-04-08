9ee9a5284f035dad4beecc322b39505f20203fe7
zuk <huangliao1010@gmail.com>
2026-04-09 00:16:35 +0800

    commit

--- web/playwright/src/main/scala/zuk/token/providers/DeepseekWebAuth.scala
+++ web/playwright/src/main/scala/zuk/token/providers/DeepseekWebAuth.scala
@@ -1,7 +1,38 @@
 package zuk.token.providers
 
+import com.microsoft.playwright.Page
+
+import scala.jdk.CollectionConverters.*
+
 class DeepseekWebAuth {
-  
+
   val deepseekWebCharURL: String = "https://chat.deepseek.com"
+
+  def webLogin(): Unit = {
+    val browserContext = ChromeBrowser.browserContext
+    if(browserContext == null){
+      return
+    }
+    val exist = browserContext.pages().asScala.map(_.url()).toSet.filter(_.startsWith(deepseekWebCharURL)).size > 0
+    var deepseekPage: Page = null
+    if(exist){
+      //如果存在，则输出全部url
+      browserContext.pages().asScala.foreach(page => {
+        val url = page.url()
+        println(s"url:${url}")
+        if(deepseekPage==null && page.url().startsWith(deepseekWebCharURL)){
+          deepseekPage = page
+        }
+      })
+    }
+    else {
+      //创建一个新页面
+      deepseekPage = browserContext.newPage()
+      //导航到登录页面
+      deepseekPage.navigate(deepseekWebCharURL)
+    }
+
+
+  }
 
 }
