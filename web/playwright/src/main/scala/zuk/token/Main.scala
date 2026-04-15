8925983c43d456fb9321913214fef37289dba930
zuk <huangliao1010@gmail.com>
2026-04-15 23:35:59 +0800

    commit

--- web/playwright/src/main/scala/zuk/token/Main.scala
+++ web/playwright/src/main/scala/zuk/token/Main.scala
@@ -9,9 +9,8 @@ object Main {
     println("hello")
     val content = "用joern实现c/c++/joern的数据溢出问题"
     ChromeBrowser.chatList.clear()
-    val dsWebAuth = new DeepseekWebAuth()
-    ChromeBrowser.chatList += dsWebAuth
-    ChromeBrowser.chatList.foreach(c=>{
+
+    (ChromeBrowser.chatList ++= Array(new DeepseekWebAuth())).foreach(c=>{
       println(c.getClass.getName)
       c.chat(content)
     })
