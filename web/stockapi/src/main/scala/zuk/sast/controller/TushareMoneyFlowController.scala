17f40cede01b4bd13fd525bef25679502a8e8ca2
zuk <huangliao1010@gmail.com>
2026-05-07 18:39:24 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/sast/controller/TushareMoneyFlowController.scala
@@ -0,0 +1,14 @@
+package zuk.sast.controller
+
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+
+/***
+ * 股票资金流向
+ */
+@RestController
+@RequestMapping(value = Array("money_flow"))
+@Component
+class TushareMoneyFlowController {
+
+}
