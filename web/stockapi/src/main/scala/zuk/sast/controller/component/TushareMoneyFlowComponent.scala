3376b6124b3eed0bdcc9649eaa57ef04b0a04f53
zuk <huangliao1010@gmail.com>
2026-05-07 22:22:02 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/controller/component/TushareMoneyFlowComponent.scala
+++ web/stockapi/src/main/scala/zuk/sast/controller/component/TushareMoneyFlowComponent.scala
@@ -11,6 +11,31 @@ import java.io.{File, FileReader}
 import java.nio.charset.Charset
 import scala.beans.BeanProperty
 
+case class MoneyflowDto() {
+
+  @BeanProperty private var ts_code: String = ""      // str TS代码
+  @BeanProperty private var trade_date: String = ""   // str 交易日期
+  @BeanProperty private var buy_sm_vol: Int = 0       // int 小单买入量（手）
+  @BeanProperty private var buy_sm_amount: Float = 0  // float 小单买入金额（万元）
+  @BeanProperty private var sell_sm_vol: Int = 0       // int 小单卖出量（手）
+  @BeanProperty private var sell_sm_amount: Float = 0  // float 小单卖出金额（万元）
+  @BeanProperty private var buy_md_vol: Int = 0         // int 中单买入量（手）
+  @BeanProperty private var buy_md_amount: Float = 0    // float 中单买入金额（万元）
+  @BeanProperty private var sell_md_vol: Int = 0        // int 中单卖出量（手）
+  @BeanProperty private var sell_md_amount: Float = 0   // float 中单卖出金额（万元）
+  @BeanProperty private var buy_lg_vol: Int = 0         // int 大单买入量（手）
+  @BeanProperty private var buy_lg_amount: Float = 0    // float 大单买入金额（万元 ）
+  @BeanProperty private var sell_lg_vol: Int = 0        // int 大单卖出量（手）
+  @BeanProperty private var sell_lg_amount: Float = 0   // float 大单卖出金额（万元 ）
+  @BeanProperty private var buy_elg_vol: Int = 0        // int 特大单买入量（手）
+  @BeanProperty private var buy_elg_amount: Float = 0   // float 特大单买入金额（万元 ）
+  @BeanProperty private var sell_elg_vol: Int = 0       // int 特大单卖出量（手）
+  @BeanProperty private var sell_elg_amount: Float = 0  // float 特大单卖出金额（万元）
+  @BeanProperty private var net_mf_vol: Int = 0         // int 净流入量（手）
+  @BeanProperty private var net_mf_amount: Float = 0    // float 净流入额（万元）
+
+}
+
 /***
  * 加载历史资金流数据
  */
@@ -39,11 +64,11 @@ class TushareMoneyFlowComponent {
     val list = file.listFiles().toList.sortBy(_.getName).reverse
     val list10 = if(list.size>10) list.take(10) else list
 
-    list10.map(f=>{
+    val records = list10.map(f=>{
       val filename = f.getName
       val in = new FileReader(f.getAbsolutePath, Charset.forName("UTF-8"))
       val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in)
-      (filename, records)
+      records
     })
 
     println("")
