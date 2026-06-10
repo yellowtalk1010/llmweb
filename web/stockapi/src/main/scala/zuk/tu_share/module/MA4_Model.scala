e4af10706365727e49536f8746eab3c83889ec9d
zuk <huangliao1010@gmail.com>
2026-06-10 20:09:26 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA4_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA4_Model.scala
@@ -3,6 +3,8 @@ package zuk.tu_share.module
 import org.apache.commons.lang3.StringUtils
 import zuk.tu_share.DataFrame
 import zuk.tu_share.dto.ModuleDay
+import zuk.tu_share.utils.ListOrderCheck
+
 import java.math.{BigDecimal, RoundingMode}
 
 class MA4_Model extends IModel {
@@ -33,6 +35,7 @@ class MA4_Model extends IModel {
       && List(day.open, day.close, day.high, day.low).map(new BigDecimal(_)).filter(_.compareTo(day.ma.ma10) <= 0).size == 0 //不能下穿ma10
       && head.change.toFloat > 0
     })
+
     val tsStock = DataFrame.STOCKS_MAP.get(days.head.ts_code).getOrElse(null)
     if(ls.size == num && tsStock != null){
       stockDto = new StockDto(tsStock, super.limitUp(days), super.limitDown(days), super.changeUpRate(days))
--- web/stockapi/src/test/scala/zuk/MainTest.scala
+++ web/stockapi/src/test/scala/zuk/MainTest.scala
@@ -4,8 +4,13 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class MainTest extends AnyFunSuite {
 
-  test("main方法测试"){
+  test("main推荐测试"){
     val args = Array("-path", "D:\\development\\github\\stockapi\\", "-json")
+    zuk.Main.main(args)
+  }
+
+  test("main回测测试") {
+    val args = Array("-path", "D:\\development\\github\\stockapi\\", "-json", "-back")
     zuk.Main.main(args)
   }
 
