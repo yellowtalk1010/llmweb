1e333d7fb1670cf848986e578a0e00d4710f84a2
zuk <huangliao1010@gmail.com>
2026-06-11 16:20:38 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA4_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA4_Model.scala
@@ -14,7 +14,7 @@ class MA4_Model extends IModel {
   override def getStockDto(): StockDto = stockDto
 
   override def run(days: List[ModuleDay]): Unit = {
-    val num = 5
+    val num = 4
     if (days.size < num){
       return
     }
