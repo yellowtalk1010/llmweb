b69122bc53604fae01d7afa2fcf811f6232f9dab
zuk <huangliao1010@gmail.com>
2026-06-15 16:23:48 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/controller/TusharePushStockController.scala
+++ web/stockapi/src/main/scala/zuk/sast/controller/TusharePushStockController.scala
@@ -130,7 +130,7 @@ class TusharePushStockController {
       val cls = e.getClass.getSimpleName
       val name = e.desc()
       map.put("cls", cls)
-      map.put("name", s"${e.desc()}")
+      map.put("name", s"${cls}：${e.desc()}")
       map
     }).toBuffer
 
