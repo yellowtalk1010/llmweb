fd81178eb3585e8bbea48ccb35d0a97aebc77f72
zuk <huangliao1010@gmail.com>
2026-06-18 01:20:08 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/controller/component/TushareStockDailyDataComponent.scala
+++ web/stockapi/src/main/scala/zuk/sast/controller/component/TushareStockDailyDataComponent.scala
@@ -1,22 +1,50 @@
 package zuk.sast.controller.component
 
 import jakarta.annotation.PostConstruct
+import org.apache.commons.csv.CSVFormat
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.stereotype.Component
 import zuk.sast.controller.mapper.StockMapper
 import zuk.sast.controller.mapper.entity.StockEntity
+import zuk.tu_share.dto.TsStock
 
-import java.io.File
+import java.io.{File, FileReader}
+import java.nio.charset.Charset
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.concurrent.{ConcurrentHashMap, Executors}
 import scala.beans.BeanProperty
 import scala.jdk.CollectionConverters.*
+import java.math.{BigDecimal, RoundingMode}
 
+case class StockDailyData() {
+  @BeanProperty var ts_code: String = ""
+  @BeanProperty var name: String = ""
+  @BeanProperty var trade_date: String = ""
+  @BeanProperty var open: String = ""
+  @BeanProperty var high: String = ""
+  @BeanProperty var low: String = ""
+  @BeanProperty var close: String = ""
+}
 
 object TushareStockDailyDataComponent {
-  val StockEntityList = new ConcurrentHashMap[String, StockEntity]()
+  val StockEntityMap = new ConcurrentHashMap[String, StockEntity]()
+  val StockDailyDataMap = new ConcurrentHashMap[String, List[StockDailyData]]()
+  private val DAY_NUM = 60
+
+  def increateRate(stockCode: String): String = {
+    if(StockDailyDataMap.get(stockCode)!=null){
+      val list = StockDailyDataMap.get(stockCode)
+      val ls = if(list.size > DAY_NUM) list.take(DAY_NUM) else list
+      val head = new BigDecimal(ls.head.close)
+      val last = new BigDecimal(ls.last.close)
+      last.divide(head, 2, RoundingMode.DOWN).toString
+    }
+    else {
+      ""
+    }
+  }
 }
 /***
  * 加载股票基本数据
@@ -63,7 +91,7 @@ class TushareStockDailyDataComponent {
         val dateStr = date.format(dateFormat)
         val list = this.stockMapper.select_MA4_MA5_By_Createtime(dateStr)
         list.forEach(e => {
-          TushareStockDailyDataComponent.StockEntityList.put(e.stockCode, e)
+          TushareStockDailyDataComponent.StockEntityMap.put(e.stockCode, e)
         })
       }
     }
@@ -76,14 +104,54 @@ class TushareStockDailyDataComponent {
 
   private def refresh_stock_daily_data(): Unit = {
     try {
-      TushareStockDailyDataComponent.StockEntityList.asScala.map(_._2).foreach(e=>{
-
-      })
+      TushareStockDailyDataComponent.StockEntityMap.asScala.map(_._2)
+        .filter(e=>TushareStockDailyDataComponent.StockDailyDataMap.get(e.stockCode)==null)
+        .foreach(e=>{
+          val filename = e.stockCode.replaceAll("\\.", "_") + ".csv"
+          val path = this.stock_daily_data_path + File.separator + filename
+          val file = new File(path)
+          if(file.isFile && file.exists()){
+            log.info(s"加载股票基本数据路径:${file.getAbsolutePath}")
+            val list = loadAllStocks(file)
+            TushareStockDailyDataComponent.StockDailyDataMap.put(e.stockCode, list)
+          }
+        })
     }
     catch {
       case exception: Exception =>
         exception.printStackTrace()
         log.error(exception.getMessage)
+    }
+  }
+
+  private def loadAllStocks(all_stocks_file: File): List[StockDailyData] = {
+
+    try {
+
+      //将tushare的csv数据转成对象
+      val in = new FileReader(all_stocks_file.getAbsolutePath, Charset.forName("UTF-8"))
+      val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in)
+
+      val dataList = records.asScala.map(record => {
+          val stockDailyData = new StockDailyData()
+          stockDailyData.ts_code = record.get("ts_code")
+          stockDailyData.name = record.get("name")
+          stockDailyData.trade_date = record.get("trade_date")
+          stockDailyData.open = record.get("open")
+          stockDailyData.high = record.get("high")
+          stockDailyData.low = record.get("low")
+          stockDailyData.close = record.get("close")
+          stockDailyData
+        })
+        .toList
+      in.close()
+      dataList
+    }
+    catch {
+      case exception: Exception =>
+        exception.printStackTrace()
+        log.error(exception.getMessage)
+        List.empty
     }
   }
 
