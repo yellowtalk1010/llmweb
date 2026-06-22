045447d90ee455db3f67b06b58a450e4a28c5ae4
zuk <huangliao1010@gmail.com>
2026-06-22 12:06:11 +0800

    commit

--- web/stockapi/src/test/scala/zuk/MainTest.scala
+++ web/stockapi/src/test/scala/zuk/MainTest.scala
@@ -1,6 +1,9 @@
 package zuk
 
+import org.apache.commons.io.FileUtils
 import org.scalatest.funsuite.AnyFunSuite
+
+import java.io.File
 
 class MainTest extends AnyFunSuite {
 
@@ -14,4 +17,77 @@ class MainTest extends AnyFunSuite {
     zuk.Main.main(args)
   }
 
+  test("pom依赖生成器"){
+    val file = new File("C:\\Users\\5132\\Desktop\\JavaClaw\\app-1.0.0-SNAPSHOT\\BOOT-INF\\lib")
+    val list = file.listFiles().toList.sortBy(_.getName)
+
+    val groupId = "a"
+    val artifactId = "b"
+    val version = "1.0.0"
+
+    list.zipWithIndex.foreach(e=>{
+      val filename = e._1.getName
+      val index = e._2
+      val path = e._1.getAbsolutePath.replaceAll("\\\\", "/")
+
+      val myArtifactId = s"${artifactId}-${index}"
+
+      /***
+       *         <dependency>
+       *             <groupId>org.junit.platform</groupId>
+       *             <artifactId>junit-platform-launcher</artifactId>
+       *             <version>6.0.3</version>
+       *             <scope>test</scope>
+       *        </dependency>
+       */
+
+      val str = {
+        s"<!-- ${filename} -->\n" +
+        "<dependency>\n" +
+        s"<groupId>${groupId}</groupId>\n" +
+        s"<artifactId>artifactId${myArtifactId}</artifactId>\n" +
+        s"<version>${version}</version>\n" +
+        "<systemPath>${project.basedir}/libs/" + s"${filename}</systemPath>\n" +
+        "<scope>system</scope>\n" +
+        "</dependency>\n"
+      }
+      println(str)
+    })
+
+    println("\n\n\n-------------------------------------m2依赖----------------------------------------\n\n\n")
+
+    list.zipWithIndex.foreach(e=>{
+      val filename = e._1.getName
+      val index = e._2
+
+      val myArtifactId = s"${artifactId}-${index}"
+
+      val str = {
+        s"<!-- ${filename} -->\n" +
+          "<dependency>\n" +
+          s"<groupId>${groupId}</groupId>\n" +
+          s"<artifactId>${myArtifactId}</artifactId>\n" +
+          s"<version>${version}</version>\n" +
+          "</dependency>\n"
+      }
+
+      val jarPath = s"${groupId}/${myArtifactId}/${version}/${myArtifactId}-${version}.jar"
+      val pomPath = s"${groupId}/${myArtifactId}/${version}/${myArtifactId}-${version}.pom"
+
+      val jarFile = new File(jarPath)
+      val pomFile = new File(pomPath)
+      if(jarFile.exists()){
+        jarFile.delete()
+      }
+      if(pomFile.exists()){
+        pomFile.delete()
+      }
+      FileUtils.copyFile(e._1, jarFile)
+      pomFile.createNewFile()
+
+      println(str)
+
+    })
+
+  }
 }
