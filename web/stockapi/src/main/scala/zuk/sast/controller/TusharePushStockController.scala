8fda5d2ce9532312ba3174a563c5c35e95054745
zuk <huangliao1010@gmail.com>
2026-06-25 17:21:39 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/controller/TusharePushStockController.scala
+++ web/stockapi/src/main/scala/zuk/sast/controller/TusharePushStockController.scala
@@ -152,135 +152,139 @@ class TusharePushStockController {
 
     log.info(s"选择模型:${modType}")
 
-    val modSet = if(PassFactory.moduleList().map(_.getClass.getSimpleName).contains(modType)) {
-      Array(modType).toSet
-    }
-    else {
-      PassFactory.moduleList().map(_.getClass.getSimpleName).toSet
-    }
-
     val response = new util.HashMap[String, Object]()
     response.put("code", s"success")
     response.put("time", s"${System.currentTimeMillis()}")
 
-    val simpleDateFormat = new SimpleDateFormat("yyyyMMdd")
-
     val file: File = getStockResultJsonPath()
-    if(file.exists() && file.isDirectory){
-      val jsonfiles = file.listFiles().filter(_.getName.endsWith(".json"))
+    if(!file.exists() || !file.isDirectory){
+      log.info(s"路径不存在: ${this.stockResultJsonPath}")
+      return response
+    }
 
-      var dateStr = simpleDateFormat.format(new Date())
-      val filterJsonFiles = jsonfiles.filter(_.getName.startsWith(dateStr)).sortBy(_.getName).reverse
-      log.info(s"json文件：${filterJsonFiles.map(_.getName).mkString("; ")}")
-
-      val stockResultJsonList = filterJsonFiles.map(file=>{
-        val array = JSONArray.parseArray(FileUtils.readFileToString(file, Charset.forName("UTF-8")), classOf[StockResultJson])
-        array.asScala.map(e=>{
-          val tsStock = new TsStock()
-          tsStock.ts_code = e.ts_code
-          e.eastmoneyURL = tsStock.getEastmoneyURL()
-          e.file=file
-          e.fileName=file.getName
-          val optionTp3 = TushareStockDailyDataComponent.getIncreateRate(e.ts_code)
-          e.remark = optionTp3.get._4
-          e
-        }).sortBy(_.modWinRate).reverse
-      })
-
-      val modWinRateClsNames = stockResultJsonList.flatMap(e=>e)
-        .groupBy(_.modClsName)
-        .filter(e=>modSet.map(_.toUpperCase).contains(e._1.toUpperCase))
-        .map(e=>(e._1, e._2.toList.head)).toList
-        .sortBy(_._2.modWinRate).reverse
-        .map(e=>(e._1, e._2.modWinRate))
-
-      log.info(s"胜率：${modWinRateClsNames.map(e=>{s"${e._1},${e._2}"}).mkString("; ")}")
-
-      val heads = stockResultJsonList.head
-      val histories = stockResultJsonList.slice(1, stockResultJsonList.length).flatMap(e=>e).sortBy(_.modWinRate).reverse
-
-      //保存MA4_MODEL模型数据
-      //tushareInitMA4ModelMA5ModelComponent.add_MA4_MA5_MODEL(heads.toList ++ histories.toList)
-
-      val pushStocks = modWinRateClsNames.map(_._1).map(clsName=>{
-        //最新数据
-        val headList = heads.filter(_.modClsName.equals(clsName)).sortBy(_.turnoverRate).reverse
-        //历史数据
-        val historyList = histories.filter(_.modClsName.equals(clsName)).groupBy(_.ts_code).map(e=>{
-          if(e._2.size==1){
-            e._2.head
-          }
-          else {
-            e._2.sortBy(_.file.getName).head //取最早推荐
-          }
-        }).toList
-          .filter(e=> !headList.map(_.ts_code).contains(e.ts_code)) //不能再headList中
-          .sortBy(_.file.getName)
-          .reverse
-        (clsName, headList, historyList)
-      })
-
-      //获取MA4_MODEL分析到的股票代码
-      val stockModel4EntityList = tushareInitMA4ModelMA5ModelComponent.get_MD4_MODEL_LIST()
-      val ma4Set = pushStocks.filter(_._1.equals("MA4_MODEL")).flatMap(e=>e._2 ++ e._3).map(e=>{
-        e.name = s"${e.name}【${stockModel4EntityList.filter(_.stockCode.trim.equals(e.ts_code.trim)).size}次】"
-        e.ts_code
-      }).toSet
-      //非MA4_MODEL模块击中MA4股票
-      pushStocks.filter(!_._1.equals("MA4_MODEL")).flatMap(e => e._2 ++ e._3).filter(e => ma4Set.contains(e.ts_code)).foreach(e => {
-        e.name = s"${e.name}【击中MA4】"
-      })
-
-      val stockModel5EntityList = tushareInitMA4ModelMA5ModelComponent.get_MD5_MODEL_LIST()
-      val ma5Set = pushStocks.filter(_._1.equals("MA5_MODEL")).flatMap(e => e._2 ++ e._3).map(e => {
-        e.name = s"${e.name}【${stockModel5EntityList.filter(_.stockCode.trim.equals(e.ts_code.trim)).size}次】"
-        e.ts_code
-      }).toSet
-
-      val allAttentionCodes = tushareStockController.getAllAttention() //全部关注的股票
-      val allBuyCodes = tushareStockController.getAllBuy() //全部购买的股票
-      val eliminateCodes = tushareStockController.getAllEliminate() //全部淘汰的股票
-
-      val maplist = pushStocks.map(e=>{
-
-        (e._2 ++ e._3).foreach(e=>{
-          if(e.ts_code.startsWith("688")){
-            e.name = e.name + "【科创】"
-          }
-          else if (e.ts_code.startsWith("920")) {
-            e.name = s"${e.name}【北交所】"
-          }
-          if(allAttentionCodes.contains(e.ts_code)){
-            e.attention = "已关注"
-          }
-          if(allBuyCodes.contains(e.ts_code)){
-            e.buy = "已购买"
-          }
-          if (eliminateCodes.contains(e.ts_code)) {
-            e.eliminate = "已淘汰"
-          }
-        })
-
-        val head = (e._2 ++ e._3).head
-        val map = new util.HashMap[String, Object]()
-        map.put("time", s"${head.file.getName}")
-        map.put("module", s"【${head.modWinRate}】${head.modDesc}【${head.modClsName}】")
-        map.put("heads", e._2.filter(e=> {
-          !e.name.toUpperCase.contains("ST")
-//            && e.turnoverRate.toFloat >= 0.3
-        }).toList.asJava) //移除股票名称中带ST的股票
-        map.put("histories", e._3.filter(e=> {
-          !e.name.toUpperCase.contains("ST")
-//            && e.turnoverRate.toFloat >= 0.3
-        }).sortBy(_.turnoverRate).reverse.asJava) //移除股票名称中带ST的股票
-        map
-      }).asJava
-
-      response.put("data", maplist)
+    //获取选择的模型集合
+    val modSet = if(PassFactory.moduleList().map(_.getClass.getSimpleName).contains(modType)) {
+      Array(modType).toSet
     }
     else {
-      log.info(s"路径不存在: ${this.stockResultJsonPath}")
+      //全部模型集合
+      PassFactory.moduleList().map(_.getClass.getSimpleName).toSet
     }
+
+    val simpleDateFormat = new SimpleDateFormat("yyyyMMdd")
+    val jsonfiles = file.listFiles().filter(_.getName.endsWith(".json"))
+
+    var dateStr = simpleDateFormat.format(new Date())
+    val filterJsonFiles = jsonfiles.filter(_.getName.startsWith(dateStr)).sortBy(_.getName).reverse
+    log.info(s"json文件：${filterJsonFiles.map(_.getName).mkString("; ")}")
+
+    val stockResultJsonList = filterJsonFiles.map(file=>{
+      val array = JSONArray.parseArray(FileUtils.readFileToString(file, Charset.forName("UTF-8")), classOf[StockResultJson])
+      array.asScala.map(e=>{
+        val tsStock = new TsStock()
+        tsStock.ts_code = e.ts_code
+        e.eastmoneyURL = tsStock.getEastmoneyURL()
+        e.file=file
+        e.fileName=file.getName
+        val optionTp3 = TushareStockDailyDataComponent.getIncreateRate(e.ts_code)
+        e.remark = optionTp3.get._4
+        e
+      }).sortBy(_.modWinRate).reverse
+    })
+
+    val modWinRateClsNames = stockResultJsonList.flatMap(e=>e)
+      .groupBy(_.modClsName)
+      .filter(e=>modSet.map(_.toUpperCase).contains(e._1.toUpperCase))
+      .map(e=>(e._1, e._2.toList.head)).toList
+      .sortBy(_._2.modWinRate).reverse
+      .map(e=>(e._1, e._2.modWinRate))
+
+    log.info(s"胜率：${modWinRateClsNames.map(e=>{s"${e._1},${e._2}"}).mkString("; ")}")
+
+    val heads = stockResultJsonList.head
+    val histories = stockResultJsonList.slice(1, stockResultJsonList.length).flatMap(e=>e).sortBy(_.modWinRate).reverse
+
+    //保存MA4_MODEL模型数据
+    //tushareInitMA4ModelMA5ModelComponent.add_MA4_MA5_MODEL(heads.toList ++ histories.toList)
+
+    val pushStocks = modWinRateClsNames.map(_._1).map(clsName=>{
+      //最新数据
+      val headList = heads.filter(_.modClsName.equals(clsName)).sortBy(_.turnoverRate).reverse
+      //历史数据
+      val historyList = histories.filter(_.modClsName.equals(clsName)).groupBy(_.ts_code).map(e=>{
+        if(e._2.size==1){
+          e._2.head
+        }
+        else {
+          e._2.sortBy(_.file.getName).head //取最早推荐
+        }
+      }).toList
+        .filter(e=> !headList.map(_.ts_code).contains(e.ts_code)) //不能再headList中
+        .sortBy(_.file.getName)
+        .reverse
+      (clsName, headList, historyList)
+    })
+
+    //获取MA4_MODEL分析到的股票代码
+    val stockModel4EntityList = tushareInitMA4ModelMA5ModelComponent.get_MD4_MODEL_LIST()
+    val ma4Set = pushStocks.filter(_._1.equals("MA4_MODEL")).flatMap(e=>e._2 ++ e._3).map(e=>{
+      e.name = s"${e.name}【${stockModel4EntityList.filter(_.stockCode.trim.equals(e.ts_code.trim)).size}次】"
+      e.ts_code
+    }).toSet
+    //非MA4_MODEL模块击中MA4股票
+    pushStocks.filter(!_._1.equals("MA4_MODEL")).flatMap(e => e._2 ++ e._3).filter(e => ma4Set.contains(e.ts_code)).foreach(e => {
+      e.name = s"${e.name}【击中MA4】"
+    })
+
+    val stockModel5EntityList = tushareInitMA4ModelMA5ModelComponent.get_MD5_MODEL_LIST()
+    val ma5Set = pushStocks.filter(_._1.equals("MA5_MODEL")).flatMap(e => e._2 ++ e._3).map(e => {
+      e.name = s"${e.name}【${stockModel5EntityList.filter(_.stockCode.trim.equals(e.ts_code.trim)).size}次】"
+      e.ts_code
+    }).toSet
+
+    val allAttentionCodes = tushareStockController.getAllAttention() //全部关注的股票
+    val allBuyCodes = tushareStockController.getAllBuy() //全部购买的股票
+    val eliminateCodes = tushareStockController.getAllEliminate() //全部淘汰的股票
+
+
+
+    val maplist = pushStocks.map(e=>{
+
+      (e._2 ++ e._3).foreach(e=>{
+        if(e.ts_code.startsWith("688")){
+          e.name = e.name + "【科创】"
+        }
+        else if (e.ts_code.startsWith("920")) {
+          e.name = s"${e.name}【北交所】"
+        }
+        if(allAttentionCodes.contains(e.ts_code)){
+          e.attention = "已关注"
+        }
+        if(allBuyCodes.contains(e.ts_code)){
+          e.buy = "已购买"
+        }
+        if (eliminateCodes.contains(e.ts_code)) {
+          e.eliminate = "已淘汰"
+        }
+      })
+
+      val head = (e._2 ++ e._3).head
+      val map = new util.HashMap[String, Object]()
+      map.put("time", s"${head.file.getName}")
+      map.put("module", s"【${head.modWinRate}】${head.modDesc}【${head.modClsName}】")
+      map.put("heads", e._2.filter(e=> {
+        !e.name.toUpperCase.contains("ST")
+//            && e.turnoverRate.toFloat >= 0.3
+      }).toList.asJava) //移除股票名称中带ST的股票
+      map.put("histories", e._3.filter(e=> {
+        !e.name.toUpperCase.contains("ST")
+//            && e.turnoverRate.toFloat >= 0.3
+      }).sortBy(_.turnoverRate).reverse.asJava) //移除股票名称中带ST的股票
+      map
+    }).asJava
+
+    response.put("data", maplist)
+
 
     response
 
