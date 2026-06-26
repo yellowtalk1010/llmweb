93b7b7cf1094f961960b8a955886087aa01724b6
zuk <huangliao1010@gmail.com>
2026-06-27 07:51:42 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA6_Model.scala
@@ -0,0 +1,118 @@
+package zuk.tu_share.module
+
+import org.apache.commons.lang3.StringUtils
+import zuk.tu_share.DataFrame
+import zuk.tu_share.dto.ModuleDay
+import zuk.tu_share.utils.ListOrderCheck
+
+import java.math.{BigDecimal, RoundingMode}
+
+/***
+ * 从交易量角度分析
+ */
+class MA6_Model extends IModel {
+
+  val num = 10
+  var stockDto: StockDto = _
+
+  override def getStockDto(): StockDto = stockDto
+
+  override def run(days: List[ModuleDay]): Unit = {
+    if (days.size < num){
+      return
+    }
+    val head = days.head
+    val list = days.take(num)
+    if(list.filter(e=>e.ma==null
+      || e.ma.ma5==null
+      || e.ma.ma10==null
+      || e.ma.ma20==null
+      || e.ma.ma30==null).size>0){
+      return
+    }
+
+    val ls = list.filter(day=>{
+      day.ma.ma5.compareTo(day.ma.ma10) > 0
+//        && day.ma.ma10.compareTo(day.ma.ma20) > 0
+//        && day.ma.ma20.compareTo(day.ma.ma30) > 0
+    })
+
+    val tsStock = DataFrame.STOCKS_MAP.get(days.head.ts_code).getOrElse(null)
+    if(ls.size == num
+      && tsStock != null
+      && ListOrderCheck.isDecreasing(list.map(_.ma.ma5.floatValue())) //ma5是递增的
+      && ListOrderCheck.isDecreasing(list.map(_.ma.ma10.floatValue())) //ma10是递增的
+//      && ListOrderCheck.isDecreasing(list.map(_.ma.ma20.floatValue())) //ma20是递增的
+//      && ListOrderCheck.isDecreasing(list.map(_.ma.ma30.floatValue())) //ma30是递增的
+
+      && calaChange(list)
+      && calaVol(list)
+    ){
+      stockDto = new StockDto(tsStock, super.limitUp(days), super.limitDown(days), super.changeUpRate(days))
+      stockDto.warningUpperShadow = super.upperShadow(days)
+      if (StringUtils.isNotBlank(head.total_mv)) {
+        stockDto.totalMV = new BigDecimal(head.total_mv).divide(new BigDecimal(10000), 2, RoundingMode.UP).floatValue()
+        stockDto.preChangeRate = new BigDecimal(head.change).setScale(2, RoundingMode.HALF_UP).floatValue()
+      }
+      else {
+        stockDto.totalMV = new BigDecimal(days(1).total_mv).divide(new BigDecimal(10000), 2, RoundingMode.UP).floatValue()
+        stockDto.preChangeRate = new BigDecimal(days(1).change).setScale(2, RoundingMode.HALF_UP).floatValue()
+      }
+    }
+  }
+
+  private def calaVol(list: List[ModuleDay]): Boolean = {
+
+    val preList = list.reverse.take(7)
+    val sufList = list.reverse.slice(7, list.size)
+
+    //平均交易量
+    val avgVol = new BigDecimal(preList.map(_.vol.toFloat).sum).divide(new BigDecimal(preList.size), 2, RoundingMode.UP)
+    //最大交易量
+    val maxVol = new BigDecimal(preList.map(_.vol.toFloat).max)
+
+
+    val moreList = sufList.map(_.vol.toFloat).filter(v=>{
+      new BigDecimal(v).divide(maxVol, 2, RoundingMode.UP).compareTo(new BigDecimal(3)) >= 0 //连续多天放3倍量
+    })
+
+    moreList.size == sufList.size
+  }
+
+  /**
+   * 连续上涨
+   */
+  private def calaChange(list: List[ModuleDay]): Boolean = {
+    val sufList = list.reverse.slice(7, list.size)
+    val st = sufList.filter(_.change.toFloat > 3.0).size == sufList.size
+
+//    var n = 0
+//    for (i <- 1 until sufList.size) {
+//      val pre = sufList(i-1)
+//      val preLowest = List(pre.open.toFloat, pre.low.toFloat, pre.high.toFloat, pre.close.toFloat).min
+//      val cur = sufList(i)
+//      val curMaxest = List(cur.open.toFloat, cur.low.toFloat, cur.high.toFloat, cur.close.toFloat).max
+//      if(curMaxest > preLowest){
+//        n = n + 1
+//      }
+//    }
+
+    st
+  }
+
+  override def desc(): String = "放量倍量连续上涨"
+
+  override def winRate: Float = {
+    val v = DataFrame.properties.get(classOf[MA5_Model].getSimpleName.toUpperCase)
+    if (v != null) {
+      v.toString.toFloat
+    }
+    else {
+      0.8818
+    }
+  }
+
+  override def reference: Float = 0.0
+
+  override def warnUpperShadow: Boolean = false
+}
