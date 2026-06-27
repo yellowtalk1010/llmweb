f01601e85f0171d11e41ab0e14b0093532fbc1f3
zuk <huangliao1010@gmail.com>
2026-06-27 21:11:32 +0800

    commit

--- web/playwright/src/main/scala/zuk/token/providers/deepseek/tasks/DeepseekTask_easymoneyConcept.scala
+++ web/playwright/src/main/scala/zuk/token/providers/deepseek/tasks/DeepseekTask_easymoneyConcept.scala
@@ -4,6 +4,41 @@ import zuk.token.providers.ITask
 
 import scala.beans.BeanProperty
 
+
+/***
+ 输入参数：
+
+
+ 提取 https://emweb.securities.eastmoney.com/pc_hsf10/pages/index.html?type=web&code=SH688323&color=b#/hxtc/tcxq 里面的板块和概念。输出的格式严格采用如下格式：
+```
+##########
+ 股票分析结果如下：
+ 股票代码：填写股票的代码
+ 股票名称：填写股票的名称
+ 概念：概念1，概念2
+ 一级行业：行业1
+ 二级行业：行业2
+ 三级行业: 行业3
+ ##########
+```
+不需要附加任何其他信息的补充说明。
+
+
+
+ 输出参数：
+ ##########
+ 股票分析结果如下：
+ 股票代码：SH688323
+ 股票名称：瑞华泰
+ 概念：电池技术，储能概念，固态电池，商业航天，PCB，华为概念，风能，深圳特区
+ 一级行业：基础化工
+ 二级行业：塑料
+ 三级行业：膜材料
+ ##########
+
+
+ */
+
 /***
  * @param chatContent
  */
