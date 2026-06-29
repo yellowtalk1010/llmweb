d7eb5660c211520d38cee1b711feab8799b3e810
zuk <huangliao1010@gmail.com>
2026-06-29 22:00:57 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/controller/component/TushareConceptComponent.scala
+++ web/stockapi/src/main/scala/zuk/sast/controller/component/TushareConceptComponent.scala
@@ -156,7 +156,7 @@ class TushareConceptComponent {
             //st = false
           }
 
-          Thread.sleep(2 * 60 * 1000)
+          Thread.sleep(5 * 60 * 1000)
         }
         catch {
           case exception: Exception =>
