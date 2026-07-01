d89aa61a94c6063a9a0bb2edfe718df72daa9c05
zuk <huangliao1010@gmail.com>
2026-07-01 12:58:19 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/controller/component/TushareConceptComponent.scala
+++ web/stockapi/src/main/scala/zuk/sast/controller/component/TushareConceptComponent.scala
@@ -42,8 +42,8 @@ class TushareConceptComponent {
     }
     val ls = this.stockInfoEntityList.filter(_.stockCode.equals(stockCode))
     if(ls.size>0 && StringUtils.isNotEmpty(ls.head.concept)){
-      val concept = ls.head.concept.split("\n").filter(l=>{
-        l.trim.startsWith("概念")
+      val concept = ls.head.concept.split("\n").map(_.trim).filter(l=>{
+        l.startsWith("概念")
           || l.startsWith("一级行业")
           || l.startsWith("二级行业")
           || l.startsWith("三级行业")
