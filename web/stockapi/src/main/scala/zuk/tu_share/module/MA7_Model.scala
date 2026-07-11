20b50206e8d8f4dd70431c92674ccba7bf5935b3
zuk <huangliao1010@gmail.com>
2026-07-11 15:21:23 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA7_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA7_Model.scala
@@ -18,32 +18,34 @@ class MA7_Model extends IModel {
   override def getStockDto(): StockDto = stockDto
 
   override def run(days: List[ModuleDay]): Unit = {
-    if (days.size < num){
+    if (days.size < 120){
       return
     }
+
     val head = days.head
-    val size = 1
-    val historyDays = days.slice(size, days.length)
-    val historyHigh = historyDays.map(_.high.toFloat).max //过去历史最高价
-    val avgVol = new BigDecimal(historyDays.map(_.vol.toFloat).sum).divide(new BigDecimal(historyDays.size), 2, RoundingMode.UP).floatValue() //平均交易量
 
-    //最近20天
-    val history_40 = days.slice(1, 20)
-    val avgVol_40 = new BigDecimal(history_40.map(_.vol.toFloat).sum).divide(new BigDecimal(history_40.size), 2, RoundingMode.UP).floatValue()
+    //历史最高价
+    val historyHigh = days.map(_.high.toFloat).max
+    //历史最低价
+    val historyLow = days.map(_.low.toFloat).min
 
-//    val lastestDays = days.take(size)
-//    val lastestLow = lastestDays.map(_.low.toFloat).min //最近最低价
-//    val lastestMaxVol = lastestDays.map(_.vol.toFloat).max //最近最大的一次交易量
+    //最近
+    val recentDays = days.slice(1, num)
+    //最近平均交易量
+    val avgVol = new BigDecimal(recentDays.map(_.vol.toFloat).sum).divide(new BigDecimal(recentDays.size), 2, RoundingMode.UP).floatValue()
 
+    //相比最高价，跌去的比例
+    val downRate = new BigDecimal(historyHigh - head.low.toFloat).divide(new BigDecimal(historyHigh),2,RoundingMode.UP)
+    //相比最低价，涨幅的比例
+    val upRate = new BigDecimal(head.high.toFloat - historyLow).divide(new BigDecimal(historyLow),2,RoundingMode.UP)
 
-    val downRate = new BigDecimal(historyHigh - head.low.toFloat).divide(new BigDecimal(historyHigh),2,RoundingMode.UP) //相比最高价，跌去的比例
-    val volRate = new BigDecimal(head.vol).divide(new BigDecimal(avgVol), 2, RoundingMode.UP).floatValue() //
-    val volRate_40 = new BigDecimal(head.vol).divide(new BigDecimal(avgVol_40), 2, RoundingMode.UP).floatValue() //
+    val volRate = new BigDecimal(head.vol).divide(new BigDecimal(avgVol), 2, RoundingMode.UP).floatValue()
 
-    if(0.4 <= downRate.floatValue() //跌超4个点
-      && volRate_40 > 2.0 //放量2倍
-      && ListOrderCheck.isDecreasing(history_40.reverse.map(_.ma.ma30.floatValue()))
-      && ListOrderCheck.isDecreasing(history_40.reverse.map(_.ma.ma20.floatValue()))
+    if(downRate.floatValue() > 0.4 //跌超大于4个点
+      && upRate.floatValue() < 0.5 //涨幅小于5个点
+      && volRate > 2.0 //放量2倍
+      && (ListOrderCheck.isDecreasing(recentDays.reverse.map(_.ma.ma30.floatValue()))
+        || ListOrderCheck.isDecreasing(recentDays.reverse.map(_.ma.ma20.floatValue())))
       && head.ma.ma30.floatValue() > head.ma.ma20.floatValue()
 //      && head.ma.ma20.floatValue() > List(head.ma.ma10.floatValue(), head.ma.ma5.floatValue()).max
       && head.change.toFloat > 2.0
