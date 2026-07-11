a1dce96b683bcc974ef0bec353d2f8443460e97e
zuk <huangliao1010@gmail.com>
2026-07-11 14:28:25 +0800

    底部放巨量

--- web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/pass/PassFactory.scala
@@ -11,24 +11,20 @@ import scala.jdk.CollectionConverters.*
 
 object PassFactory {
 
-  def moduleList(models: List[IModel] = List.empty): List[IModel] = {
+  val modules = List(
+    //        new MA1_Model,
+    new MA1_1_Model, //最高胜率
+    //        new MA3_0_Model, //上穿MA5
+    //        new MA3_1_Model,
+    //        new MA3_2_Model, //反包两日阴线后继续下跌
+    //        new MA3_3_Model,
+    new MA4_Model,
+    new MA5_Model,
+    new MA6_Model
+  )
 
-    if (models == null || models.isEmpty) {
-      List(
-//        new MA1_Model,
-        new MA1_1_Model, //最高胜率
-//        new MA3_0_Model, //上穿MA5
-//        new MA3_1_Model,
-//        new MA3_2_Model, //反包两日阴线后继续下跌
-//        new MA3_3_Model,
-        new MA4_Model,
-        new MA5_Model,
-        new MA6_Model
-      )
-    }
-    else
-      models
-
+  def moduleList(): List[IModel] = {
+    modules
   }
 
   private def passList(): List[IPass] = {
