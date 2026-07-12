c934eddc034d160709c8647cd62b820e6125743c
zuk <huangliao1010@gmail.com>
2026-07-12 18:25:40 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/utils/HanLPUtil.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/utils/HanLPUtil.scala
@@ -9,10 +9,21 @@ import scala.jdk.CollectionConverters.*
  */
 object HanLPUtil {
 
-  def createFenCi(text: String): List[Term] = {
+  def createFenCi(textList: List[String]): String = {
     //"概念：DeepSeek概念，低空经济，商业航天，一带一路，央国企改革\n一级行业：建筑装饰\n二级行业：工程咨询服务Ⅱ\n三级行业：工程咨询服务Ⅲ"
-    val termList = HanLP.segment(text)
-    termList.asScala.filter(doFilter(_)).toList
+    val ls = textList.flatMap(text=>{
+      val termList = HanLP.segment(text)
+      termList.asScala
+    })
+    val ls1 = ls.filter(doFilter(_)).map(_.word).groupBy(e=>e).toList.sortBy(_._2.size).reverse
+    (if(ls1.size>10){
+      ls1.take(10)
+    }
+    else {
+      ls1
+    }).map(tp=>{
+      s"${tp._1}【${tp._2.size}】"
+    }).mkString(",")
   }
 
   private def doFilter(term: Term): Boolean = {
