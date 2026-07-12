c2a7425d679cd1ef71bfe4481ee98932b30a07d2
zuk <huangliao1010@gmail.com>
2026-07-12 17:58:33 +0800

    commit

--- web/stockapi/src/test/scala/zuk/web/test/Test.scala
+++ web/stockapi/src/test/scala/zuk/web/test/Test.scala
@@ -1,52 +1,23 @@
 package zuk.web.test
 
+import com.hankcs.hanlp.HanLP
 import org.apache.commons.io.FileUtils
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.io.File
 import java.util.concurrent.atomic.AtomicInteger
 
+import scala.jdk.CollectionConverters.*
+
 class Test extends AnyFunSuite {
 
-  test("web") {
-    val libDirFile = new File("D:\\development\\github\\webb\\web\\zuk\\lib")
-    if(!libDirFile.exists()){
-      throw new Exception("路径不存在")
-    }
-    val files = libDirFile.listFiles().sortBy(_.getName)
-    files.map(_.getName).foreach(println)
-    println(files.size)
-    //repository
-    val num = AtomicInteger(0)
-    files.map(srcFile=>{
-      num.incrementAndGet()
-      val groupId = "com.google.zuk"
-      val artifactId = s"web_${num.get()}"
-      val version = "1.0.0"
-      val filename = s"${artifactId}-${version}"
 
-      val targetPath = s"repository/${groupId.replaceAll("\\.","/")}/${artifactId}/${version}"
-      val pomPath = targetPath + "/" + filename + ".pom"
-      val jarPath = targetPath + "/" + filename + ".jar"
+  test("分词测试") {
 
-      new File(pomPath).getParentFile.mkdirs()
-      new File(pomPath).createNewFile()
-      println(pomPath + "，完成")
-
-      FileUtils.copyFile(srcFile, new File(jarPath))
-      println(jarPath + "，完成")
-
-      println()
-
-      val depStr = s"<!-- ${srcFile.getName} -->\n" +
-                   "<dependency>\n" +
-                   s"    <groupId>${groupId}</groupId>\n" +
-                   s"    <artifactId>${artifactId}</artifactId>\n" +
-                   s"    <version>${version}</version>\n" +
-                   "</dependency>\n"
-
-      depStr
-    }).foreach(println)
+    val termList = HanLP.segment("概念：DeepSeek概念，低空经济，商业航天，一带一路，央国企改革\n一级行业：建筑装饰\n二级行业：工程咨询服务Ⅱ\n三级行业：工程咨询服务Ⅲ")
+    termList.asScala.foreach(t=>{
+      println(t.word)
+    })
 
   }
 
