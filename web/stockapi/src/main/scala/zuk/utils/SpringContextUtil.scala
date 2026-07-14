55b8f5782fd9666213ff557a946b0c9eee85102f
zuk <huangliao1010@gmail.com>
2026-07-14 16:32:22 +0800

    commit

---
+++ web/stockapi/src/main/scala/zuk/utils/SpringContextUtil.scala
@@ -0,0 +1,29 @@
+package zuk.utils
+
+import org.springframework.beans.BeansException
+import org.springframework.context.{ApplicationContext, ApplicationContextAware}
+import org.springframework.stereotype.Component
+
+object SpringContextUtil {
+  var context: ApplicationContext = null
+}
+
+@Component
+class SpringContextUtil extends ApplicationContextAware {
+
+  override def setApplicationContext(applicationContext: ApplicationContext): Unit = {
+    SpringContextUtil.context = applicationContext
+  }
+
+  @throws[BeansException]
+  def getBean[T](name: String): T = {
+    SpringContextUtil.context.getBean(name).asInstanceOf[T]
+  }
+
+  @throws[BeansException]
+  def getBean[T](clz: Class[T]): T = {
+    val result: T = SpringContextUtil.context.getBean(clz)
+    result
+  }
+
+}
