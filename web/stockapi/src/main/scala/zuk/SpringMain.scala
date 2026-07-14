943754f8d142334200e5fba91bbca772bafecf8f
zuk <huangliao1010@gmail.com>
2026-07-14 17:59:21 +0800

    commit

--- web/stockapi/src/main/scala/zuk/SpringMain.scala
+++ web/stockapi/src/main/scala/zuk/SpringMain.scala
@@ -2,6 +2,9 @@ package zuk
 
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.context.annotation.{AnnotationConfigApplicationContext, ClassPathScanningCandidateComponentProvider}
+import org.springframework.core.`type`.filter.AnnotationTypeFilter
+import org.springframework.stereotype.Component
+import zuk.tu_share.DataFrame
 import zuk.utils.SpringContextUtil
 
 import scala.jdk.CollectionConverters.*
@@ -9,8 +12,12 @@ import scala.jdk.CollectionConverters.*
 object SpringMain {
 
   def main(args: Array[String]): Unit = {
-    SpringContextUtil.context = new AnnotationConfigApplicationContext
+
+    val annotationConfigApplicationContext = new AnnotationConfigApplicationContext
+    annotationConfigApplicationContext.getBeanFactory.registerSingleton("dataFrame", DataFrame)
+    SpringContextUtil.context = annotationConfigApplicationContext
     val scanner = new ClassPathScanningCandidateComponentProvider(false)
+    scanner.addIncludeFilter(new AnnotationTypeFilter(classOf[Component]))
     val candidates = scanner.findCandidateComponents("zuk.**")
     candidates.asScala.foreach(bd=>{
       val beanClassName =  bd.getBeanClassName
