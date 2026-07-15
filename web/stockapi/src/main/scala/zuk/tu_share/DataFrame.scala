958bf60d0d76ccca0597d088b8d161ae2de7b0f9
zuk <huangliao1010@gmail.com>
2026-07-15 23:31:53 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/DataFrame.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/DataFrame.scala
@@ -32,40 +32,6 @@ object DataFrame {
    */
   val STOCKS_MAP= new mutable.HashMap[String, TsStock]()
 
-  /***
-   * 加载 all_stocks.csv 中的数据
-   */
-  @Deprecated
-  private def load__all_Stocks_csv(all_stocks_path: String): List[TsStock] = {
-    val all_stocks_file = new File(all_stocks_path)
-    println(s"加载all_stocks.csv文件，路径：${all_stocks_file.getAbsolutePath}，${all_stocks_file.exists()}")
-    if (!all_stocks_file.exists() || !all_stocks_file.isFile) {
-      System.exit(1)
-    }
-    //将tushare的csv数据转成对象
-    val in = new FileReader(all_stocks_file.getAbsolutePath, Charset.forName("UTF-8"))
-    val records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in)
-
-    val codes = records.asScala.map(record => {
-        //股票代码
-        val stockCode = record.get("ts_code")
-        val stockName = record.get("name")
-
-        val tsStock = new TsStock(stockCode, stockName)
-        tsStock.symbol = record.get("symbol")
-        tsStock.area = record.get("area")
-        tsStock.industry = record.get("industry")
-        tsStock.market = record.get("market")
-
-        tsStock
-      })
-      .toList
-    in.close()
-    println(s"${codes.size}")
-
-    codes
-  }
-
 
   /***
    * 加载模型数据
