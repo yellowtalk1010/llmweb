73bd70d67f22b0db18cbd17a1c5ec2b5abf0cede
zuk <huangliao1010@gmail.com>
2026-07-26 19:42:10 +0800

    commit

---
+++ web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
@@ -0,0 +1,12 @@
+package zuk
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class ClassCompareTest extends AnyFunSuite {
+
+  test("class比较") {
+    var jarClassPath = "C:\\Users\\5132\\.m2\\repository\\zuk\\cbsast\\merge-93\\2.0.0\\merge-93-2.0.0\\cn"
+    var targetClassPath = "D:\\development\\github\\webb\\web\\cobot-parsers\\target\\classes\\cn"
+  }
+
+}
