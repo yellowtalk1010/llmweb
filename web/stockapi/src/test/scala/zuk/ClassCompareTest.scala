4ebf4add8585250f377c4b77b25be381e2f5ad53
zuk <huangliao1010@gmail.com>
2026-07-26 20:14:22 +0800

    commit

--- web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
+++ web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
@@ -1,8 +1,9 @@
 package zuk
 
+import org.apache.commons.codec.digest.DigestUtils
 import org.scalatest.funsuite.AnyFunSuite
 
-import java.io.IOException
+import java.io.{FileInputStream, IOException}
 import java.nio.file.{Files, Path, Paths}
 import java.util
 import java.util.concurrent.atomic.AtomicInteger
@@ -43,7 +44,24 @@ class ClassCompareTest extends AnyFunSuite {
     jarClassPathFiles.asScala.foreach(p=>{
       val path = targetClassPathMap.get(p.toString)
       if(path==null){
-        println(s"${num.addAndGet(1)}缺少文件：cn${p.toString}")
+        println(s"${num.addAndGet(1)}缺少文件：cn\\${p.toString}")
+      }
+      else {
+        val p1 = jarClassPath + "\\" + p.toString
+        val fis1 = new FileInputStream(p1)
+        val md5_1 = DigestUtils.md5Hex(fis1)
+
+        val p2 = targetClassPath  + "\\" + path.toString
+        val fis2 = new FileInputStream(p2)
+        val md5_2 = DigestUtils.md5Hex(fis2)
+
+
+        if(!md5_1.equals(md5_2)){
+          println(s"文件不相同：cn\\${p.toString}")
+        }
+        else {
+          println("文件相同")
+        }
       }
     })
 
