6384c01e745311b97abfc7ad66dfa7a437f2ea1b
zuk <huangliao1010@gmail.com>
2026-08-13 10:18:22 +0800

    commit

--- web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
+++ web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
@@ -85,6 +85,10 @@ class ClassCompareTest extends AnyFunSuite {
 
   private def ignoreFiles(): Set[String] = {
     Set[String](
+      "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\ASTTypeUtil$1.class",
+      "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\ASTTypeUtil.class",
+      "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\CountedBinding.class",
+      "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\ASTVisitor.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\ASTNameCollector.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\ASTNodeProperty.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\IPDOMManager.class",
