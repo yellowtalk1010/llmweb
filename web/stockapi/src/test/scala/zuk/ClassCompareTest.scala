f148daf5cbd4475c1e9152584552f0143dbd2a6c
zuk <huangliao1010@gmail.com>
2026-08-13 17:56:34 +0800

    commit

--- web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
+++ web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
@@ -85,6 +85,8 @@ class ClassCompareTest extends AnyFunSuite {
 
   private def ignoreFiles(): Set[String] = {
     Set[String](
+      "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\IExternalFunction.class",
+      "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\IExternalBinding.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\IBinding.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\dom\\ast\\IASTTypeId.class",
       "cn\\net\\cobot\\parsers\\cparser\\internal\\core\\dom\\parser\\cpp\\CPPASTTranslationUnit.class",
