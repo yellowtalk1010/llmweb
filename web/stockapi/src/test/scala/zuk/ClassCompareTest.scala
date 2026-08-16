28a85d11cf1e0843cf7d4375c594f2c17fc33ff0
zuk <huangliao1010@gmail.com>
2026-08-16 11:18:36 +0800

    commit

--- web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
+++ web/stockapi/src/test/scala/zuk/ClassCompareTest.scala
@@ -85,6 +85,7 @@ class ClassCompareTest extends AnyFunSuite {
 
   private def ignoreFiles(): Set[String] = {
     Set[String](
+      "cn\\net\\cobot\\parsers\\cparser\\core\\index\\IndexLocationFactory.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\index\\IndexFilter$1.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\index\\IndexFilter$2.class",
       "cn\\net\\cobot\\parsers\\cparser\\core\\index\\IndexFilter.class",
