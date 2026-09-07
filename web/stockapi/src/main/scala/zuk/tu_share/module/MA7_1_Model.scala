53248d8da5a9446421932f66624dd06f9b2906ac
zuk <huangliao1010@gmail.com>
2026-09-07 22:51:39 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/module/MA7_1_Model.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/module/MA7_1_Model.scala
@@ -149,7 +149,7 @@ class MA7_1_Model extends IModel {
 
 
 
-  override def desc(): String = "底部放巨量"
+  override def desc(): String = "底部放巨量（只看量）"
 
   override def winRate: Float = {
     val v = DataFrame.properties.get(classOf[MA7_Model].getSimpleName.toUpperCase)
