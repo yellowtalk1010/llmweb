4956294bbf5b94fb9f815d392a0596c0ef00abb6
zuk <huangliao1010@gmail.com>
2026-09-08 15:13:31 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/utils/HanLPUtil.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/utils/HanLPUtil.scala
@@ -12,11 +12,13 @@ object HanLPUtil {
   def createFenCi(textList: List[String]): String = {
     //"概念：DeepSeek概念，低空经济，商业航天，一带一路，央国企改革\n一级行业：建筑装饰\n二级行业：工程咨询服务Ⅱ\n三级行业：工程咨询服务Ⅲ"
     val ls = textList.flatMap(text=>{
-      val termList = HanLP.segment(text)
-      termList.asScala
+      val termList = HanLP.segment(text).asScala.toSet.toList
+      termList
     })
+
     val ls1 = ls.filter(doFilter(_)).map(_.word).groupBy(e=>e).toList.sortBy(_._2.size).reverse
-    (if(ls1.size>100){
+
+    val str = (if(ls1.size>100){
       ls1.take(100)
     }
     else {
@@ -24,6 +26,8 @@ object HanLPUtil {
     }).map(tp=>{
       s"${tp._1}【${tp._2.size}】"
     }).mkString(",")
+
+    str
   }
 
   private def doFilter(term: Term): Boolean = {
