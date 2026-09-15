f46e8f227d0bd9515bc777437f0c9c0ea7c0b720
zuk <huangliao1010@gmail.com>
2026-09-15 22:18:16 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/utils/HanLPUtil.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/utils/HanLPUtil.scala
@@ -19,7 +19,7 @@ object HanLPUtil {
     val ls1 = ls.filter(doFilter(_)).map(_.word).groupBy(e=>e).toList.sortBy(_._2.size).reverse
 
     val str = (if(ls1.size>100){
-      ls1.take(100)
+      ls1.take(200)
     }
     else {
       ls1
@@ -27,12 +27,13 @@ object HanLPUtil {
       s"${tp._1}【${tp._2.size}】"
     }).mkString(",")
 
-    str
+    s"总词数【${ls1.size}】>>>>" + str
   }
 
   private def doFilter(term: Term): Boolean = {
-    val set = Set[String]("概念", "一级", "二级", "三级", "行业", "名称", "任务", "板块", "股票", "所属", "其他")
-    term.word.size > 1 && !set.contains(term.word)
+    //忽略分词关键字
+    val ignoreSet = Set[String]("概念", "一级", "二级", "三级", "行业", "名称", "任务", "板块", "股票", "所属", "其他", "设备", "建设", "500", "重仓", "国企", "改革", "通用")
+    term.word.size > 1 && !ignoreSet.contains(term.word)
   }
 
 }
