81b03e33f0107e48ea9e93a24eb48e91cd014157
zuk <huangliao1010@gmail.com>
2026-09-22 02:21:34 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/spring/controller/component/TushareStockDailyDataComponent.scala
+++ web/stockapi/src/main/scala/zuk/sast/spring/controller/component/TushareStockDailyDataComponent.scala
@@ -14,38 +14,17 @@ import zuk.tu_share.dto.TsStock
 import zuk.tu_share.utils.Dataset_stock_dailydata_dir
 
 import java.io.{File, FileReader}
+import java.math.{BigDecimal, RoundingMode}
 import java.nio.charset.Charset
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Date
 import java.util.concurrent.{ConcurrentHashMap, Executors}
 import scala.beans.BeanProperty
-import scala.jdk.CollectionConverters.*
-import java.math.{BigDecimal, RoundingMode}
-import java.text.SimpleDateFormat
-import java.util.Date
 import scala.collection.mutable.ListBuffer
+import scala.jdk.CollectionConverters.*
 import scala.math
-
-//case class StockDailyData() {
-//  //股票代码
-//  @BeanProperty var ts_code: String = ""
-//  //股票名称
-//  @BeanProperty var name: String = ""
-//  //交易日期
-//  @BeanProperty var trade_date: String = ""
-//  //上一个交易日收盘价
-//  @BeanProperty var pre_close: String = ""
-//  //交易日开盘价
-//  @BeanProperty var open: String = ""
-//  //交易日最高价
-//  @BeanProperty var high: String = ""
-//  //交易日最低价
-//  @BeanProperty var low: String = ""
-//  //交易日收盘价
-//  @BeanProperty var close: String = ""
-//  //涨跌幅
-//  @BeanProperty var change: String = ""
-//}
 
 object TushareStockDailyDataComponent {
   
