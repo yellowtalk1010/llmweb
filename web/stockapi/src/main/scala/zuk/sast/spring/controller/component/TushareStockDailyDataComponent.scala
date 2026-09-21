ed20269766e04e00251d8dea101a0b1f1cf986d1
zuk <huangliao1010@gmail.com>
2026-09-21 18:54:35 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/spring/controller/component/TushareStockDailyDataComponent.scala
+++ web/stockapi/src/main/scala/zuk/sast/spring/controller/component/TushareStockDailyDataComponent.scala
@@ -180,8 +180,7 @@ class TushareStockDailyDataComponent {
         .filter(e=>TushareStockDailyDataComponent.StockHistoryDailyDataMap.get(e)==null)
         .foreach(stockCode=>{
           val filename = stockCode.replaceAll("\\.", "_") + ".csv"
-          val stock_daily_data_path: String = ParseCammandParam.param.path
-          val path = stock_daily_data_path + File.separator + "module" + File.separator + filename
+          val path = ParseCammandParam.param.path + File.separator + "module" + File.separator + filename
           val file = new File(path)
           if(file.isFile && file.exists()){
             log.info(s"加载股票基本数据路径:${file.getAbsolutePath}")
