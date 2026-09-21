eddc5bf43adcaa24c2193e8e2fed54337c432d44
zuk <huangliao1010@gmail.com>
2026-09-21 16:18:44 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/spring/controller/component/ApplicationProperties.scala
+++ web/stockapi/src/main/scala/zuk/sast/spring/controller/component/ApplicationProperties.scala
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import zuk.tu_share.ParseCammandParam
 
 import java.io.File
 import scala.beans.BeanProperty
@@ -16,22 +17,18 @@ class ApplicationProperties {
   /***
    * 股票分析系统路径
    */
-  @Value("${stock.analysis.system.path}")
-  private var stock_analysis_system_path: String = null
   //股票分析引擎系统路径
-  def getStockAnalysisSystemPath: String = this.stock_analysis_system_path
+  def getStockAnalysisSystemPath: String = ParseCammandParam.param.path
   //股票分析引擎系统结果存储路径
-  def getStockAnalysisSystem_resultJsonSavePath: String = this.stock_analysis_system_path + File.separator + "result_json"
-  //股票分析引擎系统待分析的股票详情
-  def getStockAnalysisSystem_allStocksCsvPath: String = this.stock_analysis_system_path + File.separator + "all_stocks.csv"
+  def getStockAnalysisSystem_resultJsonSavePath: String = ParseCammandParam.param.result_json_dir
   //股票分析引擎系统，股票所属板块和概念路径
-  def getStockAnalysisSystem_conceptPath: String = this.stock_analysis_system_path + File.separator + "concept"
+  def getStockAnalysisSystem_conceptPath: String = ParseCammandParam.param.concept_dir
   //股票分析引擎系统，及时股票信息路径
-  def getStockAnalysisSystem_rtkPath: String = this.stock_analysis_system_path + File.separator + "rt_k" + File.separator + "rt_k.csv"
+  def getStockAnalysisSystem_rtkPath: String = ParseCammandParam.param.rtk_file
   //股票分析引擎系统，回测结果路径
-  def getStockAanlysisSystem_backTestResultPath: String = this.stock_analysis_system_path + File.separator + "MODEL_BACK_TEST_RESULT.txt"
+  def getStockAanlysisSystem_backTestResultPath: String = ParseCammandParam.param.MODEL_BACK_TEST_RESULT_file
   //股票分析引擎系统，获取模型胜率
-  def getStockAnalysisSystem_stock_config_properties: String = this.stock_analysis_system_path + File.separator + "stock_config.properties"
+  def getStockAnalysisSystem_stock_config_properties: String = ParseCammandParam.param.stock_config_properties_file
 
   /***
    * 股票数据源构建系统路径
@@ -49,16 +46,6 @@ class ApplicationProperties {
   @PostConstruct
   def init() = {
     log.info("初始化 application.properties ")
-
-    //
-    val file = new File(this.stock_analysis_system_path)
-    if(file.exists()){
-      log.info(s"股票分析系统路径:${stock_analysis_system_path}, ${file.exists()}")
-    }
-    else {
-      log.info(s"股票分析系统路径:${stock_analysis_system_path}，路径错误")
-      System.exit(1)
-    }
 
     //
     val file1 = new File(this.stock_datasource_build_system_path)
--- web/stockapi/src/main/scala/zuk/tu_share/ParseCammandParam.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/ParseCammandParam.scala
@@ -50,6 +50,9 @@ class ParseCammandParam {
   var rtk_file: String = path + File.separator + "rt_k" + File.separator + "rt_k.csv"
   var MODEL_BACK_TEST_RESULT_file: String = path + File.separator + "MODEL_BACK_TEST_RESULT.txt"
   var stock_config_properties_file: String = path + File.separator + "stock_config.properties"
+  var concept_dir: String = path + File.separator + "concept"
+  var result_json_dir: String = path + File.separator + "result_json"
+  
 
   override def toString: String = {
     s"CammandParam=path:${path}, pwd:******, back: ${back}, back_step:${back_step}, json:${json}, wrate:${wrate}"
