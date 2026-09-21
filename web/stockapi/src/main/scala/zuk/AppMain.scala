235a37c128135fc0e2be1f261da52463b54c0e9d
zuk <huangliao1010@gmail.com>
2026-09-21 12:24:07 +0800

    commit

--- web/stockapi/src/main/scala/zuk/AppMain.scala
+++ web/stockapi/src/main/scala/zuk/AppMain.scala
@@ -1,6 +1,10 @@
 package zuk
 
 import org.springframework.context.annotation.*
+import org.springframework.core.env.{ConfigurableEnvironment, PropertiesPropertySource, SystemEnvironmentPropertySource}
+import org.springframework.core.io.support.ResourcePropertySource
+
+import scala.jdk.CollectionConverters.*
 
 @Configuration
 @ComponentScan(Array("zuk"))
@@ -16,7 +20,37 @@ object AppMain {
   def main(args: Array[String]): Unit = {
     // 1. 启动 Spring 容器，传入配置类
     val ctx = new AnnotationConfigApplicationContext(classOf[AppConfig])
+    val env = ctx.getEnvironment.asInstanceOf[ConfigurableEnvironment]
+    env.getPropertySources.asScala.foreach(ps=>{
+      println(s"${ps.getName}, ${ps.getClass.getName}")
+      
+      ps match {
+        case rps: ResourcePropertySource =>
+          rps.getPropertyNames.foreach(n=>{
+            val s = s"${n}=${rps.getProperty(n)}"
+            println(s)
+          })
+        case pps: PropertiesPropertySource =>
+          //println("")
+        case seps: SystemEnvironmentPropertySource =>
+          //println("")
+        case _=>
 
+          println("")
+      }
+      
+//      if(ps.isInstanceOf[PropertiesPropertySource]){
+//        
+//      }
+//      else if(ps.isInstanceOf[ResourcePropertySource]){
+//        val resourcePropertySource = ps.asInstanceOf[org.springframework.core.io.support.ResourcePropertySource]
+//        val names = resourcePropertySource.getPropertyNames
+//        names.map(n=>{
+//          s"${n}=${resourcePropertySource.getProperty(n)}"
+//        }).foreach(println)
+//      }
+    })
+    ctx.getBeanDefinitionNames.foreach(println)
     // 2. 获取 Bean 并调用
 //    val controller = ctx.getBean(classOf[UserController])
 //    controller.show()
