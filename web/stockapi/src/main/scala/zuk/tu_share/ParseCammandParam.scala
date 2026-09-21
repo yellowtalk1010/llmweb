fbf4731bb10f516b76b1b2a5c948245dad8ccf7f
zuk <huangliao1010@gmail.com>
2026-09-21 19:12:07 +0800

    commit

--- web/stockapi/src/main/scala/zuk/tu_share/ParseCammandParam.scala
+++ web/stockapi/src/main/scala/zuk/tu_share/ParseCammandParam.scala
@@ -60,10 +60,10 @@ class ParseCammandParam {
  * @param engine_path 引擎所在路径
  */
 class EngineInfo(engine_path: String) {
-  var all_stocks_csv_file: String             = engine_path + File.separator + "all_stocks.csv"
+  var all_stocks_csv_file: String             = engine_path + File.separator + "all_stocks.csv"                       //A股市场全量股票文件
   var rtk_file: String                        = engine_path + File.separator + "rt_k" + File.separator + "rt_k.csv"   //分时数据
-  var MODEL_BACK_TEST_RESULT_file: String     = engine_path + File.separator + "MODEL_BACK_TEST_RESULT.txt"
-  var stock_config_properties_file: String    = engine_path + File.separator + "stock_config.properties"
-  var concept_dir: String                     = engine_path + File.separator + "concept"
-  var result_json_dir: String                 = engine_path + File.separator + "result_json"
+  var MODEL_BACK_TEST_RESULT_file: String     = engine_path + File.separator + "MODEL_BACK_TEST_RESULT.txt"           //回测结果保存路径
+  var stock_config_properties_file: String    = engine_path + File.separator + "stock_config.properties"              //回测模型胜率保存结果文件
+  var concept_dir: String                     = engine_path + File.separator + "concept"                              //股票概念路径
+  var result_json_dir: String                 = engine_path + File.separator + "result_json"                          //引擎分析结果保存路径
 }
