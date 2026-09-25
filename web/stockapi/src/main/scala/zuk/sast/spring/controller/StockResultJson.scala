df846d31c376ab920db72047e92a415f750e1092
zuk <huangliao1010@gmail.com>
2026-09-25 14:07:46 +0800

    commit

--- web/stockapi/src/main/scala/zuk/sast/spring/controller/StockResultJson.scala
+++ web/stockapi/src/main/scala/zuk/sast/spring/controller/StockResultJson.scala
@@ -34,7 +34,7 @@ case class StockResultJson(){
   
   @BeanProperty var historyHitCount: Int = 0 //历史出现过的次数
   
-  @BeanProperty var similarity: String = 0 //相似度胜率信息
+  @BeanProperty var similarity: String = "" //相似度胜率信息
 
 }
 
