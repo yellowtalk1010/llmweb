a5ad5b527ce2602a485942362f01afbb01dab9f4
zuk <huangliao1010@gmail.com>
2026-09-26 16:01:02 +0800

    commit

--- web/stockapi/src/main/scala/zuk/consolidation/ConsolidationScanner.scala
+++ web/stockapi/src/main/scala/zuk/consolidation/ConsolidationScanner.scala
@@ -167,7 +167,7 @@ object ConsolidationScanner {
     
     println("over")
     val map = new util.HashMap[String, String]()
-    list.groupBy(_.stockCode).map(_._2.sortBy(_.date.toLong).reverse).filter(_.size>=10).foreach(ls=>{
+    list.groupBy(_.stockCode).map(_._2.sortBy(_.date.toLong).reverse).foreach(ls=>{
       val stockCode = ls.head.stockCode
       val endDateMax = ls.head.date
       val startDateMin = ls.last.date
